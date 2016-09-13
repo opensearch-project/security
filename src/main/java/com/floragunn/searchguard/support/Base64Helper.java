a60fb7bbc5873f72cf4f2e2d590992d30920986a
floragunn <dev@search-guard.com>
2016-09-13 21:56:18 +0200

    Make org.ldaptive.SearchEntry safe for de/serialization to cope with "ElasticsearchException with LDAP authentication" #206

--- src/main/java/com/floragunn/searchguard/support/Base64Helper.java
+++ src/main/java/com/floragunn/searchguard/support/Base64Helper.java
@@ -32,6 +32,7 @@ import java.net.SocketAddress;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 
 import org.elasticsearch.ElasticsearchException;
 
@@ -89,6 +90,7 @@ public class Base64Helper {
 
         static {
             SAFE_CLASSES.add("com.floragunn.dlic.auth.ldap.LdapUser");
+            SAFE_CLASSES.add("org.ldaptive.SearchEntry");
         }
 
         public SafeObjectInputStream(InputStream in) throws IOException {
@@ -108,6 +110,8 @@ public class Base64Helper {
                     InetAddress.class.isAssignableFrom(clazz) ||
                     Number.class.isAssignableFrom(clazz) ||
                     Collection.class.isAssignableFrom(clazz) ||
+                    Map.class.isAssignableFrom(clazz) ||
+                    Enum.class.isAssignableFrom(clazz) ||
                     clazz.equals(User.class) ||
                     SAFE_CLASSES.contains(clazz.getName())
                ) {
