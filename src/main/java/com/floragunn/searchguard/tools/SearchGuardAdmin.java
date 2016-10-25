58823b10c7a6e2b4ef623b6d32e7bf97f41a0041
floragunn <dev@search-guard.com>
2016-10-25 22:36:54 +0200

    fix wrong node count when update a tribe node

--- src/main/java/com/floragunn/searchguard/tools/SearchGuardAdmin.java
+++ src/main/java/com/floragunn/searchguard/tools/SearchGuardAdmin.java
@@ -40,6 +40,9 @@ import org.apache.commons.cli.ParseException;
 import org.elasticsearch.action.WriteConsistencyLevel;
 import org.elasticsearch.action.admin.cluster.health.ClusterHealthRequest;
 import org.elasticsearch.action.admin.cluster.health.ClusterHealthResponse;
+import org.elasticsearch.action.admin.cluster.node.info.NodeInfo;
+import org.elasticsearch.action.admin.cluster.node.info.NodesInfoRequest;
+import org.elasticsearch.action.admin.cluster.node.info.NodesInfoResponse;
 import org.elasticsearch.action.admin.indices.create.CreateIndexRequest;
 import org.elasticsearch.action.admin.indices.exists.indices.IndicesExistsRequest;
 import org.elasticsearch.action.admin.indices.settings.put.UpdateSettingsRequest;
@@ -322,6 +325,8 @@ public class SearchGuardAdmin {
             System.out.println("Number of data nodes: "+chr.getNumberOfDataNodes());
             
             final boolean indexExists = tc.admin().indices().exists(new IndicesExistsRequest(index)).actionGet().isExists();
+            
+            final NodesInfoResponse nodesInfo = tc.admin().cluster().nodesInfo(new NodesInfoRequest()).actionGet();
 
             if (!indexExists) {
                 System.out.print(index +" index does not exists, attempt to create it ... ");
@@ -402,7 +407,7 @@ public class SearchGuardAdmin {
             
             ConfigUpdateResponse cur = tc.execute(ConfigUpdateAction.INSTANCE, new ConfigUpdateRequest(new String[]{"config","roles","rolesmapping","internalusers","actiongroups"})).actionGet();
             
-            success = success & checkConfigUpdateResponse(cur, chr.getNumberOfNodes(), 5);
+            success = success & checkConfigUpdateResponse(cur, nodesInfo, 5);
             
             System.out.println("Done with "+(success?"success":"failures"));
             System.exit(success?0:-1);
@@ -411,8 +416,19 @@ public class SearchGuardAdmin {
     }
     
     
-    private static boolean checkConfigUpdateResponse(ConfigUpdateResponse response, int expectedNodeCount, int expectedConfigCount) {
+    private static boolean checkConfigUpdateResponse(ConfigUpdateResponse response, NodesInfoResponse nir, int expectedConfigCount) {
         
+        int expectedNodeCount = 0;
+        
+        for(NodeInfo ni: nir) {
+            Settings nodeSettings = ni.getSettings();
+          
+            //do not count tribe clients
+            if(nodeSettings.get("tribe.name", null) == null) {
+                expectedNodeCount++;
+            }           
+        }
+
         boolean success = response.getNodes().length == expectedNodeCount;
         if(!success) {
             System.out.println("FAIL: Expected "+expectedNodeCount+" nodes to return response, but got only "+response.getNodes().length);
