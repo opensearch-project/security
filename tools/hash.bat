3db9070948dbb220abdcd1d72aa4c6ae2d9a2918
floragunn <dev@search-guard.com>
2016-10-26 20:29:25 +0200

    add standalone sgadmin artifact

--- tools/hash.bat
+++ tools/hash.bat
@@ -1,4 +1,4 @@
 @echo off
 set SCRIPT_DIR=%~dp0
-"%JAVA_HOME%\bin\java" -cp "%SCRIPT_DIR%\..\..\search-guard-ssl\*;%SCRIPT_DIR%\..\*;%SCRIPT_DIR%\..\..\..\lib\*" com.floragunn.searchguard.tools.Hasher %*
+"%JAVA_HOME%\bin\java" -cp "%SCRIPT_DIR%\..\..\search-guard-ssl\*;%SCRIPT_DIR%\..\deps\*;%SCRIPT_DIR%\..\*;%SCRIPT_DIR%\..\..\..\lib\*" com.floragunn.searchguard.tools.Hasher %*
 
--- tools/sgadmin.sh
+++ tools/sgadmin.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-java $JAVA_OPTS -cp "$DIR/../../search-guard-ssl/*:$DIR/../*:$DIR/../../../lib/*" com.floragunn.searchguard.tools.SearchGuardAdmin "$@"
+java $JAVA_OPTS -cp "$DIR/../../search-guard-ssl/*:$DIR/../*:$DIR/../deps/*:$DIR/../../../lib/*" com.floragunn.searchguard.tools.SearchGuardAdmin "$@"
