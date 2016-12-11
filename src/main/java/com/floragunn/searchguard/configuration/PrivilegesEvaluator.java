96fd460623a795c60fb30367231a9d9832c7dc49
floragunn <dev@search-guard.com>
2016-12-11 21:36:18 +0100

    fix IndexType evaluation issue

--- src/main/java/com/floragunn/searchguard/configuration/PrivilegesEvaluator.java
+++ src/main/java/com/floragunn/searchguard/configuration/PrivilegesEvaluator.java
@@ -183,7 +183,7 @@ public class PrivilegesEvaluator implements ConfigChangeListener {
         }
  
         public String getCombinedString() {
-            return index+type;
+            return index+"#"+type;
         }
 
         @Override
