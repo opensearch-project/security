383c043ca0750d3f5adca71f2ef7f012e903baa7
floragunn <dev@search-guard.com>
2017-01-01 22:01:10 +0100

    fix auditlog hook when dls/fls is also used

--- src/main/java/com/floragunn/searchguard/filter/SearchGuardFilter.java
+++ src/main/java/com/floragunn/searchguard/filter/SearchGuardFilter.java
@@ -155,10 +155,10 @@ public class SearchGuardFilter implements ActionFilter {
         }
 
         if (eval.evaluate(user, action, request)) {
+            auditLog.logAuthenticatedRequest(request, action);
             if(!dlsFlsValve.get().invoke(request, listener)) {
                 return;
             }
-            auditLog.logAuthenticatedRequest(request, action);
             chain.proceed(task, action, request, listener);
             return;
         } else {
