5b3ed821be93b61cdfbbd2b6aaedfc841870a3c2
floragunn <dev@search-guard.com>
2017-05-30 20:34:40 +0200

    fix https://github.com/floragunncom/search-guard/issues/332

--- src/main/java/com/floragunn/searchguard/configuration/PrivilegesEvaluator.java
+++ src/main/java/com/floragunn/searchguard/configuration/PrivilegesEvaluator.java
@@ -814,8 +814,12 @@ public class PrivilegesEvaluator implements ConfigChangeListener {
             if(log.isDebugEnabled()) {
                 log.debug("The following list are '_all' indices: {}", indices);
             }
-            indices.clear();
-            indices.add("_all");
+            
+            //fix https://github.com/floragunncom/search-guard/issues/332
+            if(!indices.isEmpty()) {
+                indices.clear();
+                indices.add("_all");
+            }
         }
 
         if (types.isEmpty()) {
