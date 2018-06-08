504fdaa88cab7d0a0bde9db4dcf528fd64de6a54
floragunn <dev@search-guard.com>
2018-06-08 17:38:19 +0200

    add 'verbose' parameter to authinfo endpoint

--- src/main/java/com/floragunn/searchguard/rest/SearchGuardInfoAction.java
+++ src/main/java/com/floragunn/searchguard/rest/SearchGuardInfoAction.java
@@ -20,10 +20,13 @@ package com.floragunn.searchguard.rest;
 import static org.elasticsearch.rest.RestRequest.Method.*;
 
 import java.io.IOException;
+import java.io.Serializable;
+import java.nio.charset.StandardCharsets;
 import java.security.cert.X509Certificate;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.lucene.util.RamUsageEstimator;
 import org.elasticsearch.client.node.NodeClient;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.TransportAddress;
@@ -38,6 +41,7 @@ import org.elasticsearch.rest.RestStatus;
 import org.elasticsearch.threadpool.ThreadPool;
 
 import com.floragunn.searchguard.configuration.PrivilegesEvaluator;
+import com.floragunn.searchguard.support.Base64Helper;
 import com.floragunn.searchguard.support.ConfigConstants;
 import com.floragunn.searchguard.user.User;
 
@@ -66,6 +70,9 @@ public class SearchGuardInfoAction extends BaseRestHandler {
                 
                 try {
 
+                    
+                    final boolean verbose = request.paramAsBoolean("verbose", false);
+                    
                     final X509Certificate[] certs = threadContext.getTransient(ConfigConstants.SG_SSL_PEER_CERTIFICATES);
                     final User user = (User)threadContext.getTransient(ConfigConstants.SG_USER);
                     final TransportAddress remoteAddress = (TransportAddress) threadContext.getTransient(ConfigConstants.SG_REMOTE_ADDRESS);
@@ -81,6 +88,18 @@ public class SearchGuardInfoAction extends BaseRestHandler {
                     builder.field("sg_tenants", evaluator.mapTenants(user, remoteAddress));
                     builder.field("principal", (String)threadContext.getTransient(ConfigConstants.SG_SSL_PRINCIPAL));
                     builder.field("peer_certificates", certs != null && certs.length > 0 ? certs.length + "" : "0");
+                    
+                    if(user != null && verbose) {
+                        try {
+                            builder.field("size_of_user", RamUsageEstimator.humanReadableUnits(Base64Helper.serializeObject(user).length()));
+                            builder.field("size_of_custom_attributes", RamUsageEstimator.humanReadableUnits(Base64Helper.serializeObject((Serializable) user.getCustomAttributesMap()).getBytes(StandardCharsets.UTF_8).length));
+                            builder.field("size_of_backendroles", RamUsageEstimator.humanReadableUnits(Base64Helper.serializeObject((Serializable)user.getRoles()).getBytes(StandardCharsets.UTF_8).length));
+                        } catch (Throwable e) {
+                            //ignore
+                        }
+                    }
+                    
+                    
                     builder.endObject();
 
                     response = new BytesRestResponse(RestStatus.OK, builder);
