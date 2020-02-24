95bebdc2f2ba99fb0c5d7dce4bf940554962e6b6
Palash Hedau <phhedau@amazon.com>
2020-02-24 15:21:26 -0800

    Added SuperAdmin check to allow update/delete/add of reserved config without restarting elasticsearch
    Add unit test for disallowing anyone apart from SuperAdmin to alter the reserved resources

--- src/main/java/com/amazon/opendistroforelasticsearch/security/dlic/rest/validation/AbstractConfigurationValidator.java
+++ src/main/java/com/amazon/opendistroforelasticsearch/security/dlic/rest/validation/AbstractConfigurationValidator.java
@@ -273,7 +273,7 @@ public abstract class AbstractConfigurationValidator {
     }
 
     public static enum DataType {
-        STRING, ARRAY, OBJECT;
+        STRING, ARRAY, OBJECT, BOOLEAN;
     }
 
     public static enum ErrorType {
