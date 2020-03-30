9ccb2a49090c10944e063acc5c96840c91a4c3f4
Sujith Vadakkepat <svadakke@amazon.com>
2020-03-30 13:41:26 -0700

    move log messages

--- src/main/java/com/amazon/opendistroforelasticsearch/security/auditlog/config/AuditConfig.java
+++ src/main/java/com/amazon/opendistroforelasticsearch/security/auditlog/config/AuditConfig.java
@@ -172,7 +172,9 @@ public class AuditConfig {
 
     public void log(Logger logger) {
         logger.info("Auditing on REST API is {}.", isRestApiAuditEnabled ? "enabled" : "disabled");
+        logger.info("{} are excluded from REST API auditing.", disabledRestCategories);
         logger.info("Auditing on Transport API is {}.", isTransportApiAuditEnabled ? "enabled" : "disabled");
+        logger.info("{} are excluded from Transport API auditing.", disabledTransportCategories);
         logger.info("Auditing of request body is {}.", logRequestBody ? "enabled" : "disabled");
         logger.info("Bulk requests resolution is {} during request auditing.", resolveBulkRequests ? "enabled" : "disabled");
         logger.info("Index resolution is {} during request auditing.", resolveIndices ? "enabled" : "disabled");
@@ -180,8 +182,6 @@ public class AuditConfig {
         logger.info("Auditing requests from {} users is disabled.", ignoredAuditUsers);
         logger.info("Compliance read operation requests auditing from {} users is disabled.", ignoredComplianceUsersForRead);
         logger.info("Compliance write operation requests auditing from {} users is disabled.", ignoredComplianceUsersForWrite);
-        logger.info("{} are excluded from REST API auditing.", disabledRestCategories);
-        logger.info("{} are excluded from Transport API auditing.", disabledTransportCategories);
     }
 
     @Override
