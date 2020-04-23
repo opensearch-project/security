f3894d21d91e83a8791ca97a076d464b788d829d
Gopala Krishna Ambareesh <gopalak@amazon.com>
2020-04-24 03:53:06 +0530

    Also validate & migrate NodesDn in validate & migrate API calls.

--- src/main/java/com/amazon/opendistroforelasticsearch/security/dlic/rest/api/MigrateApiAction.java
+++ src/main/java/com/amazon/opendistroforelasticsearch/security/dlic/rest/api/MigrateApiAction.java
@@ -18,6 +18,7 @@ package com.amazon.opendistroforelasticsearch.security.dlic.rest.api;
 import java.io.IOException;
 import java.nio.file.Path;
 
+import com.amazon.opendistroforelasticsearch.security.securityconf.impl.NodesDn;
 import org.elasticsearch.Version;
 import org.elasticsearch.action.ActionListener;
 import org.elasticsearch.action.admin.indices.create.CreateIndexResponse;
@@ -107,6 +108,7 @@ public class MigrateApiAction extends AbstractApiAction {
         final SecurityDynamicConfiguration<InternalUserV6> internalUsersV6 = (SecurityDynamicConfiguration<InternalUserV6>) load(CType.INTERNALUSERS, true);
         final SecurityDynamicConfiguration<RoleV6> rolesV6 = (SecurityDynamicConfiguration<RoleV6>) load(CType.ROLES, true);
         final SecurityDynamicConfiguration<RoleMappingsV6> rolesmappingV6 = (SecurityDynamicConfiguration<RoleMappingsV6>) load(CType.ROLESMAPPING, true);
+        final SecurityDynamicConfiguration<NodesDn> nodesDnV6 = (SecurityDynamicConfiguration<NodesDn>) load(CType.NODESDN, true);
 
         final SecurityDynamicConfiguration<ActionGroupsV7> actionGroupsV7 = Migration.migrateActionGroups(actionGroupsV6);
         final SecurityDynamicConfiguration<ConfigV7> configV7 = Migration.migrateConfig(configV6);
@@ -114,6 +116,7 @@ public class MigrateApiAction extends AbstractApiAction {
         final Tuple<SecurityDynamicConfiguration<RoleV7>, SecurityDynamicConfiguration<TenantV7>> rolesTenantsV7 = Migration.migrateRoles(rolesV6,
                 rolesmappingV6);
         final SecurityDynamicConfiguration<RoleMappingsV7> rolesmappingV7 = Migration.migrateRoleMappings(rolesmappingV6);
+        final SecurityDynamicConfiguration<NodesDn> nodesDnV7 = Migration.migrateNodesDn(nodesDnV6);
 
         final int replicas = cs.state().metaData().index(opendistroIndex).getNumberOfReplicas();
         final String autoExpandReplicas = cs.state().metaData().index(opendistroIndex).getSettings().get(IndexMetaData.SETTING_AUTO_EXPAND_REPLICAS);
@@ -157,6 +160,8 @@ public class MigrateApiAction extends AbstractApiAction {
                                                 XContentHelper.toXContent(rolesTenantsV7.v2(), XContentType.JSON, false)));
                                         br.add(new IndexRequest().id(CType.ROLESMAPPING.toLCString()).source(CType.ROLESMAPPING.toLCString(),
                                                 XContentHelper.toXContent(rolesmappingV7, XContentType.JSON, false)));
+                                        br.add(new IndexRequest().id(CType.NODESDN.toLCString()).source(CType.NODESDN.toLCString(),
+                                            XContentHelper.toXContent(nodesDnV7, XContentType.JSON, false)));
                                     } catch (final IOException e1) {
                                         log.error("Unable to create bulk request " + e1, e1);
                                         internalErrorResponse(channel, "Unable to create bulk request.");
