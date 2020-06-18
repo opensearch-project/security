651dc87e2d4b1901ef3be725e9c47628348f134e
Sujith Vadakkepat <svadakke@amazon.com>
2020-06-17 23:46:07 -0700

    Create indexing operation listener for every index module call (#491)
    * Create indexing operation listener for every index and check for dynamic compliance access
    
    (cherry picked from commit fa4a137b03281635d50e486dafaacf3b01e2b4ab)

--- src/main/java/com/amazon/opendistroforelasticsearch/security/compliance/ComplianceIndexingOperationListenerImpl.java
+++ src/main/java/com/amazon/opendistroforelasticsearch/security/compliance/ComplianceIndexingOperationListenerImpl.java
@@ -67,7 +67,8 @@ public final class ComplianceIndexingOperationListenerImpl extends ComplianceInd
 
     @Override
     public void postDelete(final ShardId shardId, final Delete delete, final DeleteResult result) {
-        if (auditlog.getComplianceConfig().isEnabled()) {
+        final ComplianceConfig complianceConfig = auditlog.getComplianceConfig();
+        if (isLoggingWriteEnabled(complianceConfig, shardId.getIndexName())) {
             Objects.requireNonNull(is);
             if(result.getFailure() == null && result.isFound() && delete.origin() == org.elasticsearch.index.engine.Engine.Operation.Origin.PRIMARY) {
                 auditlog.logDocumentDeleted(shardId, delete, result);
@@ -77,8 +78,7 @@ public final class ComplianceIndexingOperationListenerImpl extends ComplianceInd
 
     @Override
     public Index preIndex(final ShardId shardId, final Index index) {
-        final ComplianceConfig complianceConfig = auditlog.getComplianceConfig();
-        if (complianceConfig.isEnabled() && complianceConfig.shouldLogDiffsForWrite()) {
+        if (isLoggingWriteDiffEnabled(auditlog.getComplianceConfig(), shardId.getIndexName())) {
             Objects.requireNonNull(is);
 
             final IndexShard shard;
@@ -120,8 +120,7 @@ public final class ComplianceIndexingOperationListenerImpl extends ComplianceInd
 
     @Override
     public void postIndex(final ShardId shardId, final Index index, final Exception ex) {
-        final ComplianceConfig complianceConfig = auditlog.getComplianceConfig();
-        if (complianceConfig.isEnabled() && complianceConfig.shouldLogDiffsForWrite()) {
+        if (isLoggingWriteDiffEnabled(auditlog.getComplianceConfig(), shardId.getIndexName())) {
             threadContext.remove();
         }
     }
@@ -129,7 +128,11 @@ public final class ComplianceIndexingOperationListenerImpl extends ComplianceInd
     @Override
     public void postIndex(ShardId shardId, Index index, IndexResult result) {
         final ComplianceConfig complianceConfig = auditlog.getComplianceConfig();
-        if (complianceConfig.isEnabled() && complianceConfig.shouldLogDiffsForWrite()) {
+        if (!isLoggingWriteEnabled(complianceConfig, shardId.getIndexName())) {
+            return;
+        }
+
+        if (complianceConfig.shouldLogDiffsForWrite()) {
             final Context context = threadContext.get();
             final GetResult previousContent = context==null?null:context.getGetResult();
             threadContext.remove();
@@ -157,7 +160,7 @@ public final class ComplianceIndexingOperationListenerImpl extends ComplianceInd
             }
 
             auditlog.logDocumentWritten(shardId, previousContent, index, result);
-        } else if (complianceConfig.isEnabled()) {
+        } else {
             //no diffs
             if (result.getFailure() != null || index.origin() != org.elasticsearch.index.engine.Engine.Operation.Origin.PRIMARY) {
                 return;
@@ -167,4 +170,11 @@ public final class ComplianceIndexingOperationListenerImpl extends ComplianceInd
         }
     }
 
+    private static boolean isLoggingWriteEnabled(final ComplianceConfig complianceConfig, final String indexName) {
+        return complianceConfig != null && complianceConfig.writeHistoryEnabledForIndex(indexName);
+    }
+
+    private static boolean isLoggingWriteDiffEnabled(final ComplianceConfig complianceConfig, final String indexName) {
+        return isLoggingWriteEnabled(complianceConfig, indexName) && complianceConfig.shouldLogDiffsForWrite();
+    }
 }
