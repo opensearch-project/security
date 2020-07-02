218111980069b9912debb00fa4771c890a34967d
Jason Leezer <jleezer@paypal.com>
2020-07-02 12:03:00 -0500

    PR feedback

--- src/main/java/com/amazon/dlic/auth/http/saml/Saml2SettingsProvider.java
+++ src/main/java/com/amazon/dlic/auth/http/saml/Saml2SettingsProvider.java
@@ -50,17 +50,18 @@ import net.shibboleth.utilities.java.support.resolver.ResolverException;
 public class Saml2SettingsProvider {
     protected final static Logger log = LogManager.getLogger(Saml2SettingsProvider.class);
 
-    private Settings esSettings;
-    private MetadataResolver metadataResolver;
-    private String idpEntityId;
-    private Saml2Settings cachedSaml2Settings;
-    private DateTime metadataUpdateTime;
-    private String spSignaturePrivateKey;
+    private final Settings esSettings;
+    private final MetadataResolver metadataResolver;
+    private final String idpEntityId;
+    private PrivateKey spSignaturePrivateKey;
+    private  Saml2Settings cachedSaml2Settings;
+    private  DateTime metadataUpdateTime;
 
     Saml2SettingsProvider(Settings esSettings, MetadataResolver metadataResolver, PrivateKey spSignaturePrivateKey) {
         this.esSettings = esSettings;
         this.metadataResolver = metadataResolver;
         this.idpEntityId = esSettings.get("idp.entity_id");
+        this.spSignaturePrivateKey = spSignaturePrivateKey;
     }
 
     Saml2Settings get() throws SamlConfigException {
@@ -91,7 +92,7 @@ public class Saml2SettingsProvider {
 
             SettingsBuilder settingsBuilder = new SettingsBuilder();
 
-            configProperties.put(SettingsBuilder.SP_PRIVATEKEY_PROPERTY_KEY,spSignaturePrivateKey);
+            configProperties.put(SettingsBuilder.SP_PRIVATEKEY_PROPERTY_KEY, this.spSignaturePrivateKey);
 
             // TODO allow overriding of IdP metadata?
             settingsBuilder.fromValues(configProperties);
