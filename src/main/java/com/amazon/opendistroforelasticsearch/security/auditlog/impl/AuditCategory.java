151ef984997285c77bf5b51575bb182a7ec6058a
Sujith Vadakkepat <svadakke@amazon.com>
2020-07-20 16:38:59 -0700

    Implement ability to configure readonly fields for audit configuration (#559)
    (cherry picked from commit 67fe5d2b28b28d28987830d4aa734c113c3425cd)

--- src/main/java/com/amazon/opendistroforelasticsearch/security/auditlog/impl/AuditCategory.java
+++ src/main/java/com/amazon/opendistroforelasticsearch/security/auditlog/impl/AuditCategory.java
@@ -2,11 +2,12 @@ package com.amazon.opendistroforelasticsearch.security.auditlog.impl;
 
 import com.amazon.opendistroforelasticsearch.security.support.ConfigConstants;
 
+import com.google.common.collect.ImmutableSet;
 import org.elasticsearch.common.settings.Settings;
 
 import java.util.Collection;
-import java.util.EnumSet;
-import java.util.stream.Collectors;
+import java.util.Collections;
+import java.util.Set;
 
 public enum AuditCategory {
     BAD_HEADERS,
@@ -22,19 +23,18 @@ public enum AuditCategory {
     COMPLIANCE_INTERNAL_CONFIG_READ,
     COMPLIANCE_INTERNAL_CONFIG_WRITE;
 
-    public static EnumSet<AuditCategory> parse(final Collection<String> categories) {
-        EnumSet<AuditCategory> set = EnumSet.noneOf(AuditCategory.class);
+    public static Set<AuditCategory> parse(final Collection<String> categories) {
         if (categories.isEmpty())
-            return set;
+            return Collections.emptySet();
 
         return categories
                 .stream()
                 .map(String::toUpperCase)
                 .map(AuditCategory::valueOf)
-                .collect(Collectors.toCollection(() -> set));
+                .collect(ImmutableSet.toImmutableSet());
     }
 
-    public static EnumSet<AuditCategory> from(final Settings settings, final String key) {
+    public static Set<AuditCategory> from(final Settings settings, final String key) {
         return parse(ConfigConstants.getSettingAsSet(settings, key, ConfigConstants.OPENDISTRO_SECURITY_AUDIT_DISABLED_CATEGORIES_DEFAULT, true));
     }
 }
