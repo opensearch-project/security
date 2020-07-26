4e8b9a9e7207466fca97e569f8838908f93637bb
Debjani Banerjee <bdebjani@amazon.com>
2020-07-25 18:19:15 -0700

    Removing hidden/reserved roles added via rolesmapping
    Earlier the InternalUsers API was filtering out all hidden/reserved roles. We will just restrict it to hidden/reserved roles associated via rolesmapping. This will ensure behaviour of rolesmapping and internalusers API is the same.
    
    (cherry picked from commit 4a6ed34b8788260403fe09ccd40c0d96136428d2)

--- src/main/java/com/amazon/opendistroforelasticsearch/security/dlic/rest/api/InternalUsersApiAction.java
+++ src/main/java/com/amazon/opendistroforelasticsearch/security/dlic/rest/api/InternalUsersApiAction.java
@@ -115,18 +115,24 @@ public class InternalUsersApiAction extends PatchableResourceApiAction {
         final ObjectNode contentAsNode = (ObjectNode) content;
         final SecurityJsonNode securityJsonNode = new SecurityJsonNode(contentAsNode);
 
-        // Don't allow user to add hidden, reserved or non-existent role
+        // Don't allow user to add hidden, reserved or non-existent rolesmapping
         final List<String> opendistroSecurityRoles = securityJsonNode.get("opendistro_security_roles").asList();
         if (opendistroSecurityRoles != null) {
             final SecurityDynamicConfiguration<?> rolesConfiguration = load(CType.ROLES, false);
+            final SecurityDynamicConfiguration<?> rolesmappingConfiguration = load(CType.ROLESMAPPING, false);
             for (final String role: opendistroSecurityRoles) {
 
-                if (!rolesConfiguration.exists(role) || rolesConfiguration.isHidden(role)) {
-                    notFound(channel, "Role '"+role+"' is not found.");
+                if (rolesConfiguration.getCEntry(role) == null) {
+                    notFound(channel, "Role '"+role+"' is not available.");
                     return;
                 }
 
-                if (isReadOnly(rolesConfiguration, role)) {
+                if (rolesmappingConfiguration.isHidden(role)) {
+                    notFound(channel, "Role '"+role+"' is not available.");
+                    return;
+                }
+
+                if (isReadOnly(rolesmappingConfiguration, role)) {
                     forbidden(channel, "Role '" + role + "' is read-only.");
                     return;
                 }
