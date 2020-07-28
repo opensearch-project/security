a23797d6fbb4c913b6e00db83ecbc25c3d8d9897
Palash Hedau <palashhedau900@gmail.com>
2020-07-28 12:17:27 -0700

    Added kibana attribute to security config which will be used by tenantinfo api. (#514)
    Added test cases
    
    Co-authored-by: Palash Hedau <phhedau@amazon.com>
    
    (cherry picked from commit 72ce765f0a767d609ad392c35bd8e72f3c03dd41)

--- src/main/java/com/amazon/opendistroforelasticsearch/security/securityconf/DynamicConfigFactory.java
+++ src/main/java/com/amazon/opendistroforelasticsearch/security/securityconf/DynamicConfigFactory.java
@@ -10,6 +10,7 @@ import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.stream.Collectors;
 
 import com.amazon.opendistroforelasticsearch.security.securityconf.impl.NodesDn;
+import com.google.common.collect.ImmutableList;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.elasticsearch.client.Client;
