2ac8b5abfd7c90eff5965ea7c0ee6e25f0937df4
Sujith Vadakkepat <svadakke@amazon.com>
2020-08-07 19:32:27 -0700

    Fail on invalid rest and transport categories (#638)
    (cherry picked from commit 8800783be4ba5cb425b77d910da9766659b17044)

--- src/main/java/com/amazon/opendistroforelasticsearch/security/dlic/rest/validation/AuditValidator.java
+++ src/main/java/com/amazon/opendistroforelasticsearch/security/dlic/rest/validation/AuditValidator.java
@@ -2,11 +2,35 @@ package com.amazon.opendistroforelasticsearch.security.dlic.rest.validation;
 
 import com.amazon.opendistroforelasticsearch.security.DefaultObjectMapper;
 import com.amazon.opendistroforelasticsearch.security.auditlog.config.AuditConfig;
+import com.amazon.opendistroforelasticsearch.security.auditlog.impl.AuditCategory;
+import com.google.common.collect.ImmutableSet;
 import org.elasticsearch.common.bytes.BytesReference;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.rest.RestRequest;
 
+import java.util.Set;
+
 public class AuditValidator extends AbstractConfigurationValidator {
+
+    private static final Set<AuditCategory> DISABLED_REST_CATEGORIES = ImmutableSet.of(
+            AuditCategory.BAD_HEADERS,
+            AuditCategory.SSL_EXCEPTION,
+            AuditCategory.AUTHENTICATED,
+            AuditCategory.FAILED_LOGIN,
+            AuditCategory.GRANTED_PRIVILEGES,
+            AuditCategory.MISSING_PRIVILEGES
+    );
+
+    private static final Set<AuditCategory> DISABLED_TRANSPORT_CATEGORIES = ImmutableSet.of(
+            AuditCategory.BAD_HEADERS,
+            AuditCategory.SSL_EXCEPTION,
+            AuditCategory.AUTHENTICATED,
+            AuditCategory.FAILED_LOGIN,
+            AuditCategory.GRANTED_PRIVILEGES,
+            AuditCategory.MISSING_PRIVILEGES,
+            AuditCategory.INDEX_EVENT,
+            AuditCategory.OPENDISTRO_SECURITY_INDEX_ATTEMPT
+    );
 
     public AuditValidator(final RestRequest request,
                           final BytesReference ref,
@@ -30,7 +54,14 @@ public class AuditValidator extends AbstractConfigurationValidator {
                 && this.content.length() > 0) {
             try {
                 // try parsing to target type
-                DefaultObjectMapper.readTree(getContentAsNode(), AuditConfig.class);
+                final AuditConfig auditConfig = DefaultObjectMapper.readTree(getContentAsNode(), AuditConfig.class);
+                final AuditConfig.Filter filter = auditConfig.getFilter();
+                if (!DISABLED_REST_CATEGORIES.containsAll(filter.getDisabledRestCategories())) {
+                    throw new IllegalArgumentException("Invalid REST categories passed in the request");
+                }
+                if (!DISABLED_TRANSPORT_CATEGORIES.containsAll(filter.getDisabledTransportCategories())) {
+                    throw new IllegalArgumentException("Invalid transport categories passed in the request");
+                }
             } catch (Exception e) {
                 // this.content is not valid json
                 this.errorType = ErrorType.BODY_NOT_PARSEABLE;
