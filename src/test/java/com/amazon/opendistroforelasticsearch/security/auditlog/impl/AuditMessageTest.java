417e1ee39b442aa8bd9edcbdc580dda99f02b934
Sujith Vadakkepat <svadakke@amazon.com>
2020-08-07 18:52:03 -0700

    Clean up rest and transport header filtering (#637)
    (cherry picked from commit 221d6f03442c804e7b39d8614971f8bec874d7c2)

---
+++ src/test/java/com/amazon/opendistroforelasticsearch/security/auditlog/impl/AuditMessageTest.java
@@ -0,0 +1,89 @@
+package com.amazon.opendistroforelasticsearch.security.auditlog.impl;
+
+import com.amazon.opendistroforelasticsearch.security.auditlog.AuditLog;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import org.elasticsearch.cluster.ClusterName;
+import org.elasticsearch.cluster.node.DiscoveryNode;
+import org.elasticsearch.cluster.service.ClusterService;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class AuditMessageTest {
+
+    private static final Map<String, List<String>> TEST_REST_HEADERS = ImmutableMap.of(
+            "authorization", ImmutableList.of("test-1"),
+            "Authorization", ImmutableList.of("test-2"),
+            "AuThOrIzAtIoN", ImmutableList.of("test-3"),
+            "test-header", ImmutableList.of("test-4")
+    );
+
+    private static final Map<String, String> TEST_TRANSPORT_HEADERS = ImmutableMap.of(
+            "authorization", "test-1",
+            "Authorization", "test-2",
+            "AuThOrIzAtIoN","test-3",
+            "test-header", "test-4"
+    );
+
+    private AuditMessage message;
+
+    @Before
+    public void setUp() {
+        final ClusterService clusterServiceMock = mock(ClusterService.class);
+        when(clusterServiceMock.localNode()).thenReturn(mock(DiscoveryNode.class));
+        when(clusterServiceMock.getClusterName()).thenReturn(mock(ClusterName.class));
+        message = new AuditMessage(AuditCategory.AUTHENTICATED,
+                clusterServiceMock,
+                AuditLog.Origin.REST,
+                AuditLog.Origin.REST);
+    }
+
+    @Test
+    public void testRestHeadersAreFiltered() {
+        message.addRestHeaders(TEST_REST_HEADERS, true);
+        assertEquals(message.getAsMap().get(AuditMessage.REST_REQUEST_HEADERS), ImmutableMap.of("test-header", ImmutableList.of("test-4")));
+    }
+
+    @Test
+    public void testRestHeadersNull() {
+        message.addRestHeaders(null, true);
+        assertNull(message.getAsMap().get(AuditMessage.REST_REQUEST_HEADERS));
+        message.addRestHeaders(Collections.emptyMap(), true);
+        assertNull(message.getAsMap().get(AuditMessage.REST_REQUEST_HEADERS));
+    }
+
+    @Test
+    public void testRestHeadersAreNotFiltered() {
+        message.addRestHeaders(TEST_REST_HEADERS, false);
+        assertEquals(message.getAsMap().get(AuditMessage.REST_REQUEST_HEADERS), TEST_REST_HEADERS);
+    }
+
+    @Test
+    public void testTransportHeadersNull() {
+        message.addTransportHeaders(null, true);
+        assertNull(message.getAsMap().get(AuditMessage.TRANSPORT_REQUEST_HEADERS));
+        message.addTransportHeaders(Collections.emptyMap(), true);
+        assertNull(message.getAsMap().get(AuditMessage.TRANSPORT_REQUEST_HEADERS));
+    }
+
+    @Test
+    public void testTransportHeadersAreFiltered() {
+        message.addTransportHeaders(TEST_TRANSPORT_HEADERS, true);
+        assertEquals(message.getAsMap().get(AuditMessage.TRANSPORT_REQUEST_HEADERS), ImmutableMap.of("test-header", "test-4"));
+    }
+
+    @Test
+    public void testTransportHeadersAreNotFiltered() {
+        message.addTransportHeaders(TEST_TRANSPORT_HEADERS, false);
+        assertEquals(message.getAsMap().get(AuditMessage.TRANSPORT_REQUEST_HEADERS), TEST_TRANSPORT_HEADERS);
+    }
+}
