946459bb8646f694bf6380ffffc204d1beea186e
Sachet Alva <69499526+sachetalva@users.noreply.github.com>
2020-10-21 04:07:04 +0530

    Adding support for SSL dual mode (#712) (#783)
    * Adding SSL dual mode support for SSLOnly
    
    Co-authored-by: Vengadanathan Srinivasan <vengadan@amazon.com>

--- src/main/java/com/amazon/opendistroforelasticsearch/security/ssl/OpenDistroSecuritySSLPlugin.java
+++ src/main/java/com/amazon/opendistroforelasticsearch/security/ssl/OpenDistroSecuritySSLPlugin.java
@@ -17,6 +17,7 @@
 
 package com.amazon.opendistroforelasticsearch.security.ssl;
 
+import com.amazon.opendistroforelasticsearch.security.ssl.transport.OpenDistroSSLConfig;
 import io.netty.handler.ssl.OpenSsl;
 import io.netty.util.internal.PlatformDependent;
 
@@ -92,7 +93,8 @@ public class OpenDistroSecuritySSLPlugin extends Plugin implements ActionPlugin,
     protected PrincipalExtractor principalExtractor;
     protected final Path configPath;
     private final static SslExceptionHandler NOOP_SSL_EXCEPTION_HANDLER = new SslExceptionHandler() {};
-    
+    protected final OpenDistroSSLConfig openDistroSSLConfig;
+
 //    public OpenDistroSecuritySSLPlugin(final Settings settings, final Path configPath) {
 //        this(settings, configPath, false);
 //    }
@@ -106,6 +108,7 @@ public class OpenDistroSecuritySSLPlugin extends Plugin implements ActionPlugin,
             this.transportSSLEnabled = false;
             this.odsks = null;
             this.configPath = null;
+            openDistroSSLConfig = new OpenDistroSSLConfig(false, false);
             
             AccessController.doPrivileged(new PrivilegedAction<Object>() {
                 @Override
@@ -118,7 +121,7 @@ public class OpenDistroSecuritySSLPlugin extends Plugin implements ActionPlugin,
             
             return;
         }
-        
+        openDistroSSLConfig = new OpenDistroSSLConfig(settings);
         this.configPath = configPath;
         
         if(this.configPath != null) {
@@ -252,7 +255,8 @@ public class OpenDistroSecuritySSLPlugin extends Plugin implements ActionPlugin,
         Map<String, Supplier<Transport>> transports = new HashMap<String, Supplier<Transport>>();
         if (transportSSLEnabled) {
             transports.put("com.amazon.opendistroforelasticsearch.security.ssl.http.netty.OpenDistroSecuritySSLNettyTransport",
-                    () -> new OpenDistroSecuritySSLNettyTransport(settings, Version.CURRENT, threadPool, networkService, pageCacheRecycler, namedWriteableRegistry, circuitBreakerService, odsks, NOOP_SSL_EXCEPTION_HANDLER));
+                    () -> new OpenDistroSecuritySSLNettyTransport(settings, Version.CURRENT, threadPool, networkService, pageCacheRecycler, namedWriteableRegistry, circuitBreakerService, odsks, NOOP_SSL_EXCEPTION_HANDLER,
+                        openDistroSSLConfig));
 
         }
         return transports;
