a8835c6c80ce853ed4c4736675bacf2946093159
Sachet Alva <sacheta@amazon.com>
2020-10-20 23:29:43 +0530

    Adding support for SSL dual mode (#712)
    * Adding SSL dual mode support for SSLOnly
    
    (cherry picked from commit 27f33fb)
    
    Co-authored-by: Vengadanathan Srinivasan <vengadan@amazon.com>

--- src/main/java/com/amazon/opendistroforelasticsearch/security/support/ConfigConstants.java
+++ src/main/java/com/amazon/opendistroforelasticsearch/security/support/ConfigConstants.java
@@ -220,6 +220,7 @@ public class ConfigConstants {
     public static final String OPENDISTRO_SECURITY_COMPLIANCE_SALT_DEFAULT = "e1ukloTsQlOgPquJ";//16 chars
     public static final String OPENDISTRO_SECURITY_COMPLIANCE_HISTORY_INTERNAL_CONFIG_ENABLED  = "opendistro_security.compliance.history.internal_config_enabled";
     public static final String OPENDISTRO_SECURITY_SSL_ONLY = "opendistro_security.ssl_only";
+    public static final String OPENDISTRO_SECURITY_CONFIG_SSL_DUAL_MODE_ENABLED = "opendistro_security_config.ssl_dual_mode_enabled";
     public static final String OPENDISTRO_SECURITY_SSL_CERT_RELOAD_ENABLED = "opendistro_security.ssl_cert_reload_enabled";
     public static final String OPENDISTRO_SECURITY_DISABLE_ENVVAR_REPLACEMENT = "opendistro_security.disable_envvar_replacement";
 
