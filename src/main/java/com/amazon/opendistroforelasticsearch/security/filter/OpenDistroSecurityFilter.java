43982037fc7eca376951cdb926c69becb5824023
skkosuri-amzn <skkosuri@amazon.com>
2020-10-26 20:25:52 -0700

    Renames the option to OPENDISTRO_SECURITY_USER_AND_ROLES

--- src/main/java/com/amazon/opendistroforelasticsearch/security/filter/OpenDistroSecurityFilter.java
+++ src/main/java/com/amazon/opendistroforelasticsearch/security/filter/OpenDistroSecurityFilter.java
@@ -98,7 +98,7 @@ import com.amazon.opendistroforelasticsearch.security.user.User;
 
 import static com.amazon.opendistroforelasticsearch.security.OpenDistroSecurityPlugin.isActionTraceEnabled;
 import static com.amazon.opendistroforelasticsearch.security.OpenDistroSecurityPlugin.traceAction;
-import static com.amazon.opendistroforelasticsearch.security.support.ConfigConstants.OPENDISTRO_SECURITY_USER_ROLES_STRING;
+import static com.amazon.opendistroforelasticsearch.security.support.ConfigConstants.OPENDISTRO_SECURITY_USER_AND_ROLES;
 
 public class OpenDistroSecurityFilter implements ActionFilter {
 
@@ -293,8 +293,8 @@ public class OpenDistroSecurityFilter implements ActionFilter {
                 log.debug(pres);
             }
 
-            if(threadContext.getTransient(OPENDISTRO_SECURITY_USER_ROLES_STRING) == null) {
-                threadContext.putTransient(OPENDISTRO_SECURITY_USER_ROLES_STRING, user.getUserRolesString());
+            if(threadContext.getTransient(OPENDISTRO_SECURITY_USER_AND_ROLES) == null) {
+                threadContext.putTransient(OPENDISTRO_SECURITY_USER_AND_ROLES, user.getUserRolesString());
             }
 
             if (pres.isAllowed()) {
--- src/main/java/com/amazon/opendistroforelasticsearch/security/support/ConfigConstants.java
+++ src/main/java/com/amazon/opendistroforelasticsearch/security/support/ConfigConstants.java
@@ -99,7 +99,7 @@ public class ConfigConstants {
     public static final String OPENDISTRO_SECURITY_USER = OPENDISTRO_SECURITY_CONFIG_PREFIX+"user";
     public static final String OPENDISTRO_SECURITY_USER_HEADER = OPENDISTRO_SECURITY_CONFIG_PREFIX+"user_header";
 
-    public static final String OPENDISTRO_SECURITY_USER_ROLES_STRING = OPENDISTRO_SECURITY_CONFIG_PREFIX + "user_roles_string";
+    public static final String OPENDISTRO_SECURITY_USER_AND_ROLES = OPENDISTRO_SECURITY_CONFIG_PREFIX + "user_and_roles";
 
     public static final String OPENDISTRO_SECURITY_INJECTED_USER = "injected_user";
 
