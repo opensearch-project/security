7c5dbb947e2c2ecb3fa51a267b968556428a3cff
Chang Liu <lc12251109@gmail.com>
2020-12-05 10:21:03 -0800

    Catch and respond invalid index name exception when an index with invalid name is mentioned (#865)
    Co-authored-by: cliu123 <cgliu@amazon.com>

--- src/main/java/com/amazon/opendistroforelasticsearch/security/filter/OpenDistroSecurityFilter.java
+++ src/main/java/com/amazon/opendistroforelasticsearch/security/filter/OpenDistroSecurityFilter.java
@@ -349,6 +349,13 @@ public class OpenDistroSecurityFilter implements ActionFilter {
                 listener.onFailure(new ElasticsearchSecurityException(err, RestStatus.FORBIDDEN));
                 return;
             }
+        } catch (ElasticsearchException e) {
+            if (task != null) {
+                log.debug("Failed to apply filter. Task id: {} ({}). Action: {}", task.getId(), task.getDescription(), action, e);
+            } else {
+                log.debug("Failed to apply filter. Action: {}", action, e);
+            }
+            listener.onFailure(e);
         } catch (Throwable e) {
             log.error("Unexpected exception "+e, e);
             listener.onFailure(new ElasticsearchSecurityException("Unexpected exception " + action, RestStatus.INTERNAL_SERVER_ERROR));
