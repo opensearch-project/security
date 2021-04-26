8d3cca2359ec4462b21cece813d2f148cc4c259d
Dhiresh Jain <dhireshj@amazon.com>
2021-04-27 00:59:35 +0530

    Making disable_anonymous_authentication elasticsearch setting dynamic

---
+++ src/main/java/com/amazon/opendistroforelasticsearch/security/setting/DisableAnonymousAuthComplianceSetting.java
@@ -0,0 +1,24 @@
+package com.amazon.opendistroforelasticsearch.security.setting;
+
+import com.amazon.opendistroforelasticsearch.security.support.ConfigConstants;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.elasticsearch.common.settings.ClusterSettings;
+import org.elasticsearch.common.settings.Setting;
+import org.elasticsearch.common.settings.Settings;
+
+public class DisableAnonymousAuthComplianceSetting extends ElasticsearchDynamicSetting<Boolean>{
+
+    public DisableAnonymousAuthComplianceSetting(final Settings settings) {
+        super(Setting.boolSetting(
+                ConfigConstants.OPENDISTRO_SECURITY_COMPLIANCE_DISABLE_ANONYMOUS_AUTHENTICATION,
+                false,
+                Setting.Property.NodeScope, Setting.Property.Filtered, Setting.Property.Dynamic),
+                settings.getAsBoolean(ConfigConstants.OPENDISTRO_SECURITY_COMPLIANCE_DISABLE_ANONYMOUS_AUTHENTICATION, false));
+    }
+
+    @Override
+    protected String getClusterChangeMessage(final Boolean dynamicSettingNewValue) {
+        return String.format("Detected change in settings, cluster setting for anonymousAuthSettingDisabled is %s", dynamicSettingNewValue ? "enabled" : "disabled");
+    }
+}
