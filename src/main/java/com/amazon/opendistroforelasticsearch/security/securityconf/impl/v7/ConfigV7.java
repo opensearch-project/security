c99702a353368c925516f8fdf6e8f108c8a2ff2e
cgliu <cgliu@amazon.com>
2021-05-05 09:40:51 -0700

    Revert the index name change in ConfigV7

--- src/main/java/com/amazon/opendistroforelasticsearch/security/securityconf/impl/v7/ConfigV7.java
+++ src/main/java/com/amazon/opendistroforelasticsearch/security/securityconf/impl/v7/ConfigV7.java
@@ -137,7 +137,7 @@ public class ConfigV7 {
         public boolean multitenancy_enabled = true;
         public String server_username = "kibanaserver";
         public String opendistro_role = null;
-        public String index = ".openSearchDashboards";
+        public String index = ".kibana";
         @Override
         public String toString() {
             return "OpenSearchDashboards [multitenancy_enabled=" + multitenancy_enabled + ", server_username=" + server_username + ", opendistro_role=" + opendistro_role
