e58099b1080c100820f2c294c7d99c6247aa57c1
Azar Fazel <azar.fazel@gmail.com>
2021-08-09 16:15:25 -0700

    Modify unit tests in DashboardsInfoActionTest

--- src/test/java/org/opensearch/security/dlic/rest/api/DashboardsInfoActionTest.java
+++ src/test/java/org/opensearch/security/dlic/rest/api/DashboardsInfoActionTest.java
@@ -15,6 +15,7 @@
 
 package org.opensearch.security.dlic.rest.api;
 
+import java.util.Arrays;
 import org.opensearch.security.support.ConfigConstants;
 import org.opensearch.security.test.helper.rest.RestHelper;
 import org.apache.http.Header;
@@ -24,7 +25,6 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
-import com.google.common.collect.ImmutableList;
 
 import static org.opensearch.security.OpenSearchSecurityPlugin.LEGACY_OPENDISTRO_PREFIX;
 import static org.opensearch.security.OpenSearchSecurityPlugin.PLUGINS_PREFIX;
@@ -33,29 +33,20 @@ import static org.opensearch.security.OpenSearchSecurityPlugin.PLUGINS_PREFIX;
 public class DashboardsInfoActionTest extends AbstractRestApiUnitTest {
 
     private final String ENDPOINT;
+    private final String SECURITY_ENDPOINT;
 
-    public DashboardsInfoActionTest(String endpoint){
+    public DashboardsInfoActionTest(String endpoint, String securityEndpoint){
+
         ENDPOINT = endpoint;
+        SECURITY_ENDPOINT = securityEndpoint;
     }
 
     @Parameterized.Parameters
-    public static Iterable<String> endpoints() {
-        return ImmutableList.of(
-                LEGACY_OPENDISTRO_PREFIX + "/api/securityconfig",
-                PLUGINS_PREFIX + "/api/securityconfig"
-        );
-    }
-
-    @Test
-    public void testSecurityConfig() throws Exception {
-        Settings settings = Settings.builder().put(ConfigConstants.SECURITY_UNSUPPORTED_RESTAPI_ALLOW_SECURITYCONFIG_MODIFICATION, true).build();
-        setup(settings);
-
-        rh.keystore = "restapi/kirk-keystore.jks";
-        rh.sendAdminCertificate = true;
-        RestHelper.HttpResponse response = rh.executePostRequest(ENDPOINT, "{\"xxx\": 1}", new Header[0]);
-        Assert.assertEquals(HttpStatus.SC_METHOD_NOT_ALLOWED, response.getStatusCode());
-
+    public static Iterable<String[]> endpoints() {
+        return Arrays.asList(new String[][] {
+                {LEGACY_OPENDISTRO_PREFIX + "/kibanainfo", LEGACY_OPENDISTRO_PREFIX + "/api/securityconfig"},
+                {PLUGINS_PREFIX + "/dashboardsinfo", PLUGINS_PREFIX + "/api/securityconfig"}
+        });
     }
 
     @Test
@@ -66,20 +57,20 @@ public class DashboardsInfoActionTest extends AbstractRestApiUnitTest {
         rh.keystore = "restapi/kirk-keystore.jks";
         rh.sendAdminCertificate = true;
 
-        RestHelper.HttpResponse response = rh.executeGetRequest(PLUGINS_PREFIX + "/dashboardsinfo");
+        RestHelper.HttpResponse response = rh.executeGetRequest(ENDPOINT);
         Assert.assertEquals(HttpStatus.SC_OK, response.getStatusCode());
     }
 
     @Test
-    public void testKibanaInfo() throws Exception {
+    public void testSecurityConfig() throws Exception {
         Settings settings = Settings.builder().put(ConfigConstants.SECURITY_UNSUPPORTED_RESTAPI_ALLOW_SECURITYCONFIG_MODIFICATION, true).build();
         setup(settings);
 
         rh.keystore = "restapi/kirk-keystore.jks";
         rh.sendAdminCertificate = true;
+        RestHelper.HttpResponse response = rh.executePostRequest(SECURITY_ENDPOINT, "{\"xxx\": 1}", new Header[0]);
+        Assert.assertEquals(HttpStatus.SC_METHOD_NOT_ALLOWED, response.getStatusCode());
 
-        RestHelper.HttpResponse response = rh.executeGetRequest(LEGACY_OPENDISTRO_PREFIX + "/kibanainfo");
-        Assert.assertEquals(HttpStatus.SC_OK, response.getStatusCode());
     }
 
 }
