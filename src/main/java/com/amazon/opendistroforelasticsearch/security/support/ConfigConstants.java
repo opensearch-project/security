10c59e4c5774241a26f5e8da20438eb338abd167
Sai <karanas@amazon.com>
2021-09-15 23:13:17 +0530

    [Backport] Extended role injection support for cross cluster requests (#1195) (#1441)
    (cherry picked from commit 4b1664419dfc09f500896a3ffaa4664741d01d01)
    Signed-off-by: Sai Kumar <karanas@amazon.com>

--- src/main/java/com/amazon/opendistroforelasticsearch/security/support/ConfigConstants.java
+++ src/main/java/com/amazon/opendistroforelasticsearch/security/support/ConfigConstants.java
@@ -265,6 +265,7 @@ public class ConfigConstants {
 
     // Roles injection for plugins
     public static final String OPENDISTRO_SECURITY_INJECTED_ROLES = "opendistro_security_injected_roles";
+    public static final String OPENDISTRO_SECURITY_INJECTED_ROLES_HEADER = "opendistro_security_injected_roles_header";
 
     // Roles validation for the plugins
     public static final String OPENDISTRO_SECURITY_INJECTED_ROLES_VALIDATION = "opendistro_security_injected_roles_validation";
