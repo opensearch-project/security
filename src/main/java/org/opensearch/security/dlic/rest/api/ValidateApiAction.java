e50cade584ce579da89ed421a65999fada44313c
Dave Lago <davelago@amazon.com>
2021-10-28 10:15:58 -0400

    Updated copyright notices
    Signed-off-by: Dave Lago <davelago@amazon.com>

--- src/main/java/org/opensearch/security/dlic/rest/api/ValidateApiAction.java
+++ src/main/java/org/opensearch/security/dlic/rest/api/ValidateApiAction.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * Copyright OpenSearch Contributors
  *
  * Licensed under the Apache License, Version 2.0 (the "License").
  * You may not use this file except in compliance with the License.
