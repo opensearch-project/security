46c186ff59aa6b62bf37fa36f04be3ff0f3132d6
Nidhi Sridhar <srnidhi@amazon.com>
2021-11-04 11:05:46 -0700

    fix to include hidden indices when resolving wildcards
    (cherry-picked from commit 5a9935a)
    
    Signed-off-by: Nidhi Sridhar <srnidhi@amazon.com>

--- src/main/java/com/amazon/opendistroforelasticsearch/security/resolver/IndexResolverReplacer.java
+++ src/main/java/com/amazon/opendistroforelasticsearch/security/resolver/IndexResolverReplacer.java
@@ -687,7 +687,7 @@ public class IndexResolverReplacer {
     private IndicesOptions indicesOptionsFrom(Object localRequest) {
         
         if(!respectRequestIndicesOptions) {
-            return IndicesOptions.fromOptions(false, true, true, false);
+            return IndicesOptions.fromOptions(false, true, true, false, true);
         }
 
         if (IndicesRequest.class.isInstance(localRequest)) {
@@ -697,7 +697,7 @@ public class IndexResolverReplacer {
             return ((RestoreSnapshotRequest) localRequest).indicesOptions();
         }
         else {
-            return IndicesOptions.fromOptions(false, true, true, false);
+            return IndicesOptions.fromOptions(false, true, true, false, true);
         }
     }
 
