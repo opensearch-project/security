565f47e804ec03aeeba02ca8def563b91307fcc7
Dave Lago <davelago@amazon.com>
2021-11-09 13:30:45 -0500

    Updated copyright notices (#1477)
    Signed-off-by: Dave Lago <davelago@amazon.com>

--- src/main/java/org/opensearch/security/dlic/rest/validation/NodesDnValidator.java
+++ src/main/java/org/opensearch/security/dlic/rest/validation/NodesDnValidator.java
@@ -1,5 +1,5 @@
 /*
- * Portions Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * Portions Copyright OpenSearch Contributors
  *
  * Licensed under the Apache License, Version 2.0 (the "License").
  * You may not use this file except in compliance with the License.
