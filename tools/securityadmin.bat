7aee239296d788f986157edd199e743f501180de
Andriy Redko <andriy.redko@aiven.io>
2022-02-09 15:29:40 -0500

    Add support for OPENSEARCH_JAVA_HOME
    Signed-off-by: Andriy Redko <andriy.redko@aiven.io>

--- tools/securityadmin.bat
+++ tools/securityadmin.bat
@@ -1,3 +1,11 @@
 @echo off
 set SCRIPT_DIR=%~dp0
-"%JAVA_HOME%\bin\java" -Dorg.apache.logging.log4j.simplelog.StatusLogger.level=OFF -cp "%SCRIPT_DIR%\..\..\opendistro_security-ssl\*;%SCRIPT_DIR%\..\deps\*;%SCRIPT_DIR%\..\*;%SCRIPT_DIR%\..\..\..\lib\*" org.opensearch.security.tools.SecurityAdmin %* 2> nul
+
+rem comparing to empty string makes this equivalent to bash -v check on env var
+if not "%OPENSEARCH_JAVA_HOME%" == "" (
+  set BIN_PATH="%OPENSEARCH_JAVA_HOME%\bin\java.exe"
+) else (
+  set BIN_PATH="%JAVA_HOME%\bin\java.exe"
+)
+
+%BIN_PATH% -Dorg.apache.logging.log4j.simplelog.StatusLogger.level=OFF -cp "%SCRIPT_DIR%\..\..\opendistro_security-ssl\*;%SCRIPT_DIR%\..\deps\*;%SCRIPT_DIR%\..\*;%SCRIPT_DIR%\..\..\..\lib\*" org.opensearch.security.tools.SecurityAdmin %* 2> nul
