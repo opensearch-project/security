7ebb2ca045e6ff259c5c8084762eee5656820e33
Andriy Redko <drreta@gmail.com>
2022-02-10 16:42:53 -0500

    Add support for OPENSEARCH_JAVA_HOME (#1603)
    Signed-off-by: Andriy Redko <andriy.redko@aiven.io>

--- tools/hash.bat
+++ tools/hash.bat
@@ -1,4 +1,12 @@
 @echo off
 set SCRIPT_DIR=%~dp0
-"%JAVA_HOME%\bin\java" -cp "%SCRIPT_DIR%\..\..\opendistro_security_ssl\*;%SCRIPT_DIR%\..\deps\*;%SCRIPT_DIR%\..\*;%SCRIPT_DIR%\..\..\..\lib\*" org.opensearch.security.tools.Hasher %*
+
+rem comparing to empty string makes this equivalent to bash -v check on env var
+if not "%OPENSEARCH_JAVA_HOME%" == "" (
+  set BIN_PATH="%OPENSEARCH_JAVA_HOME%\bin\java.exe"
+) else (
+  set BIN_PATH="%JAVA_HOME%\bin\java.exe"
+)
+
+%BIN_PATH% -cp "%SCRIPT_DIR%\..\..\opendistro_security_ssl\*;%SCRIPT_DIR%\..\deps\*;%SCRIPT_DIR%\..\*;%SCRIPT_DIR%\..\..\..\lib\*" org.opensearch.security.tools.Hasher %*
 
