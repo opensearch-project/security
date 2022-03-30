ebdaaaae5f0bf06a2ca19c18912ed1244037ce65
Peter Nied <petern@amazon.com>
2022-03-30 12:48:53 -0500

    Speeding up tests (#1715)

--- src/test/java/org/opensearch/security/test/AbstractSecurityUnitTest.java
+++ src/test/java/org/opensearch/security/test/AbstractSecurityUnitTest.java
@@ -192,18 +192,7 @@ public abstract class AbstractSecurityUnitTest {
             Assert.assertEquals(clusterInfo.numNodes, cur.getNodes().size());
 
             SearchResponse sr = tc.search(new SearchRequest(".opendistro_security")).actionGet();
-            //Assert.assertEquals(5L, sr.getHits().getTotalHits());
-
             sr = tc.search(new SearchRequest(".opendistro_security")).actionGet();
-            //Assert.assertEquals(5L, sr.getHits().getTotalHits());
-
-            String type=securityConfig.getType();
-            try {
-                Thread.sleep(10000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-
         }
     }
 
