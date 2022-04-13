e213e9c23124418f6f3f2b4a35568ab89ae25232
Peter Nied <petern@amazon.com>
2022-04-13 14:12:54 -0500

    Add signal/wait model for TestAuditlogImpl (#1758)
    * Add signal/wait model for TestAuditlogImpl
    
    I have been tracking test failures with testRestMethod very often
    showing failures.  My theory is that the execution environment can
    impact the order of operations sometimes causing the audit log not to
    contain messages before it is checked.
    
    Adding a new method `doThenWaitForMessages(...)` this ensures the
    log queue is fresh, the triggering action completes, and the expected
    number of messages were recieved.  There is a second long time window
    that allows for the messages to be flushed, this is likely more than
    enough - if the messages are recieved the count down latch immediately
    continues execution so the tests will not wait if they are ready to
    proceed.
    
    While this new method is much more reliable not all tests were
    encountering such issues, so I've keep the original convention.  This
    can be migrated in one-offs or all at once if we see more troublesome
    behavoir.  The previous methods/fields are depreciated to push future
    tests to follow the new pattern.
    
    Modifications to the rest helper not to throw exceptions were needed to
    keep the Runnable declaration clean and small.
    
    Signed-off-by: Peter Nied <petern@amazon.com>

--- src/test/java/org/opensearch/security/auditlog/integration/TestAuditlogImpl.java
+++ src/test/java/org/opensearch/security/auditlog/integration/TestAuditlogImpl.java
@@ -17,6 +17,9 @@ package org.opensearch.security.auditlog.integration;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
 
 import org.opensearch.common.settings.Settings;
 
@@ -25,23 +28,61 @@ import org.opensearch.security.auditlog.sink.AuditLogSink;
 
 public class TestAuditlogImpl extends AuditLogSink {
 
+    /** Use the results of `doThenWaitForMessages(...)` instead */
+    @Deprecated
     public static List<AuditMessage> messages = new ArrayList<AuditMessage>(100);
+    /** Check messages indvidually instead of searching this string */
+    @Deprecated
     public static StringBuffer sb = new StringBuffer();
+    private static final AtomicReference<CountDownLatch> countDownRef = new AtomicReference<>();
+    private static final AtomicReference<List<AuditMessage>> messagesRef = new AtomicReference<>();
 
     public TestAuditlogImpl(String name, Settings settings, String settingsPrefix, AuditLogSink fallbackSink) {
         super(name, settings, null, fallbackSink);
     }
 
-
-    public synchronized boolean  doStore(AuditMessage msg) {
+    public synchronized boolean doStore(AuditMessage msg) {
+        if (messagesRef.get() == null || countDownRef.get() == null) {
+            throw new RuntimeException("No message latch is waiting");
+        }
         sb.append(msg.toPrettyString()+System.lineSeparator());
-        messages.add(msg);
+        messagesRef.get().add(msg);
+        countDownRef.get().countDown();
         return true;
     }
 
+    /** Unneeded after switching to `doThenWaitForMessages(...)` as data is automatically flushed */
+    @Deprecated
     public static synchronized void clear() {
-        sb.setLength(0);
-        messages.clear();
+        doThenWaitForMessages(() -> {}, 0);
+    }
+
+    /**
+     * Perform an action and then wait until the expected number of messages have been found.
+     */
+    public static List<AuditMessage> doThenWaitForMessages(final Runnable action, final int expectedCount) {
+        final CountDownLatch latch = new CountDownLatch(expectedCount);
+        final List<AuditMessage> messages = new ArrayList<>();
+        countDownRef.set(latch);
+        messagesRef.set(messages);
+
+        TestAuditlogImpl.sb = new StringBuffer();
+        TestAuditlogImpl.messages = messages; 
+        
+        try {
+            action.run();
+            final int maxSecondsToWaitForMessages = 1; 
+            final boolean foundAll = latch.await(maxSecondsToWaitForMessages, TimeUnit.SECONDS);
+            if (!foundAll) {
+                throw new RuntimeException("Did not recieve all " + expectedCount +" audit messages after a short wait.");
+            }
+            if (messages.size() != expectedCount) {
+                throw new RuntimeException("Unexpected number of messages, was expecting " + expectedCount + ", recieved " + messages.size());
+            }
+        } catch (final InterruptedException e) {
+            throw new RuntimeException("Unexpected exception", e);
+        }
+        return new ArrayList<>(messages);
     }
 
     @Override
