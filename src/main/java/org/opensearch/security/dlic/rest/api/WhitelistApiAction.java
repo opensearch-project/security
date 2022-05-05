c9c4d0eda6f8b4c171d66157c27de03963558d02
cliu123 <lc12251109@gmail.com>
2022-05-04 22:11:19 -0700

    WhitelistApiAction extends AllowlistApiAction
    Signed-off-by: cliu123 <lc12251109@gmail.com>

--- src/main/java/org/opensearch/security/dlic/rest/api/WhitelistApiAction.java
+++ src/main/java/org/opensearch/security/dlic/rest/api/WhitelistApiAction.java
@@ -18,30 +18,23 @@ package org.opensearch.security.dlic.rest.api;
 
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.Collections;
 import java.util.List;
 
-import com.fasterxml.jackson.databind.JsonNode;
 import com.google.common.collect.ImmutableList;
 
-import org.opensearch.action.index.IndexResponse;
 import org.opensearch.client.Client;
 import org.opensearch.cluster.service.ClusterService;
-import org.opensearch.common.bytes.BytesReference;
 import org.opensearch.common.inject.Inject;
 import org.opensearch.common.settings.Settings;
-import org.opensearch.rest.RestChannel;
 import org.opensearch.rest.RestController;
 import org.opensearch.rest.RestRequest;
-import org.opensearch.security.DefaultObjectMapper;
 import org.opensearch.security.auditlog.AuditLog;
 import org.opensearch.security.configuration.AdminDNs;
 import org.opensearch.security.configuration.ConfigurationRepository;
-import org.opensearch.security.dlic.rest.validation.AbstractConfigurationValidator;
-import org.opensearch.security.dlic.rest.validation.WhitelistValidator;
 import static org.opensearch.security.dlic.rest.support.Utils.addDeprecatedRoutesPrefix;
 import org.opensearch.security.privileges.PrivilegesEvaluator;
 import org.opensearch.security.securityconf.impl.CType;
-import org.opensearch.security.securityconf.impl.SecurityDynamicConfiguration;
 import org.opensearch.security.ssl.transport.PrincipalExtractor;
 import org.opensearch.threadpool.ThreadPool;
 
@@ -85,14 +78,12 @@ import org.opensearch.threadpool.ThreadPool;
  * be used to populate the index.
  * <p>
  */
-public class WhitelistApiAction extends PatchableResourceApiAction {
+public class WhitelistApiAction extends AllowlistApiAction {
     private static final List<DeprecatedRoute> routes = addDeprecatedRoutesPrefix(ImmutableList.of(
             new DeprecatedRoute(RestRequest.Method.GET, "/whitelist", "[/whitelist] is a deprecated endpoint. Please use [/allowlist] instead."),
             new DeprecatedRoute(RestRequest.Method.PUT, "/whitelist", "[/whitelist] is a deprecated endpoint. Please use [/allowlist] instead."),
             new DeprecatedRoute(RestRequest.Method.PATCH, "/whitelist", "[/whitelist] is a deprecated endpoint. Please use [/allowlist] instead.")
     ));
-
-    private static final String name = "config";
 
     @Inject
     public WhitelistApiAction(final Settings settings, final Path configPath, final RestController controller, final Client client,
@@ -101,55 +92,9 @@ public class WhitelistApiAction extends PatchableResourceApiAction {
         super(settings, configPath, controller, client, adminDNs, cl, cs, principalExtractor, evaluator, threadPool, auditLog);
     }
 
-    @Override
-    protected void handleApiRequest(final RestChannel channel, final RestRequest request, final Client client) throws IOException {
-        if (!isSuperAdmin()) {
-            forbidden(channel, "API allowed only for super admin.");
-            return;
-        }
-        super.handleApiRequest(channel, request, client);
+    public List<Route> routes() {
+        return Collections.emptyList();
     }
-
-    @Override
-    protected void handleGet(final RestChannel channel, RestRequest request, Client client, final JsonNode content)
-            throws IOException {
-
-
-        final SecurityDynamicConfiguration<?> configuration = load(getConfigName(), true);
-        filter(configuration);
-        successResponse(channel, configuration);
-    }
-
-    @Override
-    protected void handleDelete(final RestChannel channel, final RestRequest request, final Client client, final JsonNode content) throws IOException {
-        notImplemented(channel, RestRequest.Method.DELETE);
-    }
-
-    @Override
-    protected void handlePut(final RestChannel channel, final RestRequest request, final Client client, final JsonNode content) throws IOException {
-        final SecurityDynamicConfiguration<?> existingConfiguration = load(getConfigName(), false);
-
-        if (existingConfiguration.getSeqNo() < 0) {
-            forbidden(channel, "Security index need to be updated to support '" + getConfigName().toLCString() + "'. Use SecurityAdmin to populate.");
-            return;
-        }
-
-        boolean existed = existingConfiguration.exists(name);
-        existingConfiguration.putCObject(name, DefaultObjectMapper.readTree(content, existingConfiguration.getImplementingClass()));
-
-        saveAnUpdateConfigs(client, request, getConfigName(), existingConfiguration, new OnSucessActionListener<IndexResponse>(channel) {
-
-            @Override
-            public void onResponse(IndexResponse response) {
-                if (existed) {
-                    successResponse(channel, "'" + name + "' updated.");
-                } else {
-                    createdResponse(channel, "'" + name + "' created.");
-                }
-            }
-        });
-    }
-
 
     @Override
     public List<DeprecatedRoute> deprecatedRoutes() {
@@ -159,16 +104,6 @@ public class WhitelistApiAction extends PatchableResourceApiAction {
     @Override
     protected Endpoint getEndpoint() {
         return Endpoint.WHITELIST;
-    }
-
-    @Override
-    protected AbstractConfigurationValidator getValidator(RestRequest request, BytesReference ref, Object... param) {
-        return new WhitelistValidator(request, ref, this.settings, param);
-    }
-
-    @Override
-    protected String getResourceName() {
-        return name;
     }
 
     @Override
