2ed9d385b5569c131b3115da72745856e0b62509
Peter Nied <petern@amazon.com>
2022-05-10 22:17:09 +0000

    Fixing breaking changes from FilterLeafReader
    Signed-off-by: Peter Nied <petern@amazon.com>

--- src/main/java/org/opensearch/security/configuration/DlsFlsFilterLeafReader.java
+++ src/main/java/org/opensearch/security/configuration/DlsFlsFilterLeafReader.java
@@ -421,6 +421,11 @@ class DlsFlsFilterLeafReader extends SequentialStoredFieldsLeafReader  {
         public void close() throws IOException {
             in.close();
         }
+
+        @Override
+        public long ramBytesUsed() {
+            return in.ramBytesUsed();
+        }
     }
 
     @Override
@@ -516,12 +521,6 @@ class DlsFlsFilterLeafReader extends SequentialStoredFieldsLeafReader  {
         }
 
         @Override
-        public void stringField(final FieldInfo fieldInfo, final String value) throws IOException {
-            fieldReadCallback.stringFieldRead(fieldInfo, value);
-            delegate.stringField(fieldInfo, value);
-        }
-
-        @Override
         public void intField(final FieldInfo fieldInfo, final int value) throws IOException {
             fieldReadCallback.numericFieldRead(fieldInfo, value);
             delegate.intField(fieldInfo, value);
@@ -605,11 +604,6 @@ class DlsFlsFilterLeafReader extends SequentialStoredFieldsLeafReader  {
         }
 
         @Override
-        public void stringField(final FieldInfo fieldInfo, final String value) throws IOException {
-            delegate.stringField(fieldInfo, value);
-        }
-
-        @Override
         public void intField(final FieldInfo fieldInfo, final int value) throws IOException {
             delegate.intField(fieldInfo, value);
         }
@@ -673,17 +667,6 @@ class DlsFlsFilterLeafReader extends SequentialStoredFieldsLeafReader  {
         @Override
         public int hashCode() {
             return delegate.hashCode();
-        }
-
-        @Override
-        public void stringField(final FieldInfo fieldInfo, final String value) throws IOException {
-            final Optional<MaskedField> mf = maskedFieldsMap.getMaskedField(fieldInfo.name);
-
-            if(mf.isPresent()) {
-                delegate.stringField(fieldInfo, mf.get().mask(value));
-            } else {
-                delegate.stringField(fieldInfo, value);
-            }
         }
 
         @Override
