56c9768ab2a7725bcfee43bd8128f0322fcb0634
Darshit Chanpura <dchanp@amazon.com>
2022-06-06 11:50:56 -0400

    Updates license headers
    Signed-off-by: Darshit Chanpura <dchanp@amazon.com>

--- src/main/java/org/opensearch/security/tools/SecurityAdmin.java
+++ src/main/java/org/opensearch/security/tools/SecurityAdmin.java
@@ -14,18 +14,14 @@
  */
 
 /*
- * Portions Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
  *
- * Licensed under the Apache License, Version 2.0 (the "License").
- * You may not use this file except in compliance with the License.
- * A copy of the License is located at
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
  *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * or in the "license" file accompanying this file. This file is distributed
- * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
- * express or implied. See the License for the specific language governing
- * permissions and limitations under the License.
+ * Modifications Copyright OpenSearch Contributors. See
+ * GitHub history for details.
  */
 
 package org.opensearch.security.tools;
