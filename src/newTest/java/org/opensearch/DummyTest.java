17a37a363078d174b0dd27fa685ba374b44465a8
devops <devops@automat.berlin>
2022-06-13 16:53:47 +0200

    add dummy test to newTest directory

---
+++ src/newTest/java/org/opensearch/DummyTest.java
@@ -0,0 +1,18 @@
+package org.opensearch;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class DummyTest {
+
+    @Test
+    public void test() {
+        int a = 1, b = 2, expectedSum = 3;
+
+        int actualSum = a + b;
+
+        assertEquals(expectedSum, actualSum);
+    }
+
+}
