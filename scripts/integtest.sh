7717120100e9d7f561a022eabbe8e8498b7f8154
Darshit Chanpura <dchanp@amazon.com>
2022-07-20 17:24:49 -0400

    Updates integtest script
    Signed-off-by: Darshit Chanpura <dchanp@amazon.com>

--- scripts/integtest.sh
+++ scripts/integtest.sh
@@ -14,16 +14,16 @@ function usage() {
     echo "Optional arguments:"
     echo -e "-b BIND_ADDRESS\t, defaults to localhost | 127.0.0.1, can be changed to any IP or domain name for the cluster location."
     echo -e "-p BIND_PORT\t, defaults to 9200, can be changed to any port for the cluster location."
-    echo -e "-t TRANSPORT_PORT\t, defaults to 9300, can be changed to any port for the cluster location."
     echo -e "-s SECURITY_ENABLED\t(true | false), defaults to true. Specify the OpenSearch/Dashboards have security enabled or not."
     echo -e "-c CREDENTIAL\t(usename:password), no defaults, effective when SECURITY_ENABLED=true."
     echo -e "-h\tPrint this message."
     echo -e "-v OPENSEARCH_VERSION\t, no defaults"
     echo -e "-n SNAPSHOT\t, defaults to false"
+    echo -e "-m CLUSTER_NAME\t, defaults to docker-cluster"
     echo "--------------------------------------------------------------------------"
 }
 
-while getopts ":h:b:p:s:c:v:n:t:" arg; do
+while getopts ":h:b:p:s:c:v:n:t:m:" arg; do
     case $arg in
         h)
             usage
@@ -43,6 +43,9 @@ while getopts ":h:b:p:s:c:v:n:t:" arg; do
             ;;
         c)
             CREDENTIAL=$OPTARG
+            ;;
+        m)
+            CLUSTER_NAME=$OPTARG
             ;;
         v)
             # Do nothing as we're not consuming this param.
@@ -73,11 +76,6 @@ then
   BIND_PORT="9200"
 fi
 
-if [ -z "$TRANSPORT_PORT" ]
-then
-  TRANSPORT_PORT="9300"
-fi
-
 if [ -z "$SECURITY_ENABLED" ]
 then
   SECURITY_ENABLED="true"
@@ -88,7 +86,17 @@ then
   CREDENTIAL="admin:admin"
 fi
 
+if [ -z "$CREDENTIAL" ]
+then
+  CREDENTIAL="admin:admin"
+fi
+
+if [ -z "$CLUSTER_NAME" ]
+then
+  CLUSTER_NAME="docker-cluster"
+fi
+
 USERNAME=`echo $CREDENTIAL | awk -F ':' '{print $1}'`
 PASSWORD=`echo $CREDENTIAL | awk -F ':' '{print $2}'`
 
-./gradlew integTestRemote -Dfollower.http_host="$BIND_ADDRESS:$BIND_PORT" -Dfollower.transport_host="$BIND_ADDRESS:$TRANSPORT_PORT" -Dsecurity_enabled=$SECURITY_ENABLED -Duser=$USERNAME -Dpassword=$PASSWORD --console=plain
+./gradlew integTestRemote -Dtests.rest.cluster="$BIND_ADDRESS:$BIND_PORT" -Dtests.cluster="$BIND_ADDRESS:$BIND_PORT" -Dsecurity_enabled=$SECURITY_ENABLED -Dtests.clustername=$CLUSTER_NAME -Dhttps=true -Duser=$USERNAME -Dpassword=$PASSWORD
