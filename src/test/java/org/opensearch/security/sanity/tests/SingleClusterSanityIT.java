6c3f82af00f0ea9e4ccb563935b4fb3aabf2ee84
Darshit Chanpura <dchanp@amazon.com>
2022-08-02 19:38:31 -0400

    Removes incorrect license headers
    Signed-off-by: Darshit Chanpura <dchanp@amazon.com>

--- src/test/java/org/opensearch/security/sanity/tests/SingleClusterSanityIT.java
+++ src/test/java/org/opensearch/security/sanity/tests/SingleClusterSanityIT.java
@@ -1,19 +1,4 @@
 /*
- * Copyright 2015-2018 _floragunn_ GmbH
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-/*
  * SPDX-License-Identifier: Apache-2.0
  *
  * The OpenSearch Contributors require contributions made to
@@ -26,18 +11,15 @@
 
 package org.opensearch.security.sanity.tests;
 
+import org.hamcrest.MatcherAssert;
+import org.junit.Test;
+
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-import org.hamcrest.MatcherAssert;
-import org.junit.Test;
-
-import static org.hamcrest.Matchers.anEmptyMap;
-import static org.hamcrest.Matchers.contains;
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.*;
 
 
 @SuppressWarnings("unchecked")
