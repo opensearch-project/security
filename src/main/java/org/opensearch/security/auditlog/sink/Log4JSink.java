823ca5483dfec19482cb5d03c09b8ecbac34608f
jchipmunk <andrey.pustovetov@gmail.com>
2022-08-05 13:04:31 +0300

    Triple audit logging fix (#1995)
    Revert some changes introduced by https://github.com/opensearch-project/security/pull/1563 to correct work with log4j.
    
    Signed-off-by: Andrey Pustovetov <andrey.pustovetov@gmail.com>

--- src/main/java/org/opensearch/security/auditlog/sink/Log4JSink.java
+++ src/main/java/org/opensearch/security/auditlog/sink/Log4JSink.java
@@ -30,40 +30,17 @@ public final class Log4JSink extends AuditLogSink {
         loggerName = settings.get( settingsPrefix + ".log4j.logger_name","sgaudit");
         auditLogger = LogManager.getLogger(loggerName);
         logLevel = Level.toLevel(settings.get(settingsPrefix + ".log4j.level","INFO").toUpperCase());
-        enabled = isLogLevelEnabled(auditLogger, logLevel);
+        enabled = auditLogger.isEnabled(logLevel);
     }
 
     public boolean isHandlingBackpressure() {
         return !enabled; //no submit to thread pool if not enabled
     }
 
-
     public boolean doStore(final AuditMessage msg) {
         if(enabled) {
-            logAtLevel(auditLogger, logLevel, msg.toJson());
+            auditLogger.log(logLevel, msg.toJson());
         }
         return true;
-    }
-
-    private boolean isLogLevelEnabled(Logger logger, Level level) {
-        boolean isEnabled = false;
-        switch(level.toString()) {
-            case "TRACE": isEnabled = logger.isTraceEnabled();
-            case "DEBUG": isEnabled = logger.isDebugEnabled();
-            case "INFO": isEnabled = logger.isInfoEnabled();
-            case "WARN": isEnabled = logger.isWarnEnabled();
-            case "ERROR": isEnabled = logger.isErrorEnabled();
-        }
-        return isEnabled;
-    }
-
-    private void logAtLevel(Logger logger, Level level, String msg) {
-        switch(level.toString()) {
-            case "TRACE": logger.trace(msg);
-            case "DEBUG": logger.debug(msg);
-            case "INFO": logger.info(msg);
-            case "WARN": logger.warn(msg);
-            case "ERROR": logger.error(msg);
-        }
     }
 }
