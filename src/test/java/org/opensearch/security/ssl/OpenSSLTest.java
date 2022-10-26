221ece12bf7780b46522cd8238f8dd1490902127
Peter Nied <petern@amazon.com>
2022-10-26 16:32:54 -0500

    Fix build break
    Signed-off-by: Peter Nied <petern@amazon.com>

--- src/test/java/org/opensearch/security/ssl/OpenSSLTest.java
+++ src/test/java/org/opensearch/security/ssl/OpenSSLTest.java
@@ -66,7 +66,7 @@ public class OpenSSLTest extends SSLTest {
 
     @Before
     public void setup() {
-        Assume.assumeFalse(PlatformDependant.isWindows());
+        Assume.assumeFalse(PlatformDependent.isWindows());
         allowOpenSSL = true;
     }
 
