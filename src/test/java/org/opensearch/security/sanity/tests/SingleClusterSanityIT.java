4a8971e5d1399a8723d980f0726c924d92850e78
Darshit Chanpura <dchanp@amazon.com>
2022-12-16 17:10:45 -0500

    Backports sanity tests to 1.x
    Signed-off-by: Darshit Chanpura <dchanp@amazon.com>

---
+++ src/test/java/org/opensearch/security/sanity/tests/SingleClusterSanityIT.java
@@ -0,0 +1,51 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ *
+ * Modifications Copyright OpenSearch Contributors. See
+ * GitHub history for details.
+ */
+
+package org.opensearch.security.sanity.tests;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.hamcrest.MatcherAssert;
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.anEmptyMap;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
+
+
+@SuppressWarnings("unchecked")
+public class SingleClusterSanityIT extends SecurityRestTestCase {
+
+    private static final String SECURITY_PLUGIN_NAME = "opensearch-security";
+
+    @Test
+    public void testSecurityPluginInstallation() throws Exception {
+        verifyPluginInstallationOnAllNodes();
+    }
+
+    private void verifyPluginInstallationOnAllNodes() throws Exception {
+
+        Map<String, Map<String, Object>> nodesInCluster = (Map<String, Map<String, Object>>) getAsMapByAdmin("_nodes").get("nodes");
+
+        for (Map<String, Object> node : nodesInCluster.values()) {
+
+            List<Map<String, Object>> plugins = (List<Map<String, Object>>) node.get("plugins");
+            Set<Object> pluginNames = plugins.stream().map(map -> map.get("name")).collect(Collectors.toSet());
+
+            MatcherAssert.assertThat(pluginNames, contains(SECURITY_PLUGIN_NAME));
+        }
+        MatcherAssert.assertThat(nodesInCluster, is(not(anEmptyMap())));
+    }
+}
