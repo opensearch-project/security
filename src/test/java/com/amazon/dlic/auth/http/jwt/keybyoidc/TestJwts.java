e84f471a4673f696c3142baf108032f355744d01
Andrea Pasqualini <anpasq@gmail.com>
2023-03-02 18:22:32 +0100

    Clock skew tolerance for oidc token validation (#2482)
    (cherry picked from commit 9cce399f980e3b8d77619ec24df8a2828207b60d)

--- src/test/java/com/amazon/dlic/auth/http/jwt/keybyoidc/TestJwts.java
+++ src/test/java/com/amazon/dlic/auth/http/jwt/keybyoidc/TestJwts.java
@@ -105,4 +105,15 @@ class TestJwts {
 		return new JoseJwtProducer().processJwt(signedToken, null, signatureProvider);
 	}
 
+	static String createMcCoySignedOct1(long nbf, long exp)
+	{
+		JwtToken jwt_token = create(
+			MCCOY_SUBJECT, TEST_AUDIENCE, 
+			ROLES_CLAIM, TEST_ROLES_STRING, 
+			JwtConstants.CLAIM_NOT_BEFORE, nbf, 
+			JwtConstants.CLAIM_EXPIRY, exp);
+
+		return createSigned(jwt_token, TestJwk.OCT_1);
+	}
+
 }
