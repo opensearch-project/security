34135c64f08c8fada6a3a2b13a561e207091bf4f
Stephen Crawford <steecraw@amazon.com>
2023-03-27 11:02:20 -0400

    Add extensionsManager into GuiceHolder
    Signed-off-by: Stephen Crawford <steecraw@amazon.com>

--- src/main/java/org/opensearch/security/OpenSearchSecurityPlugin.java
+++ src/main/java/org/opensearch/security/OpenSearchSecurityPlugin.java
@@ -93,6 +93,7 @@ import org.opensearch.common.util.concurrent.ThreadContext;
 import org.opensearch.core.xcontent.NamedXContentRegistry;
 import org.opensearch.env.Environment;
 import org.opensearch.env.NodeEnvironment;
+import org.opensearch.extensions.ExtensionsManager;
 import org.opensearch.http.HttpServerTransport;
 import org.opensearch.http.HttpServerTransport.Dispatcher;
 import org.opensearch.index.Index;
@@ -1166,16 +1167,18 @@ public final class OpenSearchSecurityPlugin extends OpenSearchSecuritySSLPlugin 
         private static RepositoriesService repositoriesService;
         private static RemoteClusterService remoteClusterService;
         private static IndicesService indicesService;
-
         private static PitService pitService;
+
+        private static ExtensionsManager extensionsManager;
 
         @Inject
         public GuiceHolder(final RepositoriesService repositoriesService,
-                final TransportService remoteClusterService, IndicesService indicesService, PitService pitService) {
+                final TransportService remoteClusterService, IndicesService indicesService, PitService pitService, ExtensionsManager extensionsManager) {
             GuiceHolder.repositoriesService = repositoriesService;
             GuiceHolder.remoteClusterService = remoteClusterService.getRemoteClusterService();
             GuiceHolder.indicesService = indicesService;
             GuiceHolder.pitService = pitService;
+            GuiceHolder.extensionsManager = extensionsManager;
         }
 
         public static RepositoriesService getRepositoriesService() {
@@ -1191,7 +1194,9 @@ public final class OpenSearchSecurityPlugin extends OpenSearchSecuritySSLPlugin 
         }
 
         public static PitService getPitService() { return pitService; }
-        
+
+        public static ExtensionsManager getExtensionsManager() { return extensionsManager; }
+
         @Override
         public void close() {
         }
