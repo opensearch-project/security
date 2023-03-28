0610af1edfdb83e5b9b08ff8338f9d81c7f3f2b5
Stephen Crawford <steecraw@amazon.com>
2023-03-28 12:58:03 -0400

    add licenses and spotless
    Signed-off-by: Stephen Crawford <steecraw@amazon.com>

--- src/main/java/org/opensearch/security/extensions/ExtensionsService.java
+++ src/main/java/org/opensearch/security/extensions/ExtensionsService.java
@@ -1,9 +1,39 @@
+/*
+ * Copyright 2015-2018 _floragunn_ GmbH
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ *
+ * Modifications Copyright OpenSearch Contributors. See
+ * GitHub history for details.
+ */
+
 package org.opensearch.security.extensions;
+
+import java.io.IOException;
+import java.util.*;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+
 import org.opensearch.ExceptionsHelper;
 import org.opensearch.action.index.IndexRequest;
 import org.opensearch.action.index.IndexResponse;
@@ -25,9 +55,6 @@ import org.opensearch.security.securityconf.impl.SecurityDynamicConfiguration;
 import org.opensearch.security.support.ConfigConstants;
 import org.opensearch.security.support.SecurityJsonNode;
 import org.opensearch.transport.TransportService;
-
-import java.io.IOException;
-import java.util.*;
 
 /**
  * This class handles extension registration and operations on behalf of the Security Plugin.
@@ -166,5 +193,3 @@ public class ExtensionsService {
         return internalUsersConfiguration.exists(extensionUniqueId);
     }
 }
-
-
--- src/test/java/org/opensearch/security/extensions/ExtensionRegistrationUnitTests.java
+++ src/test/java/org/opensearch/security/extensions/ExtensionRegistrationUnitTests.java
@@ -1,8 +1,39 @@
+/*
+ * Copyright 2015-2018 _floragunn_ GmbH
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ *
+ * Modifications Copyright OpenSearch Contributors. See
+ * GitHub history for details.
+ */
+
 package org.opensearch.security.extensions;
 
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.function.Supplier;
+
 import org.junit.Before;
 import org.junit.Test;
+
 import org.opensearch.client.Client;
 import org.opensearch.cluster.metadata.IndexNameExpressionResolver;
 import org.opensearch.cluster.routing.allocation.decider.Decision;
@@ -21,10 +52,6 @@ import org.opensearch.security.support.ConfigConstants;
 import org.opensearch.security.test.SingleClusterTest;
 import org.opensearch.threadpool.ThreadPool;
 import org.opensearch.watcher.ResourceWatcherService;
-
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.function.Supplier;
 
 import static org.junit.Assert.assertEquals;
 
