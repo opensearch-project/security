6a29fe553f616b2b8f79f0e947b5b8fd8ad5b4da
Stephen Crawford <steecraw@amazon.com>
2023-03-28 12:09:59 -0400

    Extension Service Outline
    Signed-off-by: Stephen Crawford <steecraw@amazon.com>

---
+++ src/main/java/org/opensearch/security/extensions/ExtensionsConfigConstants.java
@@ -0,0 +1,6 @@
+package org.opensearch.security.extensions;
+
+public class ExtensionsConfigConstants {
+    public static final String EXTENSIONS_CONFIG_PREFIX = "_extensions_";
+    public static final String EXTENSIONS_CONF_REQUEST_HEADER = EXTENSIONS_CONFIG_PREFIX + "conf_request";
+}
