4742b90234fa7f425af45d7814470ae5ecc4c91f
Ryan Liang <jiallian@amazon.com>
2023-03-28 23:53:19 -0700

    Fix the format
    Signed-off-by: Ryan Liang <jiallian@amazon.com>

--- src/test/java/org/opensearch/security/test/helper/file/FileHelper.java
+++ src/test/java/org/opensearch/security/test/helper/file/FileHelper.java
@@ -47,11 +47,11 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import org.opensearch.common.bytes.BytesReference;
+import org.opensearch.common.xcontent.XContentFactory;
+import org.opensearch.common.xcontent.XContentType;
 import org.opensearch.core.xcontent.NamedXContentRegistry;
 import org.opensearch.core.xcontent.XContentBuilder;
-import org.opensearch.common.xcontent.XContentFactory;
 import org.opensearch.core.xcontent.XContentParser;
-import org.opensearch.common.xcontent.XContentType;
 
 import static org.opensearch.core.xcontent.DeprecationHandler.THROW_UNSUPPORTED_OPERATION;
 
