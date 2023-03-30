1ed509c977da03c67026295aef6c70f720efbbbc
Stephen Crawford <steecraw@amazon.com>
2023-03-30 11:01:21 -0400

    Copy from main
    Signed-off-by: Stephen Crawford <steecraw@amazon.com>

--- src/test/java/org/opensearch/security/ssl/OpenSSLTest.java
+++ src/test/java/org/opensearch/security/ssl/OpenSSLTest.java
@@ -1,10 +1,10 @@
 /*
  * Copyright 2015-2017 floragunn GmbH
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
@@ -12,7 +12,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * 
+ *
  */
 
 package org.opensearch.security.ssl;
@@ -40,6 +40,7 @@ import org.opensearch.node.PluginAwareNode;
 import org.opensearch.security.OpenSearchSecurityPlugin;
 import org.opensearch.security.ssl.util.SSLConfigConstants;
 import org.opensearch.security.support.ConfigConstants;
+import org.opensearch.security.test.AbstractSecurityUnitTest;
 import org.opensearch.security.test.helper.file.FileHelper;
 import org.opensearch.security.test.helper.rest.RestHelper;
 import org.opensearch.transport.Netty4Plugin;
@@ -110,14 +111,14 @@ public class OpenSSLTest extends SSLTest {
         Assume.assumeTrue(OpenSearchSecuritySSLPlugin.OPENSSL_SUPPORTED && OpenSsl.isAvailable());
         super.testNodeClientSSL();
     }
-    
+
     @Override
     @Test
     public void testHttpsOptionalAuth() throws Exception {
         Assume.assumeTrue(OpenSearchSecuritySSLPlugin.OPENSSL_SUPPORTED && OpenSsl.isAvailable());
         super.testHttpsOptionalAuth();
     }
-    
+
     @Test
     public void testAvailCiphersOpenSSL() throws Exception {
         Assume.assumeTrue(OpenSearchSecuritySSLPlugin.OPENSSL_SUPPORTED && OpenSsl.isAvailable());
@@ -138,7 +139,7 @@ public class OpenSSLTest extends SSLTest {
         System.out.println("OpenSSL secure ciphers: " + openSSLSecureCiphers);
         Assert.assertTrue(openSSLSecureCiphers.size() > 0);
     }
-    
+
     @Test
     public void testHttpsEnforceFail() throws Exception {
         Assume.assumeTrue(OpenSearchSecuritySSLPlugin.OPENSSL_SUPPORTED && OpenSsl.isAvailable());
@@ -156,22 +157,22 @@ public class OpenSSLTest extends SSLTest {
         Assume.assumeTrue(OpenSearchSecuritySSLPlugin.OPENSSL_SUPPORTED && OpenSsl.isAvailable());
         super.testHttpsAndNodeSSLFailedCipher();
     }
-    
+
     @Test
     public void testHttpsAndNodeSSLPem() throws Exception {
         Assume.assumeTrue(OpenSearchSecuritySSLPlugin.OPENSSL_SUPPORTED && OpenSsl.isAvailable());
         super.testHttpsAndNodeSSLPKCS8Pem();
     }
-    
+
     @Test
     public void testHttpsAndNodeSSLPemEnc() throws Exception {
         Assume.assumeTrue(OpenSearchSecuritySSLPlugin.OPENSSL_SUPPORTED && OpenSsl.isAvailable());
         super.testHttpsAndNodeSSLPemEnc();
     }
-    
+
     @Test
     public void testNodeClientSSLwithOpenSslTLSv13() throws Exception {
-        
+
         Assume.assumeTrue(OpenSearchSecuritySSLPlugin.OPENSSL_SUPPORTED && OpenSsl.isAvailable() && OpenSsl.version() > 0x10101009L);
 
         final Settings settings = Settings.builder().put("plugins.security.ssl.transport.enabled", true)
@@ -189,7 +190,7 @@ public class OpenSSLTest extends SSLTest {
                 .build();
 
         setupSslOnlyMode(settings);
-        
+
         RestHelper rh = nonSslRestHelper();
 
         final Settings tcSettings = Settings.builder().put("cluster.name", clusterInfo.clustername).put("path.home", "/tmp")
