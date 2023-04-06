cd36a68b7c7c0ac3ee574de431069c63c46947db
Stephen Crawford <steecraw@amazon.com>
2023-04-06 16:35:25 -0400

    Clean service
    Signed-off-by: Stephen Crawford <steecraw@amazon.com>

--- src/main/java/org/opensearch/security/user/UserService.java
+++ src/main/java/org/opensearch/security/user/UserService.java
@@ -122,7 +122,6 @@ public class UserService {
                                               final SecurityDynamicConfiguration<?> configuration) {
         final IndexRequest ir = new IndexRequest(this.securityIndex);
 
-        // final String type = "_doc";
         final String id = cType.toLCString();
 
         configuration.removeStatic();
