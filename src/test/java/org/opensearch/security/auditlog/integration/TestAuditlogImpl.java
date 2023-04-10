521b89919707658c73da0f09035a752f42768750
Stephen Crawford <steecraw@amazon.com>
2023-04-10 12:37:55 -0400

    reset auditlog file
    Signed-off-by: Stephen Crawford <steecraw@amazon.com>

--- src/test/java/org/opensearch/security/auditlog/integration/TestAuditlogImpl.java
+++ src/test/java/org/opensearch/security/auditlog/integration/TestAuditlogImpl.java
@@ -9,154 +9,159 @@
  * GitHub history for details.
  */
 
-package org.opensearch.security;
+package org.opensearch.security.auditlog.integration;
 
-import java.nio.file.Path;
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.function.Supplier;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.stream.Collectors;
 
-import org.junit.Assert;
-import org.junit.Test;
-
-import org.opensearch.OpenSearchSecurityException;
-import org.opensearch.action.admin.indices.create.CreateIndexRequest;
-import org.opensearch.action.admin.indices.create.CreateIndexResponse;
-import org.opensearch.client.Client;
-import org.opensearch.cluster.metadata.IndexNameExpressionResolver;
-import org.opensearch.cluster.service.ClusterService;
-import org.opensearch.common.io.stream.NamedWriteableRegistry;
 import org.opensearch.common.settings.Settings;
-import org.opensearch.core.xcontent.NamedXContentRegistry;
-import org.opensearch.env.Environment;
-import org.opensearch.env.NodeEnvironment;
-import org.opensearch.node.Node;
-import org.opensearch.node.PluginAwareNode;
-import org.opensearch.plugins.ActionPlugin;
-import org.opensearch.plugins.Plugin;
-import org.opensearch.repositories.RepositoriesService;
-import org.opensearch.script.ScriptService;
-import org.opensearch.security.support.ConfigConstants;
-import org.opensearch.security.test.AbstractSecurityUnitTest;
-import org.opensearch.security.test.DynamicSecurityConfig;
-import org.opensearch.security.test.SingleClusterTest;
-import org.opensearch.threadpool.ThreadPool;
-import org.opensearch.transport.Netty4ModulePlugin;
-import org.opensearch.watcher.ResourceWatcherService;
+import org.opensearch.security.auditlog.impl.AuditMessage;
+import org.opensearch.security.auditlog.sink.AuditLogSink;
 
-public class TransportUserInjectorIntegTest extends SingleClusterTest {
+public class TestAuditlogImpl extends AuditLogSink {
 
-    public static class UserInjectorPlugin extends Plugin implements ActionPlugin {
-        Settings settings;
-        public static String injectedUser = null;
+    /** Use the results of `doThenWaitForMessages(...)` instead */
+    @Deprecated
+    public static List<AuditMessage> messages = new ArrayList<AuditMessage>(100);
+    /** Check messages indvidually instead of searching this string */
+    @Deprecated
+    public static StringBuffer sb = new StringBuffer();
+    private static final AtomicReference<CountDownLatch> countDownRef = new AtomicReference<>();
+    private static final AtomicReference<List<AuditMessage>> messagesRef = new AtomicReference<>();
 
-        public UserInjectorPlugin(final Settings settings, final Path configPath) {
-            this.settings = settings;
-        }
-
-        @Override
-        public Collection<Object> createComponents(Client client, ClusterService clusterService, ThreadPool threadPool,
-                                                   ResourceWatcherService resourceWatcherService, ScriptService scriptService,
-                                                   NamedXContentRegistry xContentRegistry, Environment environment,
-                                                   NodeEnvironment nodeEnvironment, NamedWriteableRegistry namedWriteableRegistry,
-                                                   IndexNameExpressionResolver indexNameExpressionResolver,
-                                                   Supplier<RepositoriesService> repositoriesServiceSupplier) {
-            if(injectedUser != null)
-                threadPool.getThreadContext().putTransient(ConfigConstants.OPENDISTRO_SECURITY_INJECTED_USER, injectedUser);
-            return new ArrayList<>();
-        }
+    public TestAuditlogImpl(String name, Settings settings, String settingsPrefix, AuditLogSink fallbackSink) {
+        super(name, settings, null, fallbackSink);
     }
 
-    @Test
-    public void testSecurityUserInjection() throws Exception {
-        final Settings clusterNodeSettings = Settings.builder()
-                .put(ConfigConstants.SECURITY_UNSUPPORTED_INJECT_USER_ENABLED, true)
-                .build();
-        setup(clusterNodeSettings, new DynamicSecurityConfig().setSecurityRolesMapping("roles_transport_inject_user.yml"), Settings.EMPTY);
-        final Settings tcSettings = AbstractSecurityUnitTest.nodeRolesSettings(Settings.builder(), false, false)
-                .put(minimumSecuritySettings(Settings.EMPTY).get(0))
-                .put("cluster.name", clusterInfo.clustername)
-                .put("path.data", "./target/data/" + clusterInfo.clustername + "/cert/data")
-                .put("path.logs", "./target/data/" + clusterInfo.clustername + "/cert/logs")
-                .put("path.home", "./target")
-                .put("node.name", "testclient")
-                .put("discovery.initial_state_timeout", "8s")
-                .put("plugins.security.allow_default_init_securityindex", "true")
-                .put(ConfigConstants.SECURITY_UNSUPPORTED_INJECT_USER_ENABLED, true)
-                .putList("discovery.zen.ping.unicast.hosts", clusterInfo.nodeHost + ":" + clusterInfo.nodePort)
-                .build();
-
-
-        // 1. without user injection
-        try (Node node = new PluginAwareNode(false, tcSettings, Netty4ModulePlugin.class,
-                OpenSearchSecurityPlugin.class, UserInjectorPlugin.class).start()) {
-            waitForInit(node.client());
-            CreateIndexResponse cir = node.client().admin().indices().create(new CreateIndexRequest("captain-logs-1")).actionGet();
-            Assert.assertTrue(cir.isAcknowledged());
+    public synchronized boolean doStore(AuditMessage msg) {
+        if (messagesRef.get() == null || countDownRef.get() == null) {
+            // Ignore any messages that are sent before TestAuditlogImpl is waiting.
+            return true;
         }
-
-
-        // 2. with invalid backend roles
-        UserInjectorPlugin.injectedUser = "ttt|kkk";
-        Exception exception = null;
-        try (Node node = new PluginAwareNode(false, tcSettings, Netty4ModulePlugin.class,
-                OpenSearchSecurityPlugin.class, UserInjectorPlugin.class).start()) {
-            waitForInit(node.client());
-            CreateIndexResponse cir = node.client().admin().indices().create(new CreateIndexRequest("captain-logs-2")).actionGet();
-            Assert.fail("Expecting exception");
-        } catch (OpenSearchSecurityException ex) {
-            exception = ex;
-            log.warn(ex.toString());
-            Assert.assertNotNull(exception);
-            Assert.assertTrue(exception.getMessage().contains("indices:admin/create"));
-        }
-
-        // 3. with valid backend roles for injected user
-        UserInjectorPlugin.injectedUser = "injectedadmin|injecttest";
-        try (Node node = new PluginAwareNode(false, tcSettings, Netty4ModulePlugin.class,
-                OpenSearchSecurityPlugin.class, UserInjectorPlugin.class).start()) {
-            waitForInit(node.client());
-            CreateIndexResponse cir = node.client().admin().indices().create(new CreateIndexRequest("captain-logs-2")).actionGet();
-            Assert.assertTrue(cir.isAcknowledged());
-        }
+        sb.append(msg.toPrettyString()+System.lineSeparator());
+        messagesRef.get().add(msg);
+        countDownRef.get().countDown();
+        return true;
     }
 
-    @Test
-    public void testSecurityUserInjectionWithConfigDisabled() throws Exception {
-        final Settings clusterNodeSettings = Settings.builder()
-                .put(ConfigConstants.SECURITY_UNSUPPORTED_INJECT_USER_ENABLED, false)
-                .build();
-        setup(clusterNodeSettings, new DynamicSecurityConfig().setSecurityRolesMapping("roles_transport_inject_user.yml"), Settings.EMPTY);
-        final Settings tcSettings = AbstractSecurityUnitTest.nodeRolesSettings(Settings.builder(), false, false)
-                .put(minimumSecuritySettings(Settings.EMPTY).get(0))
-                .put("cluster.name", clusterInfo.clustername)
-                .put("path.data", "./target/data/" + clusterInfo.clustername + "/cert/data")
-                .put("path.logs", "./target/data/" + clusterInfo.clustername + "/cert/logs")
-                .put("path.home", "./target")
-                .put("node.name", "testclient")
-                .put("discovery.initial_state_timeout", "8s")
-                .put("plugins.security.allow_default_init_securityindex", "true")
-                .put(ConfigConstants.SECURITY_UNSUPPORTED_INJECT_USER_ENABLED, false)
-                .putList("discovery.zen.ping.unicast.hosts", clusterInfo.nodeHost + ":" + clusterInfo.nodePort)
-                .build();
+    /** Unneeded after switching to `doThenWaitForMessages(...)` as data is automatically flushed */
+    @Deprecated
+    public static synchronized void clear() {
+        doThenWaitForMessages(() -> {}, 0);
+    }
 
-        // 1. without user injection
-        try (Node node = new PluginAwareNode(false, tcSettings, Netty4ModulePlugin.class,
-                OpenSearchSecurityPlugin.class, UserInjectorPlugin.class).start()) {
-            waitForInit(node.client());
-            CreateIndexResponse cir = node.client().admin().indices().create(new CreateIndexRequest("captain-logs-1")).actionGet();
-            Assert.assertTrue(cir.isAcknowledged());
+    /**
+     * Perform an action and then wait until the expected number of messages have been found.
+     */
+    public static List<AuditMessage> doThenWaitForMessages(final Runnable action, final int expectedCount) {
+        final List<AuditMessage> missedMessages = new ArrayList<>();
+        final List<AuditMessage> messages = new ArrayList<>();
+        final CountDownLatch latch = resetAuditStorage(expectedCount, messages);
+
+        try {
+            action.run();
+            final int maxSecondsToWaitForMessages = 1;
+            boolean foundAll = false;
+            foundAll = latch.await(maxSecondsToWaitForMessages, TimeUnit.SECONDS);
+            // After the wait has prevent any new messages from being recieved
+            resetAuditStorage(0, missedMessages);
+            if (!foundAll || messages.size() != expectedCount) {
+                throw new MessagesNotFoundException(expectedCount, messages);
+            }
+        } catch (final InterruptedException e) {
+            throw new RuntimeException("Unexpected exception", e);
         }
 
-        // with invalid backend roles
-        UserInjectorPlugin.injectedUser = "ttt|kkk";
-        try (Node node = new PluginAwareNode(false, tcSettings, Netty4ModulePlugin.class,
-                OpenSearchSecurityPlugin.class, UserInjectorPlugin.class).start()) {
-            waitForInit(node.client());
-            CreateIndexResponse cir = node.client().admin().indices().create(new CreateIndexRequest("captain-logs-2")).actionGet();
-            // Should pass as the user injection is disabled
-            Assert.assertTrue(cir.isAcknowledged());
+        // Do not check for missed messages if no messages were expected
+        if (expectedCount != 0) {
+            try {
+                Thread.sleep(100);
+                if (missedMessages.size() != 0) {
+                    final String missedMessagesErrorMessage = new StringBuilder()
+                    .append("Audit messages were missed! ")
+                    .append("Found " + (missedMessages.size()) + " messages.")
+                    .append("Messages found during this time: \n\n")
+                    .append(missedMessages.stream()
+                        .map(AuditMessage::toString)
+                        .collect(Collectors.joining("\n")))
+                    .toString();
+
+                    throw new RuntimeException(missedMessagesErrorMessage);
+                }
+            } catch (final Exception e) {
+                throw new RuntimeException("Unexpected exception", e);
+            }
         }
 
+        // Next usage of this class might be using raw stringbuilder / list so reset before that test might run
+        resetAuditStorage(0, new ArrayList<>());
+        return new ArrayList<>(messages);
+    }
+
+    /**
+     * Resets all of the mechanics for fresh messages to be captured
+     *
+     * @param expectedMessageCount The number of messages before the latch is signalled, indicating all messages have been recieved
+     * @param messages Where messages will be stored after being recieved
+     */
+    private static CountDownLatch resetAuditStorage(int expectedMessageCount, List<AuditMessage> messages) {
+        final CountDownLatch latch = new CountDownLatch(expectedMessageCount);
+        countDownRef.set(latch);
+        messagesRef.set(messages);
+
+        TestAuditlogImpl.sb = new StringBuffer();
+        TestAuditlogImpl.messages = messages;
+        return latch;
+    }
+
+    /**
+     * Perform an action and then wait until a single message has been found.
+     */
+    public static AuditMessage doThenWaitForMessage(final Runnable action) {
+        return doThenWaitForMessages(action, 1).get(0);
+    }
+
+    @Override
+    public boolean isHandlingBackpressure() {
+        return true;
+    }
+
+    public static class MessagesNotFoundException extends RuntimeException {
+        private final int expectedCount;
+        private final int missingCount;
+        private final List<AuditMessage> foundMessages;
+        public MessagesNotFoundException(final int expectedCount, List<AuditMessage> foundMessages) {
+            super(MessagesNotFoundException.createDetailMessage(expectedCount, foundMessages));
+            this.expectedCount = expectedCount;
+            this.missingCount = expectedCount - foundMessages.size();
+            this.foundMessages = foundMessages;
+        }
+
+        public int getExpectedCount() {
+            return expectedCount;
+        }
+
+        public int getMissingCount() {
+            return missingCount;
+        }
+
+        public List<AuditMessage> getFoundMessages() {
+            return foundMessages;
+        }
+
+        private static String createDetailMessage(final int expectedCount, final List<AuditMessage> foundMessages) {
+            return new StringBuilder()
+                .append("Did not receive all " + expectedCount + " audit messages after a short wait. ")
+                .append("Missing " + (expectedCount - foundMessages.size()) + " messages.")
+                .append("Messages found during this time: \n\n")
+                .append(foundMessages.stream()
+                    .map(AuditMessage::toString)
+                    .collect(Collectors.joining("\n")))
+                .toString();
+        }
     }
 }
