e7a8d50e453b472d683d67c92acfe639cf972456
scosta <samuel.costa@eliatra.com>
2023-05-09 00:55:57 +0100

    #2595 spotless apply
    Signed-off-by: scosta <samuel.costa@eliatra.com>

--- src/main/java/org/opensearch/security/dlic/rest/api/ExtensionRegistrationApiAction.java
+++ src/main/java/org/opensearch/security/dlic/rest/api/ExtensionRegistrationApiAction.java
@@ -162,4 +162,3 @@ public class ExtensionRegistrationApiAction extends AbstractApiAction {
         return new ExtensionRegistrationValidator(request, isSuperAdmin(), ref, this.settings, params);
     }
 }
-
