9515181f1c41a6e6c7fd1e3cc24f0868e4315062
Ryan Liang <jiallian@amazon.com>
2023-05-10 09:51:26 -0700

    Fix the conflicts
    Signed-off-by: Ryan Liang <jiallian@amazon.com>

--- src/integrationTest/java/org/opensearch/test/framework/matcher/GetSettingsResponseContainsIndicesMatcher.java
+++ src/integrationTest/java/org/opensearch/test/framework/matcher/GetSettingsResponseContainsIndicesMatcher.java
@@ -38,11 +38,7 @@ class GetSettingsResponseContainsIndicesMatcher extends TypeSafeDiagnosingMatche
 			if (!indexToSettings.containsKey(index)) {
 				mismatchDescription
 						.appendText("Response contains settings of indices: ")
-<<<<<<< HEAD
 						.appendValue(indexToSettings.keySet().toArray());
-=======
-						.appendValue(indexToSettings.keySet());
->>>>>>> main
 				return false;
 			}
 		}
