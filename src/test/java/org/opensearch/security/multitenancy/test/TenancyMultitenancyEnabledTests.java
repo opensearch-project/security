f41fb9482c94816c03598997fdc72849cd1235d9
Stephen Crawford <steecraw@amazon.com>
2023-06-01 10:40:45 -0400

    Half sec test changes
    Signed-off-by: Stephen Crawford <steecraw@amazon.com>

--- src/test/java/org/opensearch/security/multitenancy/test/TenancyMultitenancyEnabledTests.java
+++ src/test/java/org/opensearch/security/multitenancy/test/TenancyMultitenancyEnabledTests.java
@@ -31,12 +31,14 @@ public class TenancyMultitenancyEnabledTests extends SingleClusterTest {
     private static final Header ON_USER_TENANT = new BasicHeader("securitytenant", "__user__");
 
     private static String createIndexPatternDoc(final String title) {
-        return "{"+
-        "\"type\" : \"index-pattern\","+
-        "\"updated_at\" : \"2018-09-29T08:56:59.066Z\","+
-        "\"index-pattern\" : {"+
-            "\"title\" : \"" + title + "\""+
-            "}}";
+        return "{"
+            + "\"type\" : \"index-pattern\","
+            + "\"updated_at\" : \"2018-09-29T08:56:59.066Z\","
+            + "\"index-pattern\" : {"
+            + "\"title\" : \""
+            + title
+            + "\""
+            + "}}";
     }
 
     @Override
@@ -46,37 +48,69 @@ public class TenancyMultitenancyEnabledTests extends SingleClusterTest {
 
     @Test
     public void testMultitenancyDisabled_endToEndTest() throws Exception {
-        setup(Settings.EMPTY,
-                new DynamicSecurityConfig(),
-                Settings.builder().put("plugins.security.restapi.roles_enabled.0", "security_rest_api_access").build(),
-                true);
+        setup(
+            Settings.EMPTY,
+            new DynamicSecurityConfig(),
+            Settings.builder().put("plugins.security.restapi.roles_enabled.0", "security_rest_api_access").build(),
+            true
+        );
 
-        final HttpResponse getSettingResponse = nonSslRestHelper().executeGetRequest("/_plugins/_security/api/tenancy/config", AS_REST_API_USER);
+        final HttpResponse getSettingResponse = nonSslRestHelper().executeGetRequest(
+            "/_plugins/_security/api/tenancy/config",
+            AS_REST_API_USER
+        );
         assertThat(getSettingResponse.getStatusCode(), equalTo(HttpStatus.SC_OK));
         assertThat(getSettingResponse.findValueInJson("multitenancy_enabled"), equalTo("true"));
 
         HttpResponse getDashboardsinfoResponse = nonSslRestHelper().executeGetRequest("/_plugins/_security/dashboardsinfo", AS_USER);
-        assertThat(getDashboardsinfoResponse.findValueInJson("multitenancy_enabled"),equalTo("true"));
+        assertThat(getDashboardsinfoResponse.findValueInJson("multitenancy_enabled"), equalTo("true"));
 
-        final HttpResponse createDocInGlobalTenantResponse = nonSslRestHelper().executePostRequest(".kibana/_doc?refresh=true", createIndexPatternDoc("globalIndex"), AS_USER);
+        final HttpResponse createDocInGlobalTenantResponse = nonSslRestHelper().executePostRequest(
+            ".kibana/_doc?refresh=true",
+            createIndexPatternDoc("globalIndex"),
+            AS_USER
+        );
         assertThat(createDocInGlobalTenantResponse.getStatusCode(), equalTo(HttpStatus.SC_CREATED));
-        final HttpResponse createDocInUserTenantResponse = nonSslRestHelper().executePostRequest(".kibana/_doc?refresh=true", createIndexPatternDoc("userIndex"), ON_USER_TENANT, AS_USER);
+        final HttpResponse createDocInUserTenantResponse = nonSslRestHelper().executePostRequest(
+            ".kibana/_doc?refresh=true",
+            createIndexPatternDoc("userIndex"),
+            ON_USER_TENANT,
+            AS_USER
+        );
         assertThat(createDocInUserTenantResponse.getStatusCode(), equalTo(HttpStatus.SC_CREATED));
 
-        final HttpResponse searchInUserTenantWithMutlitenancyEnabled = nonSslRestHelper().executeGetRequest(".kibana/_search", ON_USER_TENANT, AS_USER);
+        final HttpResponse searchInUserTenantWithMutlitenancyEnabled = nonSslRestHelper().executeGetRequest(
+            ".kibana/_search",
+            ON_USER_TENANT,
+            AS_USER
+        );
         assertThat(searchInUserTenantWithMutlitenancyEnabled.getStatusCode(), equalTo(HttpStatus.SC_OK));
-        assertThat(searchInUserTenantWithMutlitenancyEnabled.findValueInJson("hits.hits[0]._source.index-pattern.title"), equalTo("userIndex"));
+        assertThat(
+            searchInUserTenantWithMutlitenancyEnabled.findValueInJson("hits.hits[0]._source.index-pattern.title"),
+            equalTo("userIndex")
+        );
 
-        final HttpResponse updateMutlitenancyToDisabled = nonSslRestHelper().executePutRequest("/_plugins/_security/api/tenancy/config", "{\"multitenancy_enabled\": \"false\"}", AS_REST_API_USER);
+        final HttpResponse updateMutlitenancyToDisabled = nonSslRestHelper().executePutRequest(
+            "/_plugins/_security/api/tenancy/config",
+            "{\"multitenancy_enabled\": \"false\"}",
+            AS_REST_API_USER
+        );
         assertThat(updateMutlitenancyToDisabled.getStatusCode(), equalTo(HttpStatus.SC_OK));
         assertThat(updateMutlitenancyToDisabled.findValueInJson("multitenancy_enabled"), equalTo("false"));
 
         getDashboardsinfoResponse = nonSslRestHelper().executeGetRequest("/_plugins/_security/dashboardsinfo", AS_USER);
-        assertThat(getDashboardsinfoResponse.findValueInJson("multitenancy_enabled"),equalTo("false"));
+        assertThat(getDashboardsinfoResponse.findValueInJson("multitenancy_enabled"), equalTo("false"));
 
-        final HttpResponse searchInUserTenantWithMutlitenancyDisabled = nonSslRestHelper().executeGetRequest(".kibana/_search", ON_USER_TENANT, AS_USER);
+        final HttpResponse searchInUserTenantWithMutlitenancyDisabled = nonSslRestHelper().executeGetRequest(
+            ".kibana/_search",
+            ON_USER_TENANT,
+            AS_USER
+        );
         assertThat(searchInUserTenantWithMutlitenancyDisabled.getStatusCode(), equalTo(HttpStatus.SC_OK));
-        assertThat(searchInUserTenantWithMutlitenancyDisabled.findValueInJson("hits.hits[0]._source.index-pattern.title"), equalTo("globalIndex"));
+        assertThat(
+            searchInUserTenantWithMutlitenancyDisabled.findValueInJson("hits.hits[0]._source.index-pattern.title"),
+            equalTo("globalIndex")
+        );
     }
 
 }
