f8ef61648a3b91bc6a545a941cad74cc522be1f9
Sam <128482925+samuelcostae@users.noreply.github.com>
2023-06-16 12:14:11 +0100

    Apply suggestions from code review
    Typos
    
    Co-authored-by: Stephen Crawford <65832608+scrawfor99@users.noreply.github.com>
    Signed-off-by: Sam <128482925+samuelcostae@users.noreply.github.com>

--- src/main/java/org/opensearch/security/privileges/SecurityIndexAccessEvaluator.java
+++ src/main/java/org/opensearch/security/privileges/SecurityIndexAccessEvaluator.java
@@ -117,7 +117,7 @@ public class SecurityIndexAccessEvaluator {
                 if (filterSecurityIndex) {
                     irr.replace(request, false, "*", "-" + securityIndex);
                     if (isDebugEnabled) {
-                        log.debug("Filtered '{}'from {}, resulting list with *,-{} is {}", securityIndex, requestedResolved, securityIndex,
+                        log.debug("Filtered '{}' from {}, resulting list with *,-{} is {}", securityIndex, requestedResolved, securityIndex,
                                 irr.resolveRequest(request));
                     }
                     return presponse;
