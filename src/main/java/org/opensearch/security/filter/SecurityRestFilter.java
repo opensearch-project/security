c443b7aae995eb8a38d79380c59b9ee6eb3cd4fd
Ryan Liang <jiallian@amazon.com>
2023-07-14 11:21:17 -0700

    Fix the suffix of OBO endpoint
    Signed-off-by: Ryan Liang <jiallian@amazon.com>

--- src/main/java/org/opensearch/security/filter/SecurityRestFilter.java
+++ src/main/java/org/opensearch/security/filter/SecurityRestFilter.java
@@ -89,6 +89,8 @@ public class SecurityRestFilter {
     private static final String HEALTH_SUFFIX = "health";
     private static final String WHO_AM_I_SUFFIX = "whoami";
 
+    private static final String ON_BEHALF_OF_SUFFIX = "onbehalfof";
+
     private static final String REGEX_PATH_PREFIX = "/(" + LEGACY_OPENDISTRO_PREFIX + "|" + PLUGINS_PREFIX + ")/" + "(.*)";
     private static final Pattern PATTERN_PATH_PREFIX = Pattern.compile(REGEX_PATH_PREFIX);
 
@@ -260,8 +262,7 @@ public class SecurityRestFilter {
         }
 
         if (HTTPHelper.containsOBOToken(request)) {
-            String OBO_ENDPOINT_PREFIX = "_plugins/_security/api/user/onbehalfof";
-            if (request.method() == Method.POST && OBO_ENDPOINT_PREFIX.equals(suffix)) {
+            if (request.method() == Method.POST && ON_BEHALF_OF_SUFFIX.equals(suffix)) {
                 final OpenSearchException exception = ExceptionUtils.invalidUsageOfOBOTokenException();
                 log.error(exception.toString());
                 auditLog.logBadHeaders(request);
