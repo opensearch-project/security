378d4f1b90d8f2c7fc600ba301c77cfd922922c4
Peter Nied <petern@amazon.com>
2023-08-08 04:08:48 +0000

    Remove log spam for files that are cleaned up
    Signed-off-by: Peter Nied <petern@amazon.com>

--- src/test/java/org/opensearch/security/test/helper/cluster/ClusterHelper.java
+++ src/test/java/org/opensearch/security/test/helper/cluster/ClusterHelper.java
@@ -319,14 +319,12 @@ public final class ClusterHelper {
             Files.walkFileTree(testsDataDir.toPath(), new SimpleFileVisitor<>() {
                 @Override
                 public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-                    System.out.println("Deleting file " + file.getFileName());
                     Files.delete(file);
                     return FileVisitResult.CONTINUE;
                 }
 
                 @Override
                 public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
-                    System.out.println("Deleting directory " + dir.getFileName());
                     Files.delete(dir);
                     return FileVisitResult.CONTINUE;
                 }
