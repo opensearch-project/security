55ba25fdeb2d0602eaa429371281aef95440966a
Ryan Liang <jiallian@amazon.com>
2023-08-15 11:44:13 -0700

    React to changes in ActionListener in core
    Signed-off-by: Ryan Liang <jiallian@amazon.com>

--- src/main/java/org/opensearch/security/configuration/ConfigurationLoaderSecurity7.java
+++ src/main/java/org/opensearch/security/configuration/ConfigurationLoaderSecurity7.java
@@ -41,7 +41,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import org.opensearch.LegacyESVersion;
-import org.opensearch.action.ActionListener;
+import org.opensearch.core.action.ActionListener;
 import org.opensearch.action.get.GetResponse;
 import org.opensearch.action.get.MultiGetItemResponse;
 import org.opensearch.action.get.MultiGetRequest;
