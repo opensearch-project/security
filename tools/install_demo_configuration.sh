5dfbf9f12f8e9658546fa0635ae2ad7acddc2051
Stephen Crawford <steecraw@amazon.com>
2023-09-20 15:43:45 -0400

    try env var
    Signed-off-by: Stephen Crawford <steecraw@amazon.com>

--- tools/install_demo_configuration.sh
+++ tools/install_demo_configuration.sh
@@ -391,6 +391,7 @@ echo 'plugins.security.system_indices.enabled: true' | $SUDO_CMD tee -a "$OPENSE
 echo 'plugins.security.system_indices.indices: [".plugins-ml-config", ".plugins-ml-connector", ".plugins-ml-model-group", ".plugins-ml-model", ".plugins-ml-task", ".plugins-ml-conversation-meta", ".plugins-ml-conversation-interactions", ".opendistro-alerting-config", ".opendistro-alerting-alert*", ".opendistro-anomaly-results*", ".opendistro-anomaly-detector*", ".opendistro-anomaly-checkpoints", ".opendistro-anomaly-detection-state", ".opendistro-reports-*", ".opensearch-notifications-*", ".opensearch-notebooks", ".opensearch-observability", ".ql-datasources", ".opendistro-asynchronous-search-response*", ".replication-metadata-store", ".opensearch-knn-models", ".geospatial-ip2geo-data*", ".opendistro-job-scheduler-lock"]' | $SUDO_CMD tee -a "$OPENSEARCH_CONF_FILE" > /dev/null
 
 # Read the admin password from the file or use the initialAdminPassword if set
+echo "Content of security config dir is:  $(ls $OPENSEARCH_CONF_DIR/opensearch-security/)
 echo "HEAD of password file is: $(head $OPENSEARCH_CONF_DIR/opensearch-security/initialAdminPassword.txt)"
 
 
