1261c3faf0981ac34b426b4b393eca9c250b9567
Stephen Crawford <steecraw@amazon.com>
2023-09-20 14:06:16 -0400

    Print dir
    Signed-off-by: Stephen Crawford <steecraw@amazon.com>

--- tools/install_demo_configuration.sh
+++ tools/install_demo_configuration.sh
@@ -391,6 +391,9 @@ echo 'plugins.security.system_indices.enabled: true' | $SUDO_CMD tee -a "$OPENSE
 echo 'plugins.security.system_indices.indices: [".plugins-ml-config", ".plugins-ml-connector", ".plugins-ml-model-group", ".plugins-ml-model", ".plugins-ml-task", ".plugins-ml-conversation-meta", ".plugins-ml-conversation-interactions", ".opendistro-alerting-config", ".opendistro-alerting-alert*", ".opendistro-anomaly-results*", ".opendistro-anomaly-detector*", ".opendistro-anomaly-checkpoints", ".opendistro-anomaly-detection-state", ".opendistro-reports-*", ".opensearch-notifications-*", ".opensearch-notebooks", ".opensearch-observability", ".ql-datasources", ".opendistro-asynchronous-search-response*", ".replication-metadata-store", ".opensearch-knn-models", ".geospatial-ip2geo-data*", ".opendistro-job-scheduler-lock"]' | $SUDO_CMD tee -a "$OPENSEARCH_CONF_FILE" > /dev/null
 
 # Read the admin password from the file or use the initialAdminPassword if set
+echo "Security config dir has"
+ls $OPENSEARCH_PLUGINS_DIR/security/config
+
 ADMIN_PASSWORD=$(head -n 1 "$ADMIN_PASSWORD_FILE")
 
 if [ -z "$ADMIN_PASSWORD" ]; then
