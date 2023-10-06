4bafead3cd4b20e5012a5b57c26c2f17a1f9479f
Andrey Pleskach <ples@aiven.io>
2023-10-06 15:22:37 +0300

    Add missing license header (#3467)
    Added missing license header for some files.
    
    - [ ] New functionality includes testing
    - [ ] New functionality has been documented
    - [ ] Commits are signed per the DCO using --signoff
    
    By submitting this pull request, I confirm that my contribution is made
    under the terms of the Apache 2.0 license.
    For more information on following Developer Certificate of Origin and
    signing off your commits, please check
    [here](https://github.com/opensearch-project/OpenSearch/blob/main/CONTRIBUTING.md#developer-certificate-of-origin).
    
    Signed-off-by: Andrey Pleskach <ples@aiven.io>
    (cherry picked from commit fb82bbbfcc133fa4ae0bba227c881f7a6ebbe7bd)

--- src/test/java/org/opensearch/security/dlic/rest/api/AccountApiActionConfigValidationsTest.java
+++ src/test/java/org/opensearch/security/dlic/rest/api/AccountApiActionConfigValidationsTest.java
@@ -1,12 +1,12 @@
 /*
-* Copyright OpenSearch Contributors
-* SPDX-License-Identifier: Apache-2.0
-*
-* The OpenSearch Contributors require contributions made to
-* this file be licensed under the Apache-2.0 license or a
-* compatible open source license.
-*
-*/
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ *
+ */
 package org.opensearch.security.dlic.rest.api;
 
 import com.fasterxml.jackson.databind.node.ObjectNode;
