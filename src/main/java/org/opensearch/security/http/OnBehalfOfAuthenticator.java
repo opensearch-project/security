3cbc910b0fc73648abfec28e87f7a0f6193d8234
Ryan Liang <jiallian@amazon.com>
2023-10-19 14:58:43 -0700

    Add doPrivileged change
    Signed-off-by: Ryan Liang <jiallian@amazon.com>

--- src/main/java/org/opensearch/security/http/OnBehalfOfAuthenticator.java
+++ src/main/java/org/opensearch/security/http/OnBehalfOfAuthenticator.java
@@ -67,8 +67,18 @@ public class OnBehalfOfAuthenticator implements HTTPAuthenticator {
         String oboEnabledSetting = settings.get("enabled", "true");
         oboEnabled = Boolean.parseBoolean(oboEnabledSetting);
         encryptionKey = settings.get("encryption_key");
-        JwtParserBuilder builder = initParserBuilder(settings.get("signing_key"));
-        jwtParser = builder.build();
+
+        final SecurityManager sm = System.getSecurityManager();
+        if (sm != null) {
+            sm.checkPermission(new SpecialPermission());
+        }
+        jwtParser = AccessController.doPrivileged(new PrivilegedAction<JwtParser>() {
+            @Override
+            public JwtParser run() {
+                JwtParserBuilder builder = initParserBuilder(settings.get("signing_key"));
+                return builder.build();
+            }
+        });
 
         this.clusterName = clusterName;
         this.encryptionUtil = new EncryptionDecryptionUtil(encryptionKey);
