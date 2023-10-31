515e55941d933105a086d9ba03ec5a155eac60b3
Peter Nied <petern@amazon.com>
2023-10-31 09:07:26 -0400

    [Backport 2.x][Refactor] Improve asynchronous test cases format (#3601) (#3630)
    ### Description
    Signed-off-by: Peter Nied <petern@amazon.com>
    (cherry picked from commit 1ad8cf480cd31d2fd10fdb16e974816cb64fe0a8)

--- src/integrationTest/java/org/opensearch/security/ResourceFocusedTests.java
+++ src/integrationTest/java/org/opensearch/security/ResourceFocusedTests.java
@@ -16,31 +16,27 @@ import static org.opensearch.test.framework.TestSecurityConfig.Role.ALL_ACCESS;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.lang.management.GarbageCollectorMXBean;
-import java.lang.management.ManagementFactory;
-import java.lang.management.MemoryPoolMXBean;
-import java.lang.management.MemoryUsage;
 import java.nio.charset.StandardCharsets;
-import java.util.List;
 import java.util.Map;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.TimeUnit;
-import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.zip.GZIPOutputStream;
 
+import org.apache.http.HttpStatus;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.ByteArrayEntity;
 import org.apache.http.entity.ContentType;
 import org.apache.http.message.BasicHeader;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.BeforeClass;
 import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.opensearch.action.index.IndexRequest;
 import org.opensearch.client.Client;
+import org.opensearch.test.framework.AsyncActions;
 import org.opensearch.test.framework.TestSecurityConfig;
 import org.opensearch.test.framework.TestSecurityConfig.User;
 import org.opensearch.test.framework.cluster.ClusterManager;
@@ -52,6 +48,7 @@ import com.carrotsearch.randomizedtesting.annotations.ThreadLeakScope;
 @RunWith(com.carrotsearch.randomizedtesting.RandomizedRunner.class)
 @ThreadLeakScope(ThreadLeakScope.Scope.NONE)
 public class ResourceFocusedTests {
+    private final static Logger LOG = LogManager.getLogger(AsyncActions.class);
     private static final User ADMIN_USER = new User("admin").roles(ALL_ACCESS);
     private static final User LIMITED_USER = new User("limited_user").roles(
         new TestSecurityConfig.Role("limited-role").clusterPermissions(
@@ -93,9 +90,8 @@ public class ResourceFocusedTests {
         final String requestPath = "/*/_search";
         final int parrallelism = 5;
         final int totalNumberOfRequests = 100;
-        final boolean statsPrinter = false;
 
-        runResourceTest(size, requestPath, parrallelism, totalNumberOfRequests, statsPrinter);
+        runResourceTest(size, requestPath, parrallelism, totalNumberOfRequests);
     }
 
     @Test
@@ -105,9 +101,8 @@ public class ResourceFocusedTests {
         final String requestPath = "/*/_search";
         final int parrallelism = 20;
         final int totalNumberOfRequests = 10_000;
-        final boolean statsPrinter = false;
 
-        runResourceTest(size, requestPath, parrallelism, totalNumberOfRequests, statsPrinter);
+        runResourceTest(size, requestPath, parrallelism, totalNumberOfRequests);
     }
 
     @Test
@@ -116,62 +111,27 @@ public class ResourceFocusedTests {
         final RequestBodySize size = RequestBodySize.Small;
         final String requestPath = "/*/_search";
         final int parrallelism = 100;
-        final int totalNumberOfRequests = 1_000_000;
-        final boolean statsPrinter = false;
+        final int totalNumberOfRequests = 15_000;
 
-        runResourceTest(size, requestPath, parrallelism, totalNumberOfRequests, statsPrinter);
+        runResourceTest(size, requestPath, parrallelism, totalNumberOfRequests);
     }
 
-    private Long runResourceTest(
+    private void runResourceTest(
         final RequestBodySize size,
         final String requestPath,
         final int parrallelism,
-        final int totalNumberOfRequests,
-        final boolean statsPrinter
+        final int totalNumberOfRequests
     ) {
         final byte[] compressedRequestBody = createCompressedRequestBody(size);
         try (final TestRestClient client = cluster.getRestClient(new BasicHeader("Content-Encoding", "gzip"))) {
+            final var requests = AsyncActions.generate(() -> {
+                final HttpPost post = new HttpPost(client.getHttpServerUri() + requestPath);
+                post.setEntity(new ByteArrayEntity(compressedRequestBody, ContentType.APPLICATION_JSON));
+                return client.executeRequest(post);
+            }, parrallelism, totalNumberOfRequests);
 
-            if (statsPrinter) {
-                printStats();
-            }
-            final HttpPost post = new HttpPost(client.getHttpServerUri() + requestPath);
-            post.setEntity(new ByteArrayEntity(compressedRequestBody, ContentType.APPLICATION_JSON));
-
-            final ForkJoinPool forkJoinPool = new ForkJoinPool(parrallelism);
-
-            final List<CompletableFuture<Void>> waitingOn = IntStream.rangeClosed(1, totalNumberOfRequests)
-                .boxed()
-                .map(i -> CompletableFuture.runAsync(() -> client.executeRequest(post), forkJoinPool))
-                .collect(Collectors.toList());
-            Supplier<Long> getCount = () -> waitingOn.stream().filter(cf -> cf.isDone() && !cf.isCompletedExceptionally()).count();
-
-            CompletableFuture<Void> statPrinter = statsPrinter ? CompletableFuture.runAsync(() -> {
-                while (true) {
-                    printStats();
-                    System.err.println(" & Succesful completions: " + getCount.get());
-                    try {
-                        Thread.sleep(500);
-                    } catch (Exception e) {
-                        break;
-                    }
-                }
-            }, forkJoinPool) : CompletableFuture.completedFuture(null);
-
-            final CompletableFuture<Void> allOfThem = CompletableFuture.allOf(waitingOn.toArray(new CompletableFuture[0]));
-
-            try {
-                allOfThem.get(30, TimeUnit.SECONDS);
-                statPrinter.cancel(true);
-            } catch (final Exception e) {
-                // Ignored
-            }
-
-            if (statsPrinter) {
-                printStats();
-                System.err.println(" & Succesful completions: " + getCount.get());
-            }
-            return getCount.get();
+            AsyncActions.getAll(requests, 2, TimeUnit.MINUTES)
+                .forEach((response) -> { response.assertStatusCode(HttpStatus.SC_UNAUTHORIZED); });
         }
     }
 
@@ -217,51 +177,17 @@ public class ResourceFocusedTests {
             gzipOutputStream.finish();
 
             final byte[] compressedRequestBody = byteArrayOutputStream.toByteArray();
-            System.err.println(
-                "^^^"
-                    + String.format(
-                        "Original size was %,d bytes, compressed to %,d bytes, ratio %,.2f",
-                        uncompressedBytesSize,
-                        compressedRequestBody.length,
-                        ((double) uncompressedBytesSize / compressedRequestBody.length)
-                    )
+            LOG.info(
+                String.format(
+                    "Original size was %,d bytes, compressed to %,d bytes, ratio %,.2f",
+                    uncompressedBytesSize,
+                    compressedRequestBody.length,
+                    ((double) uncompressedBytesSize / compressedRequestBody.length)
+                )
             );
             return compressedRequestBody;
         } catch (final IOException ioe) {
             throw new RuntimeException(ioe);
         }
     }
-
-    private void printStats() {
-        System.err.println("** Stats ");
-        printMemory();
-        printMemoryPools();
-        printGCPools();
-    }
-
-    private void printMemory() {
-        final Runtime runtime = Runtime.getRuntime();
-
-        final long totalMemory = runtime.totalMemory(); // Total allocated memory
-        final long freeMemory = runtime.freeMemory(); // Amount of free memory
-        final long usedMemory = totalMemory - freeMemory; // Amount of used memory
-
-        System.err.println("   Memory Total: " + totalMemory + " Free:" + freeMemory + " Used:" + usedMemory);
-    }
-
-    private void printMemoryPools() {
-        List<MemoryPoolMXBean> memoryPools = ManagementFactory.getMemoryPoolMXBeans();
-        for (MemoryPoolMXBean memoryPool : memoryPools) {
-            MemoryUsage usage = memoryPool.getUsage();
-            System.err.println("   " + memoryPool.getName() + " USED: " + usage.getUsed() + " MAX: " + usage.getMax());
-        }
-    }
-
-    private void printGCPools() {
-        List<GarbageCollectorMXBean> garbageCollectors = ManagementFactory.getGarbageCollectorMXBeans();
-        for (GarbageCollectorMXBean garbageCollector : garbageCollectors) {
-            System.err.println("   " + garbageCollector.getName() + " COLLECTION TIME: " + garbageCollector.getCollectionTime());
-        }
-    }
-
 }
