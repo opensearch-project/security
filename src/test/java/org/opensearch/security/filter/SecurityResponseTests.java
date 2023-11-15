40770c962b4b9b7d001be2018aa7062ecf64b46f
Craig Perkins <cwperx@amazon.com>
2023-11-15 11:05:40 -0500

    Explicitly pass contentType in SecurityResponse constructor
    Signed-off-by: Craig Perkins <cwperx@amazon.com>

--- src/test/java/org/opensearch/security/filter/SecurityResponseTests.java
+++ src/test/java/org/opensearch/security/filter/SecurityResponseTests.java
@@ -14,19 +14,19 @@ package org.opensearch.security.filter;
 import org.apache.http.HttpStatus;
 import org.junit.Test;
 
+import org.opensearch.common.xcontent.XContentType;
 import org.opensearch.core.rest.RestStatus;
 import org.opensearch.rest.RestResponse;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
-import static org.opensearch.security.filter.SecurityResponse.CONTENT_TYPE_APP_JSON;
 import static io.netty.handler.codec.http.HttpHeaders.Values.APPLICATION_JSON;
 
 public class SecurityResponseTests {
 
     @Test
     public void testSecurityResponseHasSingleContentType() {
-        final SecurityResponse response = new SecurityResponse(HttpStatus.SC_OK, CONTENT_TYPE_APP_JSON, "foo bar");
+        final SecurityResponse response = new SecurityResponse(HttpStatus.SC_OK, null, "foo bar", XContentType.JSON.mediaType());
 
         final RestResponse restResponse = response.asRestResponse();
         assertThat(restResponse.status(), equalTo(RestStatus.OK));
