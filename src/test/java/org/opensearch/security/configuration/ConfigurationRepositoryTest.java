af122f497dd3fc0fece8a06870ba47d791e84d29
David Osorno <daosgu27@gmail.com>
2023-11-17 21:44:04 -0800

    Test deep clone method
    Signed-off-by: David Osorno <daosgu27@gmail.com>

--- src/test/java/org/opensearch/security/configuration/ConfigurationRepositoryTest.java
+++ src/test/java/org/opensearch/security/configuration/ConfigurationRepositoryTest.java
@@ -14,15 +14,12 @@ package org.opensearch.security.configuration;
 import java.io.IOException;
 import java.nio.file.Path;
 
-import com.google.common.cache.Cache;
-import com.google.common.cache.CacheBuilder;
 import org.junit.Before;
 import org.junit.Test;
 
 import org.opensearch.client.Client;
 import org.opensearch.cluster.service.ClusterService;
 import org.opensearch.common.settings.Settings;
-import org.opensearch.security.DefaultObjectMapper;
 import org.opensearch.security.auditlog.AuditLog;
 import org.opensearch.security.securityconf.impl.CType;
 import org.opensearch.security.securityconf.impl.SecurityDynamicConfiguration;
@@ -52,7 +49,6 @@ public class ConfigurationRepositoryTest {
     private ClusterService clusterService;
 
     private ThreadPool threadPool;
-    private Cache<CType, SecurityDynamicConfiguration<?>> configCache;
 
     @Before
     public void setUp() {
@@ -64,7 +60,6 @@ public class ConfigurationRepositoryTest {
             .build();
 
         threadPool = new ThreadPool(settings);
-        configCache = CacheBuilder.newBuilder().build();
     }
 
     private ConfigurationRepository createConfigurationRepository(Settings settings) {
@@ -109,39 +104,11 @@ public class ConfigurationRepositoryTest {
         SecurityDynamicConfiguration<?> config = configRepository.getConfiguration(CType.CONFIG);
         SecurityDynamicConfiguration<?> emptyConfig = SecurityDynamicConfiguration.empty();
 
-        assertThat(config, instanceOf(SecurityDynamicConfiguration.class));
+        assertThat(config, is(instanceOf(SecurityDynamicConfiguration.class)));
         assertThat(config.getCEntries().size(), is(equalTo(0)));
         assertThat(config.getVersion(), is(equalTo(emptyConfig.getVersion())));
         assertThat(config.getCType(), is(equalTo(emptyConfig.getCType())));
         assertThat(config.getSeqNo(), is(equalTo(emptyConfig.getSeqNo())));
         assertThat(config, is(not(equalTo(emptyConfig))));
-    }
-
-    @Test
-    public void getConfiguration_withValidConfigurationShouldReturnDeepClone() throws Exception {
-
-        Settings settings = Settings.builder().put(ConfigConstants.SECURITY_ALLOW_DEFAULT_INIT_SECURITYINDEX, true).build();
-        ConfigurationRepository configRepository = createConfigurationRepository(settings);
-
-        var objectMapper = DefaultObjectMapper.objectMapper;
-        var objectNode = objectMapper.createObjectNode();
-        objectNode.set("_meta", objectMapper.createObjectNode().put("type", CType.ROLES.toLCString()).put("config_version", 2));
-        objectNode.set("kibana_read_only", objectMapper.createObjectNode().put("reserved", true));
-        objectNode.set("some_hidden_role", objectMapper.createObjectNode().put("hidden", true));
-        objectNode.set("all_access", objectMapper.createObjectNode().put("static", true)); // it reserved as well
-        objectNode.set("security_rest_api_access", objectMapper.createObjectNode().put("reserved", true));
-
-        configCache.put(
-            CType.ROLES,
-            SecurityDynamicConfiguration.fromJson(objectMapper.writeValueAsString(objectNode), CType.ROLES, 2, 1, 1)
-        );
-
-        SecurityDynamicConfiguration<?> config = configRepository.getConfiguration(CType.ROLES);
-        SecurityDynamicConfiguration<?> deepCloneConfig = configRepository.getConfiguration(CType.ROLES);
-
-        assertThat(config, is(notNullValue()));
-        assertThat(config, instanceOf(SecurityDynamicConfiguration.class));
-        // assertThat(config.getCEntries().size(), is(greaterThan(0)));
-        assertThat(config, is(not(equalTo(deepCloneConfig))));
     }
 }
