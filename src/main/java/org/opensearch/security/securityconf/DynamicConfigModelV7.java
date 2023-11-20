1bf104bb3a3e4f3d73134c7fc582b28b386fd853
Prabhas Kurapati <prabhask@berkeley.edu>
2023-11-20 13:28:00 -0800

    remove code
    Signed-off-by: Prabhas Kurapati <prabhask@berkeley.edu>

--- src/main/java/org/opensearch/security/securityconf/DynamicConfigModelV7.java
+++ src/main/java/org/opensearch/security/securityconf/DynamicConfigModelV7.java
@@ -76,8 +76,6 @@ public class DynamicConfigModelV7 extends DynamicConfigModel {
     private final Path configPath;
     private SortedSet<AuthDomain> restAuthDomains;
     private Set<AuthorizationBackend> restAuthorizers;
-    private SortedSet<AuthDomain> transportAuthDomains;
-    private Set<AuthorizationBackend> transportAuthorizers;
     private List<Destroyable> destroyableComponents;
     private final InternalAuthenticationBackend iab;
 
@@ -234,8 +232,6 @@ public class DynamicConfigModelV7 extends DynamicConfigModel {
 
         final SortedSet<AuthDomain> restAuthDomains0 = new TreeSet<>();
         final Set<AuthorizationBackend> restAuthorizers0 = new HashSet<>();
-        final SortedSet<AuthDomain> transportAuthDomains0 = new TreeSet<>();
-        final Set<AuthorizationBackend> transportAuthorizers0 = new HashSet<>();
         final List<Destroyable> destroyableComponents0 = new LinkedList<>();
         final List<AuthFailureListener> ipAuthFailureListeners0 = new ArrayList<>();
         final Multimap<String, AuthFailureListener> authBackendFailureListeners0 = ArrayListMultimap.create();
@@ -246,9 +242,8 @@ public class DynamicConfigModelV7 extends DynamicConfigModel {
 
         for (final Entry<String, AuthzDomain> ad : authzDyn.getDomains().entrySet()) {
             final boolean httpEnabled = ad.getValue().http_enabled;
-            final boolean transportEnabled = ad.getValue().transport_enabled;
 
-            if (httpEnabled || transportEnabled) {
+            if (httpEnabled) {
                 try {
 
                     final String authzBackendClazz = ad.getValue().authorization_backend.type;
@@ -279,10 +274,6 @@ public class DynamicConfigModelV7 extends DynamicConfigModel {
 
                     if (httpEnabled) {
                         restAuthorizers0.add(authorizationBackend);
-                    }
-
-                    if (transportEnabled) {
-                        transportAuthorizers0.add(authorizationBackend);
                     }
 
                     if (authorizationBackend instanceof Destroyable) {
@@ -359,10 +350,6 @@ public class DynamicConfigModelV7 extends DynamicConfigModel {
                         restAuthDomains0.add(_ad);
                     }
 
-                    if (transportEnabled) {
-                        transportAuthDomains0.add(_ad);
-                    }
-
                     if (httpAuthenticator instanceof Destroyable) {
                         destroyableComponents0.add((Destroyable) httpAuthenticator);
                     }
@@ -398,9 +385,7 @@ public class DynamicConfigModelV7 extends DynamicConfigModel {
         List<Destroyable> originalDestroyableComponents = destroyableComponents;
 
         restAuthDomains = Collections.unmodifiableSortedSet(restAuthDomains0);
-        transportAuthDomains = Collections.unmodifiableSortedSet(transportAuthDomains0);
         restAuthorizers = Collections.unmodifiableSet(restAuthorizers0);
-        transportAuthorizers = Collections.unmodifiableSet(transportAuthorizers0);
 
         destroyableComponents = Collections.unmodifiableList(destroyableComponents0);
 
