fd18c24af0ec504d03dc4ea0f3177f37c897ff4e
Craig Perkins <cwperx@amazon.com>
2023-12-12 10:56:25 -0500

    Add imports
    Signed-off-by: Craig Perkins <cwperx@amazon.com>

--- src/test/java/org/opensearch/security/SlowIntegrationTests.java
+++ src/test/java/org/opensearch/security/SlowIntegrationTests.java
@@ -26,7 +26,10 @@
 
 package org.opensearch.security;
 
+import java.io.IOException;
+
 import com.google.common.collect.Lists;
+import org.apache.http.HttpStatus;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -36,6 +39,7 @@ import org.opensearch.action.admin.cluster.settings.ClusterUpdateSettingsRequest
 import org.opensearch.action.admin.indices.create.CreateIndexRequest;
 import org.opensearch.cluster.health.ClusterHealthStatus;
 import org.opensearch.common.settings.Settings;
+import org.opensearch.common.unit.TimeValue;
 import org.opensearch.node.Node;
 import org.opensearch.node.PluginAwareNode;
 import org.opensearch.security.ssl.util.SSLConfigConstants;
@@ -44,6 +48,7 @@ import org.opensearch.security.test.AbstractSecurityUnitTest;
 import org.opensearch.security.test.SingleClusterTest;
 import org.opensearch.security.test.helper.cluster.ClusterConfiguration;
 import org.opensearch.security.test.helper.file.FileHelper;
+import org.opensearch.security.test.helper.rest.RestHelper;
 import org.opensearch.transport.Netty4ModulePlugin;
 
 public class SlowIntegrationTests extends SingleClusterTest {
