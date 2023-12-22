fa37995a9dfc1d0fb3a774da3d74c3d04670e4ee
Peter Nied <petern@amazon.com>
2023-12-22 00:19:31 +0000

    Add deprecation check for jwt_header setting
    Signed-off-by: Peter Nied <petern@amazon.com>

--- src/main/java/com/amazon/dlic/auth/http/jwt/HTTPJwtAuthenticator.java
+++ src/main/java/com/amazon/dlic/auth/http/jwt/HTTPJwtAuthenticator.java
@@ -26,11 +26,13 @@ import org.apache.logging.log4j.Logger;
 
 import org.opensearch.OpenSearchSecurityException;
 import org.opensearch.SpecialPermission;
+import org.opensearch.common.logging.DeprecationLogger;
 import org.opensearch.common.settings.Settings;
 import org.opensearch.common.util.concurrent.ThreadContext;
 import org.opensearch.security.auth.HTTPAuthenticator;
 import org.opensearch.security.filter.SecurityRequest;
 import org.opensearch.security.filter.SecurityResponse;
+import org.opensearch.security.setting.DeprecatedSettings;
 import org.opensearch.security.user.AuthCredentials;
 import org.opensearch.security.util.KeyUtils;
 
@@ -44,6 +46,8 @@ import static org.apache.http.HttpHeaders.AUTHORIZATION;
 public class HTTPJwtAuthenticator implements HTTPAuthenticator {
 
     protected final Logger log = LogManager.getLogger(this.getClass());
+    protected final DeprecationLogger deprecationLog = DeprecationLogger.getLogger(this.getClass());
+
 
     private static final Pattern BASIC = Pattern.compile("^\\s*Basic\\s.*", Pattern.CASE_INSENSITIVE);
     private static final String BEARER = "bearer ";
@@ -68,6 +72,10 @@ public class HTTPJwtAuthenticator implements HTTPAuthenticator {
         subjectKey = settings.get("subject_key");
         requireAudience = settings.get("required_audience");
         requireIssuer = settings.get("required_issuer");
+
+        if (!jwtHeaderName.equals(AUTHORIZATION)) {
+            deprecationLog.deprecate("jwt_header", "The 'jwt_header' setting will be removed in the next major version of OpenSearch.  Consult https://github.com/opensearch-project/security/issues/3886 for more details.");
+        }
 
         final JwtParserBuilder jwtParserBuilder = KeyUtils.createJwtParserBuilderFromSigningKey(signingKey, log);
         if (jwtParserBuilder == null) {
