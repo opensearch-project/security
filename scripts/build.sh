12f3aaf4e8e6a00b504b59f8ec7cd78d15359a65
Craig Perkins <cwperx@amazon.com>
2024-01-18 14:22:02 -0500

    Add step to publishToMavenLocal in the build script
    Signed-off-by: Craig Perkins <cwperx@amazon.com>

--- scripts/build.sh
+++ scripts/build.sh
@@ -77,6 +77,7 @@ echo "COPY ${distributions}/*.zip"
 mkdir -p $OUTPUT/plugins
 cp ${distributions}/*.zip ./$OUTPUT/plugins
 
+./gradlew publishToMavenLocal -Dopensearch.version=$VERSION -Dbuild.snapshot=$SNAPSHOT -Dbuild.version_qualifier=$QUALIFIER
 ./gradlew publishPluginZipPublicationToZipStagingRepository -Dopensearch.version=$VERSION -Dbuild.snapshot=$SNAPSHOT -Dbuild.version_qualifier=$QUALIFIER
 mkdir -p $OUTPUT/maven/org/opensearch
 cp -r ./build/local-staging-repo/org/opensearch/. $OUTPUT/maven/org/opensearch
