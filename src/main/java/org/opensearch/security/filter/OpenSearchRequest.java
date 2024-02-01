6a9330cfed878fdf48dadae01623384708cbe8e1
Craig Perkins <cwperx@amazon.com>
2024-02-01 12:11:08 -0500

    Use HashMap
    Signed-off-by: Craig Perkins <cwperx@amazon.com>

--- src/main/java/org/opensearch/security/filter/OpenSearchRequest.java
+++ src/main/java/org/opensearch/security/filter/OpenSearchRequest.java
@@ -12,6 +12,7 @@
 package org.opensearch.security.filter;
 
 import java.net.InetSocketAddress;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -69,10 +70,10 @@ public class OpenSearchRequest implements SecurityRequest {
 
     @Override
     public Map<String, String> params() {
-        return new Map<>(underlyingRequest.params()) {
+        return new HashMap<>(underlyingRequest.params()) {
             @Override
-            public String get(String key) {
-                return underlyingRequest.param(key);
+            public String get(Object key) {
+                return underlyingRequest.param((String) key);
             }
         };
     }
