2065339f79f28f51818f2ebea55c29fd42101f8f
Nils Bandener <nils.bandener@eliatra.com>
2024-05-30 11:47:37 +0200

    Optimized privilege evaluation
    Signed-off-by: Nils Bandener <nils.bandener@eliatra.com>

--- src/main/java/org/opensearch/security/privileges/PrivilegesEvaluationContext.java
+++ src/main/java/org/opensearch/security/privileges/PrivilegesEvaluationContext.java
@@ -10,19 +10,26 @@
  */
 package org.opensearch.security.privileges;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Supplier;
+
 import com.google.common.collect.ImmutableSet;
 
 import org.opensearch.action.ActionRequest;
+import org.opensearch.cluster.ClusterState;
+import org.opensearch.cluster.metadata.IndexNameExpressionResolver;
 import org.opensearch.security.resolver.IndexResolverReplacer;
+import org.opensearch.security.support.WildcardMatcher;
 import org.opensearch.security.user.User;
 import org.opensearch.tasks.Task;
 
 /**
  * Request-scoped context information for privilege evaluation.
- *
+ * <p>
  * This class carries metadata about the request and provides caching facilities for data which might need to be
  * evaluated several times per request.
- *
+ * <p>
  * As this class is request-scoped, it is only used by a single thread. Thus, no thread synchronization mechanisms
  * are necessary.
  */
@@ -34,25 +41,63 @@ public class PrivilegesEvaluationContext {
     private final Task task;
     private ImmutableSet<String> mappedRoles;
     private final IndexResolverReplacer indexResolverReplacer;
+    private final IndexNameExpressionResolver indexNameExpressionResolver;
+    private final Supplier<ClusterState> clusterStateSupplier;
 
-    public PrivilegesEvaluationContext(
+    /**
+     * This caches the ready to use WildcardMatcher instances for the current request. Many index patterns have
+     * to be executed several times per request (for example first for action privileges, later for DLS). Thus,
+     * it makes sense to cache and later re-use these.
+     */
+    private final Map<String, WildcardMatcher> renderedPatternTemplateCache = new HashMap<>();
+
+    PrivilegesEvaluationContext(
         User user,
         ImmutableSet<String> mappedRoles,
         String action,
         ActionRequest request,
         Task task,
-        IndexResolverReplacer indexResolverReplacer
+        IndexResolverReplacer indexResolverReplacer,
+        IndexNameExpressionResolver indexNameExpressionResolver,
+        Supplier<ClusterState> clusterStateSupplier
     ) {
         this.user = user;
         this.mappedRoles = mappedRoles;
         this.action = action;
         this.request = request;
-        this.task = task;
+        this.clusterStateSupplier = clusterStateSupplier;
         this.indexResolverReplacer = indexResolverReplacer;
+        this.indexNameExpressionResolver = indexNameExpressionResolver;
+        this.task = task;
     }
 
     public User getUser() {
         return user;
+    }
+
+    /**
+     * Interpolates any attribute references (like ${user.name}) in the given string and parses the result
+     * to a WildcardMatcher. This method catches earlier rendered templates in order to avoid recurring re-rendering
+     * of templates during a single privilege evaluation pass.
+     *
+     * @throws ExpressionEvaluationException if the resulting pattern could not be parsed. This is usually the case
+     * if an invalid regex was supplied.
+     */
+    public WildcardMatcher getRenderedMatcher(String template) throws ExpressionEvaluationException {
+        WildcardMatcher matcher = this.renderedPatternTemplateCache.get(template);
+
+        if (matcher == null) {
+            try {
+                matcher = WildcardMatcher.from(UserAttributes.replaceProperties(template, this));
+            } catch (Exception e) {
+                // This especially happens for invalid regular expressions
+                throw new ExpressionEvaluationException("Error while evaluating expression in " + template, e);
+            }
+
+            this.renderedPatternTemplateCache.put(template, matcher);
+        }
+
+        return matcher;
     }
 
     public String getAction() {
@@ -94,4 +139,11 @@ public class PrivilegesEvaluationContext {
         this.mappedRoles = mappedRoles;
     }
 
+    public Supplier<ClusterState> getClusterStateSupplier() {
+        return clusterStateSupplier;
+    }
+
+    public IndexNameExpressionResolver getIndexNameExpressionResolver() {
+        return indexNameExpressionResolver;
+    }
 }
--- src/main/java/org/opensearch/security/privileges/RestLayerPrivilegesEvaluator.java
+++ src/main/java/org/opensearch/security/privileges/RestLayerPrivilegesEvaluator.java
@@ -16,89 +16,38 @@ import java.util.Set;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import org.opensearch.OpenSearchSecurityException;
-import org.opensearch.cluster.service.ClusterService;
-import org.opensearch.common.util.concurrent.ThreadContext;
-import org.opensearch.core.common.transport.TransportAddress;
-import org.opensearch.security.securityconf.ConfigModel;
-import org.opensearch.security.securityconf.SecurityRoles;
-import org.opensearch.security.support.ConfigConstants;
 import org.opensearch.security.user.User;
-import org.opensearch.threadpool.ThreadPool;
-
-import org.greenrobot.eventbus.Subscribe;
 
 public class RestLayerPrivilegesEvaluator {
     protected final Logger log = LogManager.getLogger(this.getClass());
-    private final ClusterService clusterService;
-    private ThreadContext threadContext;
-    private ConfigModel configModel;
+    private final PrivilegesEvaluator privilegesEvaluator;
 
-    public RestLayerPrivilegesEvaluator(final ClusterService clusterService, final ThreadPool threadPool) {
-        this.clusterService = clusterService;
-        this.threadContext = threadPool.getThreadContext();
+    public RestLayerPrivilegesEvaluator(PrivilegesEvaluator privilegesEvaluator) {
+        this.privilegesEvaluator = privilegesEvaluator;
     }
 
-    @Subscribe
-    public void onConfigModelChanged(final ConfigModel configModel) {
-        this.configModel = configModel;
-    }
-
-    SecurityRoles getSecurityRoles(final Set<String> roles) {
-        return configModel.getSecurityRoles().filter(roles);
-    }
-
-    boolean isInitialized() {
-        return configModel != null && configModel.getSecurityRoles() != null;
-    }
-
-    public PrivilegesEvaluatorResponse evaluate(final User user, final Set<String> actions) {
-        if (!isInitialized()) {
-            throw new OpenSearchSecurityException("OpenSearch Security is not initialized.");
-        }
-
-        final PrivilegesEvaluatorResponse presponse = new PrivilegesEvaluatorResponse();
-
-        final TransportAddress caller = threadContext.getTransient(ConfigConstants.OPENDISTRO_SECURITY_REMOTE_ADDRESS);
-
-        final Set<String> mappedRoles = mapRoles(user, caller);
-
-        presponse.resolvedSecurityRoles.addAll(mappedRoles);
-        final SecurityRoles securityRoles = getSecurityRoles(mappedRoles);
+    public PrivilegesEvaluatorResponse evaluate(final User user, final String routeName, final Set<String> actions) {
+        PrivilegesEvaluationContext context = privilegesEvaluator.createContext(user, routeName);
 
         final boolean isDebugEnabled = log.isDebugEnabled();
         if (isDebugEnabled) {
-            log.debug("Evaluate permissions for {} on {}", user, clusterService.localNode().getName());
+            log.debug("Evaluate permissions for {}", user);
             log.debug("Action: {}", actions);
-            log.debug("Mapped roles: {}", mappedRoles.toString());
+            log.debug("Mapped roles: {}", context.getMappedRoles().toString());
         }
 
-        for (final String action : actions) {
-            if (!securityRoles.impliesClusterPermissionPermission(action)) {
-                presponse.missingPrivileges.add(action);
-                presponse.allowed = false;
-                log.info(
-                    "No permission match for {} [Action [{}]] [RolesChecked {}]. No permissions for {}",
-                    user,
-                    action,
-                    securityRoles.getRoleNames(),
-                    presponse.missingPrivileges
-                );
-            } else {
-                if (isDebugEnabled) {
-                    log.debug("Allowed because we have permissions for {}", actions);
-                }
-                presponse.allowed = true;
+        PrivilegesEvaluatorResponse result = privilegesEvaluator.getActionPrivileges().hasAnyClusterPrivilege(context, actions);
 
-                // break the loop as we found the matching permission
-                break;
-            }
+        if (!result.allowed) {
+            log.info(
+                "No permission match for {} [Action [{}]] [RolesChecked {}]. No permissions for {}",
+                user,
+                routeName,
+                context.getMappedRoles(),
+                result.getMissingPrivileges()
+            );
         }
 
-        return presponse;
-    }
-
-    Set<String> mapRoles(final User user, final TransportAddress caller) {
-        return this.configModel.mapSecurityRoles(user, caller);
+        return result;
     }
 }
