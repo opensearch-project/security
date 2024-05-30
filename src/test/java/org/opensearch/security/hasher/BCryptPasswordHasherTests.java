79f1a8cab4774b7c63e1d6feedb85e8b716a9200
Dan Cecoi <dan.cecoi@sas.com>
2024-05-30 11:08:53 +0100

    Fixed checkstyle scan failures
    Signed-off-by: Dan Cecoi <Dan.Cecoi@sas.com>

--- src/test/java/org/opensearch/security/hasher/BCryptPasswordHasherTests.java
+++ src/test/java/org/opensearch/security/hasher/BCryptPasswordHasherTests.java
@@ -17,7 +17,11 @@ import org.junit.Test;
 
 import org.opensearch.OpenSearchSecurityException;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertThrows;
+import static org.junit.Assert.assertTrue;
 
 public class BCryptPasswordHasherTests {
 
