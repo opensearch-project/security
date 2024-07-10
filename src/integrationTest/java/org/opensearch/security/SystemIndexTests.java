dec2d9b43ebc3f4d5626453f60501decef643e38
Craig Perkins <cwperx@amazon.com>
2024-07-10 17:14:57 -0400

    Enable system index protection for testing
    Signed-off-by: Craig Perkins <cwperx@amazon.com>

--- src/integrationTest/java/org/opensearch/security/SystemIndexTests.java
+++ src/integrationTest/java/org/opensearch/security/SystemIndexTests.java
@@ -28,6 +28,7 @@ import org.opensearch.test.framework.cluster.TestRestClient.HttpResponse;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.opensearch.security.support.ConfigConstants.SECURITY_RESTAPI_ROLES_ENABLED;
+import static org.opensearch.security.support.ConfigConstants.SECURITY_SYSTEM_INDICES_ENABLED_KEY;
 import static org.opensearch.test.framework.TestSecurityConfig.Role.ALL_ACCESS;
 import static org.opensearch.test.framework.TestSecurityConfig.User.USER_ADMIN;
 
@@ -43,7 +44,14 @@ public class SystemIndexTests {
         .authc(AUTHC_DOMAIN)
         .users(USER_ADMIN)
         .plugin(ExampleSystemIndexPlugin.class)
-        .nodeSettings(Map.of(SECURITY_RESTAPI_ROLES_ENABLED, List.of("user_" + USER_ADMIN.getName() + "__" + ALL_ACCESS.getName())))
+        .nodeSettings(
+            Map.of(
+                SECURITY_RESTAPI_ROLES_ENABLED,
+                List.of("user_" + USER_ADMIN.getName() + "__" + ALL_ACCESS.getName()),
+                SECURITY_SYSTEM_INDICES_ENABLED_KEY,
+                true
+            )
+        )
         .build();
 
     @Test
@@ -69,7 +77,7 @@ public class SystemIndexTests {
             // regular user cannot delete system index
             HttpResponse response4 = client.delete(".system-index1");
 
-            assertThat(response4.getStatusCode(), equalTo(RestStatus.OK.getStatus()));
+            assertThat(response4.getStatusCode(), equalTo(RestStatus.FORBIDDEN.getStatus()));
         }
     }
 }
