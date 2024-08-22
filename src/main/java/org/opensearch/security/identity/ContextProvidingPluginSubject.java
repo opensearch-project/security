675f20aedef4bc815452c360cd84ff07f1e8bdcc
Craig Perkins <cwperx@amazon.com>
2024-08-22 17:54:35 -0400

    Remove unused code
    Signed-off-by: Craig Perkins <cwperx@amazon.com>

--- src/main/java/org/opensearch/security/identity/ContextProvidingPluginSubject.java
+++ src/main/java/org/opensearch/security/identity/ContextProvidingPluginSubject.java
@@ -1,17 +1,13 @@
 package org.opensearch.security.identity;
 
 import java.security.Principal;
-import java.util.Collection;
-import java.util.List;
 import java.util.concurrent.Callable;
 
 import org.opensearch.common.settings.Settings;
 import org.opensearch.common.util.concurrent.ThreadContext;
 import org.opensearch.identity.NamedPrincipal;
 import org.opensearch.identity.PluginSubject;
-import org.opensearch.indices.SystemIndexDescriptor;
 import org.opensearch.plugins.Plugin;
-import org.opensearch.plugins.SystemIndexPlugin;
 import org.opensearch.security.securityconf.impl.v7.RoleV7;
 import org.opensearch.security.support.ConfigConstants;
 import org.opensearch.security.user.PluginUser;
@@ -28,19 +24,6 @@ public class ContextProvidingPluginSubject implements PluginSubject {
         this.threadPool = threadPool;
         this.pluginPrincipal = new NamedPrincipal(plugin.getClass().getCanonicalName());
         this.pluginUser = new PluginUser(pluginPrincipal.getName());
-        if (plugin instanceof SystemIndexPlugin) {
-            Collection<SystemIndexDescriptor> systemIndexDescriptors = ((SystemIndexPlugin) plugin).getSystemIndexDescriptors(settings);
-            roleV7 = new RoleV7();
-            if (systemIndexDescriptors != null) {
-                List<String> systemIndexPatterns = systemIndexDescriptors.stream().map(SystemIndexDescriptor::getIndexPattern).toList();
-                RoleV7.Index indexPermissions = new RoleV7.Index();
-                indexPermissions.setIndex_patterns(systemIndexPatterns);
-                indexPermissions.setAllowed_actions(List.of(ConfigConstants.SYSTEM_INDEX_PERMISSION));
-                roleV7.setIndex_permissions(List.of(indexPermissions));
-            }
-        } else {
-            roleV7 = null;
-        }
     }
 
     @Override
