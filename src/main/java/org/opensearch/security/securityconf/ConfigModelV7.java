7e32bcfb49996646ee011d778de8484cf243c12e
Craig Perkins <cwperx@amazon.com>
2024-08-30 14:41:42 -0400

    Remove change in ConfigModelV7
    Signed-off-by: Craig Perkins <cwperx@amazon.com>

--- src/main/java/org/opensearch/security/securityconf/ConfigModelV7.java
+++ src/main/java/org/opensearch/security/securityconf/ConfigModelV7.java
@@ -304,13 +304,7 @@ public class ConfigModelV7 extends ConfigModel {
 
             for (SecurityRole role : roles) {
                 for (IndexPattern ip : role.getIpatterns()) {
-                    final Set<String> concreteIndices = new HashSet<>(ip.concreteIndexNames(user, resolver, cs));
-                    final List<String> dataStreamNames = resolver.dataStreamNames(
-                        cs.state(),
-                        IndicesOptions.LENIENT_EXPAND_OPEN,
-                        ip.indexPattern
-                    );
-                    concreteIndices.addAll(dataStreamNames);
+                    final Set<String> concreteIndices = ip.concreteIndexNames(user, resolver, cs);
                     String dls = ip.getDlsQuery(user);
 
                     if (dls != null && dls.length() > 0) {
