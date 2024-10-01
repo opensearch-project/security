0a9addbb8f43f1aff587c1af3a7daf4dabec143c
Derek Ho <dxho@amazon.com>
2024-10-01 17:06:37 -0400

    Removes unused get/post/put cache endpoint (#4761)
    Signed-off-by: Derek Ho <dxho@amazon.com>

--- src/main/java/org/opensearch/security/dlic/rest/api/FlushCacheApiAction.java
+++ src/main/java/org/opensearch/security/dlic/rest/api/FlushCacheApiAction.java
@@ -36,14 +36,7 @@ public class FlushCacheApiAction extends AbstractApiAction {
 
     private final static Logger LOGGER = LogManager.getLogger(FlushCacheApiAction.class);
 
-    private static final List<Route> routes = addRoutesPrefix(
-        ImmutableList.of(
-            new Route(Method.DELETE, "/cache"),
-            new Route(Method.GET, "/cache"),
-            new Route(Method.PUT, "/cache"),
-            new Route(Method.POST, "/cache")
-        )
-    );
+    private static final List<Route> routes = addRoutesPrefix(ImmutableList.of(new Route(Method.DELETE, "/cache")));
 
     @Inject
     public FlushCacheApiAction(
