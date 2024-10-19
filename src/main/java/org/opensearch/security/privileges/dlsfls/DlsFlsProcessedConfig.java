7a035309c8264c1e2a84861a898794748afcf8c5
Nils Bandener <nils.bandener@eliatra.com>
2024-10-19 09:26:46 +0200

    Tests and cleanup
    Signed-off-by: Nils Bandener <nils.bandener@eliatra.com>

--- src/main/java/org/opensearch/security/privileges/dlsfls/DlsFlsProcessedConfig.java
+++ src/main/java/org/opensearch/security/privileges/dlsfls/DlsFlsProcessedConfig.java
@@ -21,6 +21,8 @@ import org.opensearch.cluster.metadata.Metadata;
 import org.opensearch.cluster.service.ClusterService;
 import org.opensearch.common.settings.Settings;
 import org.opensearch.core.xcontent.NamedXContentRegistry;
+import org.opensearch.security.privileges.ActionPrivileges;
+import org.opensearch.security.privileges.ClusterStateMetadataDependentPrivileges;
 import org.opensearch.security.securityconf.impl.SecurityDynamicConfiguration;
 import org.opensearch.security.securityconf.impl.v7.RoleV7;
 import org.opensearch.threadpool.ThreadPool;
@@ -29,14 +31,13 @@ import org.opensearch.threadpool.ThreadPool;
  * Encapsulates the processed DLS/FLS configuration from roles.yml.
  * The current instance is held and managed by DlsFlsValveImpl.
  */
-public class DlsFlsProcessedConfig {
+public class DlsFlsProcessedConfig extends ClusterStateMetadataDependentPrivileges {
     private static final Logger log = LogManager.getLogger(DlsFlsProcessedConfig.class);
 
     private final DocumentPrivileges documentPrivileges;
     private final FieldPrivileges fieldPrivileges;
     private final FieldMasking fieldMasking;
     private long metadataVersionEffective = -1;
-    private Future<?> updateFuture;
 
     public DlsFlsProcessedConfig(
         SecurityDynamicConfiguration<RoleV7> rolesConfiguration,
@@ -62,74 +63,22 @@ public class DlsFlsProcessedConfig {
         return this.fieldMasking;
     }
 
-    public void updateIndices(Map<String, IndexAbstraction> indexMetadata) {
+    @Override
+    protected void updateClusterStateMetadata(Metadata metadata) {
         long start = System.currentTimeMillis();
+        Map<String, IndexAbstraction> indexLookup = metadata.getIndicesLookup();
 
-        this.documentPrivileges.updateIndices(indexMetadata);
-        this.fieldPrivileges.updateIndices(indexMetadata);
-        this.fieldMasking.updateIndices(indexMetadata);
+        this.documentPrivileges.updateIndices(indexLookup);
+        this.fieldPrivileges.updateIndices(indexLookup);
+        this.fieldMasking.updateIndices(indexLookup);
 
         long duration = System.currentTimeMillis() - start;
 
         log.debug("Updating DlsFlsProcessedConfig took {} ms", duration);
     }
 
-    /**
-     * Updates the stateful index configuration asynchronously with the index metadata from the current cluster state.
-     * As the update process can take some seconds for clusters with many indices, this method "de-bounces" the updates,
-     * i.e., a further update will be only initiated after the previous update has finished. This is okay as the
-     * underlying DocumentPrivileges/FieldPrivileges classes can handle the case that they do not have the most
-     * recent information. These classes will fall back to slower methods then.
-     */
-    public synchronized void updateIndicesAsync(ClusterService clusterService, ThreadPool threadPool) {
-        long currentMetadataVersion = clusterService.state().metadata().version();
-
-        if (currentMetadataVersion <= this.metadataVersionEffective) {
-            return;
-        }
-
-        if (this.updateFuture == null || this.updateFuture.isDone()) {
-            this.updateFuture = threadPool.generic().submit(() -> {
-                for (int i = 0;; i++) {
-                    if (i > 10) {
-                        try {
-                            // In case we got many consecutive updates, let's sleep a little to let
-                            // other operations catch up.
-                            Thread.sleep(100);
-                        } catch (InterruptedException e) {
-                            return;
-                        }
-                    }
-
-                    Metadata metadata = clusterService.state().metadata();
-
-                    synchronized (DlsFlsProcessedConfig.this) {
-                        if (metadata.version() <= DlsFlsProcessedConfig.this.metadataVersionEffective) {
-                            return;
-                        }
-                    }
-
-                    try {
-                        log.debug("Updating DlsFlsProcessedConfig with metadata version {}", metadata.version());
-                        updateIndices(metadata.getIndicesLookup());
-                    } catch (Exception e) {
-                        log.error("Error while updating DlsFlsProcessedConfig", e);
-                    } finally {
-                        synchronized (DlsFlsProcessedConfig.this) {
-                            DlsFlsProcessedConfig.this.metadataVersionEffective = metadata.version();
-                            if (DlsFlsProcessedConfig.this.updateFuture.isCancelled()) {
-                                return;
-                            }
-                        }
-                    }
-                }
-            });
-        }
-    }
-
-    public synchronized void shutdown() {
-        if (this.updateFuture != null && !this.updateFuture.isDone()) {
-            this.updateFuture.cancel(true);
-        }
+    @Override
+    protected long getCurrentlyUsedMetadataVersion() {
+        return this.metadataVersionEffective;
     }
 }
