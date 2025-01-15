2fc3b8cb458a6a4661b752227ceac63befe45450
Craig Perkins <cwperx@amazon.com>
2025-01-14 20:05:09 -0500

    Fix checkstyle error
    Signed-off-by: Craig Perkins <cwperx@amazon.com>

--- src/integrationTest/java/org/opensearch/security/systemindex/sampleplugin/IndexDocumentIntoSystemIndexAction.java
+++ src/integrationTest/java/org/opensearch/security/systemindex/sampleplugin/IndexDocumentIntoSystemIndexAction.java
@@ -11,7 +11,9 @@
 package org.opensearch.security.systemindex.sampleplugin;
 
 import org.opensearch.action.ActionType;
+// CS-SUPPRESS-SINGLE: RegexpSingleline It is not possible to use phrase "cluster manager" instead of master here
 import org.opensearch.action.support.master.AcknowledgedResponse;
+// CS-ENFORCE-SINGLE
 
 public class IndexDocumentIntoSystemIndexAction extends ActionType<AcknowledgedResponse> {
     public static final IndexDocumentIntoSystemIndexAction INSTANCE = new IndexDocumentIntoSystemIndexAction();
