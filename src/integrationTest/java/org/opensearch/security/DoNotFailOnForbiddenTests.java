f19a0a724c1fc16f472f878e58595cbc3c37e17d
Darshit Chanpura <dchanp@amazon.com>
2025-01-16 13:49:44 -0500

    Updates import statement
    Signed-off-by: Darshit Chanpura <dchanp@amazon.com>

--- src/integrationTest/java/org/opensearch/security/DoNotFailOnForbiddenTests.java
+++ src/integrationTest/java/org/opensearch/security/DoNotFailOnForbiddenTests.java
@@ -12,6 +12,7 @@ package org.opensearch.security;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.stream.Collectors;
 
