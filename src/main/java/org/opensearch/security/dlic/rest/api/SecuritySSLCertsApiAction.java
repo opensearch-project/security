cb874202fc1ed5ebbbb6ad4b984a66a0200eeea1
shikharj05 <8859327+shikharj05@users.noreply.github.com>
2025-02-10 15:18:15 +0530

    Fix deprecated routes for certs API
    Signed-off-by: shikharj05 <8859327+shikharj05@users.noreply.github.com>

--- src/main/java/org/opensearch/security/dlic/rest/api/SecuritySSLCertsApiAction.java
+++ src/main/java/org/opensearch/security/dlic/rest/api/SecuritySSLCertsApiAction.java
@@ -44,7 +44,7 @@ import static org.opensearch.security.dlic.rest.api.Responses.response;
 import static org.opensearch.security.dlic.rest.api.RestApiAdminPrivilegesEvaluator.CERTS_INFO_ACTION;
 import static org.opensearch.security.dlic.rest.api.RestApiAdminPrivilegesEvaluator.RELOAD_CERTS_ACTION;
 import static org.opensearch.security.dlic.rest.support.Utils.OPENDISTRO_API_DEPRECATION_MESSAGE;
-import static org.opensearch.security.dlic.rest.support.Utils.addDeprecatedRoutesPrefix;
+import static org.opensearch.security.dlic.rest.support.Utils.addLegacyRoutesPrefix;
 import static org.opensearch.security.dlic.rest.support.Utils.addRoutesPrefix;
 
 /**
@@ -65,7 +65,7 @@ public class SecuritySSLCertsApiAction extends AbstractApiAction {
         )
     );
 
-    private static final List<DeprecatedRoute> DEPRECATED_ROUTES = addDeprecatedRoutesPrefix(
+    private static final List<DeprecatedRoute> DEPRECATED_ROUTES = addLegacyRoutesPrefix(
         ImmutableList.of(
             new DeprecatedRoute(Method.GET, "/ssl/certs", OPENDISTRO_API_DEPRECATION_MESSAGE),
             new DeprecatedRoute(Method.PUT, "/ssl/{certType}/reloadcerts", OPENDISTRO_API_DEPRECATION_MESSAGE)
