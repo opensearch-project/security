54c72fa04feb186757a7e68281d95ef5a1ac4b3f
shikharj05 <8859327+shikharj05@users.noreply.github.com>
2025-02-18 17:19:01 +0530

    Fix rest integ tests
    Signed-off-by: shikharj05 <8859327+shikharj05@users.noreply.github.com>

--- src/integrationTest/java/org/opensearch/security/api/InternalUsersRestApiIntegrationTest.java
+++ src/integrationTest/java/org/opensearch/security/api/InternalUsersRestApiIntegrationTest.java
@@ -419,10 +419,14 @@ public class InternalUsersRestApiIntegrationTest extends AbstractConfigEntityApi
         assertThat(actualObjectNode.toPrettyString(), not(actualObjectNode.has("hash")));
         assertThat(actualObjectNode.toPrettyString(), actualObjectNode.get("backend_roles"), is(expectedObjectNode.get("backend_roles")));
         assertThat(actualObjectNode.toPrettyString(), actualObjectNode.get("attributes"), is(expectedObjectNode.get("attributes")));
+        // can be either of OPENSEARCH_SECURITY_ROLES or OPENDISTRO_SECURITY_ROLES
+        JsonNode expectedRoles = expectedObjectNode.get(OPENSEARCH_SECURITY_ROLES) != null ?
+                expectedObjectNode.get(OPENSEARCH_SECURITY_ROLES):
+                expectedObjectNode.get(OPENDISTRO_SECURITY_ROLES);
         assertThat(
             actualObjectNode.toPrettyString(),
             actualObjectNode.get("opendistro_security_roles"),
-            is(expectedObjectNode.get("opendistro_security_roles"))
+            is(expectedRoles)
         );
     }
 
