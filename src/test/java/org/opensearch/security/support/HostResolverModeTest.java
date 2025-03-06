aa22ca86e00b50b4e7b48580062aaaee84e69eb3
Shikhar Jain <8859327+shikharj05@users.noreply.github.com>
2025-03-06 18:15:30 +0530

    Add support for CIDR ranges in ignore_hosts setting. (#5099)
    Signed-off-by: shikharj05 <8859327+shikharj05@users.noreply.github.com>

---
+++ src/test/java/org/opensearch/security/support/HostResolverModeTest.java
@@ -0,0 +1,35 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ *
+ * Modifications Copyright OpenSearch Contributors. See
+ * GitHub history for details.
+ */
+
+package org.opensearch.security.support;
+
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class HostResolverModeTest {
+
+    @Test
+    public void testIpHostnameValue() {
+        assertThat(HostResolverMode.IP_HOSTNAME.getValue(), is("ip-hostname"));
+    }
+
+    @Test
+    public void testIpHostnameLookupValue() {
+        assertThat(HostResolverMode.IP_HOSTNAME_LOOKUP.getValue(), is("ip-hostname-lookup"));
+    }
+
+    @Test
+    public void testEnumCount() {
+        assertThat(HostResolverMode.values().length, is(2));
+    }
+}
