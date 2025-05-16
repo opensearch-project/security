3e95d8700ddb9cd00bb3170f2bfb460f956511e6
Rishav Kumar <rishavaz@amazon.com>
2025-05-16 12:55:46 +0530

    added test-user in default configuration
    Signed-off-by: Rishav Kumar <rishavaz@amazon.com>

--- src/integrationTest/java/org/opensearch/security/api/FlushCacheApiIntegrationTest.java
+++ src/integrationTest/java/org/opensearch/security/api/FlushCacheApiIntegrationTest.java
@@ -17,7 +17,6 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class FlushCacheApiIntegrationTest extends AbstractApiIntegrationTest {
-    private final static String TEST_USER = "testuser";
 
     private String cachePath() {
         return super.apiPath("cache");
