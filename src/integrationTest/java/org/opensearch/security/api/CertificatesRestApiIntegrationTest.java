9c2a0952205f7c8316d0daad10a336da1265d26b
Finn Carroll <carrofin@amazon.com>
2025-05-30 12:41:02 -0700

    Spotless apply
    Signed-off-by: Finn Carroll <carrofin@amazon.com>

--- src/integrationTest/java/org/opensearch/security/api/CertificatesRestApiIntegrationTest.java
+++ src/integrationTest/java/org/opensearch/security/api/CertificatesRestApiIntegrationTest.java
@@ -15,7 +15,6 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.StringJoiner;
 import java.util.stream.Collectors;
 
