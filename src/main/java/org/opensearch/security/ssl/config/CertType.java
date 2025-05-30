ba4d046024ac1177382e5156c5d5864ef07056de
Finn Carroll <carrofin@amazon.com>
2025-05-30 12:40:18 -0700

    Make CertTypes registry write only.
    Signed-off-by: Finn Carroll <carrofin@amazon.com>

--- src/main/java/org/opensearch/security/ssl/config/CertType.java
+++ src/main/java/org/opensearch/security/ssl/config/CertType.java
@@ -12,8 +12,9 @@
 package org.opensearch.security.ssl.config;
 
 import java.io.IOException;
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Locale;
 import java.util.Objects;
 import java.util.Set;
@@ -22,42 +23,82 @@ import org.opensearch.core.common.io.stream.StreamInput;
 import org.opensearch.core.common.io.stream.StreamOutput;
 import org.opensearch.core.common.io.stream.Writeable;
 
+import javax.annotation.Nonnull;
+
 import static org.opensearch.security.ssl.util.SSLConfigConstants.SSL_HTTP_PREFIX;
-import static org.opensearch.security.ssl.util.SSLConfigConstants.SSL_TRANSPORT_CLIENT_PREFIX;
 import static org.opensearch.security.ssl.util.SSLConfigConstants.SSL_TRANSPORT_PREFIX;
+import static org.opensearch.security.ssl.util.SSLConfigConstants.SSL_TRANSPORT_CLIENT_PREFIX;
+import static org.opensearch.security.ssl.util.SSLConfigConstants.SSL_TRANSPORT_SERVER_PREFIX;
 
 /**
- * CertTypes have a 1-to-1 relationship with ssl context configurations and identify
+ * CertTypes have a 1-to-1 relationship with ssl contexts and identify
  * the setting prefix under which configuration settings are located.
- *
- * CertTypes are uniquely identified by their `certID` (the last element of their setting prefix)
- * as this is how users identify certs in the certificates info API.
+ * CertTypes are uniquely identified by a `certID` which is used as the key for registering CertTypes on a node
+ * and fetching certificate info through a CertificatesInfoNodesRequest.
  */
 public class CertType implements Writeable {
     private final String sslConfigSettingPrefix;
 
     public static CertType HTTP = new CertType(SSL_HTTP_PREFIX);
     public static CertType TRANSPORT = new CertType(SSL_TRANSPORT_PREFIX);
-    public static CertType TRANSPORT_CLIENT = new CertType(SSL_TRANSPORT_CLIENT_PREFIX);
+    public static CertType TRANSPORT_CLIENT = new CertType(SSL_TRANSPORT_CLIENT_PREFIX) {
+        @Override
+        public String certID() { return "transport_client"; }
+    };
+    public static CertType TRANSPORT_SERVER = new CertType(SSL_TRANSPORT_SERVER_PREFIX) {
+        @Override
+        public String certID() { return "transport_server"; }
+    };
 
-    /*
-     * REGISTERED_CERT_TYPES provides visibility of known configured certificates to certificates api.
-     * {@link org.opensearch.security.dlic.rest.api.ssl.CertificatesInfoNodesRequest}.
-     * Disabled or invalid cert configurations are still registered here.
-     */
-    public static final Set<CertType> REGISTERED_CERT_TYPES = new HashSet<>(Arrays.asList(HTTP, TRANSPORT, TRANSPORT_CLIENT));
-    public static boolean certRegistered(String certID){
-        for (CertType certType : REGISTERED_CERT_TYPES) {
-            if (Objects.equals(certType.certID(), certID)) {
-                return true;
+    public static class NodeCertTypeRegistry implements Iterable<CertType> {
+        private final Set<CertType> RegisteredCertType = new HashSet<>();
+
+        public NodeCertTypeRegistry(CertType... initialCertTypes) {
+            for (CertType certType : initialCertTypes){
+                register(certType);
             }
         }
-        return false;
+
+        public void register(CertType certType) {
+            if(RegisteredCertType.contains(certType)){
+                throw new IllegalArgumentException("Cert type " + certType + " is already registered in CertType registry");
+            }
+            RegisteredCertType.add(certType);
+        }
+
+        public boolean contains(CertType certType) {
+            return RegisteredCertType.contains(certType);
+        }
+
+        public boolean contains(String certID) {
+            for (CertType certType : RegisteredCertType) {
+                if (Objects.equals(certType.certID(), certID)) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        @Nonnull
+        @Override
+        public Iterator<CertType> iterator() {
+            return Collections.unmodifiableSet(RegisteredCertType).iterator();
+        }
     }
+
+    /*
+    Write only map for tracking certificates type discovered and registered on a node.
+    Not all ssl context configurations are known at compile time, so we track newly discovered CertTypes here.
+    */
+    public static final NodeCertTypeRegistry CERT_TYPE_REGISTRY = new NodeCertTypeRegistry(
+            HTTP,
+            TRANSPORT,
+            TRANSPORT_CLIENT,
+            TRANSPORT_SERVER
+    );
 
     public CertType(String sslConfigSettingPrefix) {
         this.sslConfigSettingPrefix = sslConfigSettingPrefix;
-
     }
 
     public CertType(final StreamInput in) throws IOException {
