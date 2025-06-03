947c333fb3418892d0a324b5755e3ed25a936ab0
Finn Carroll <carrofin@amazon.com>
2025-06-03 11:43:59 -0700

    Fix CertificatesRestApiIntegrationTests.
    Certificate structure was never checked in this test suite due to the usage of
    CertType.HTTP.name().toUpperCase. Which does not match previous convention of
    representing certificate names in lowercase.
    
    Additionally integ tests configure a subject AND root certificate on each node.
    Fixing certificate validation here to handle root cert case.
    
    Additionally on board these tests to the new CertType class.
    
    Signed-off-by: Finn Carroll <carrofin@amazon.com>

--- src/integrationTest/java/org/opensearch/security/api/CertificatesRestApiIntegrationTest.java
+++ src/integrationTest/java/org/opensearch/security/api/CertificatesRestApiIntegrationTest.java
@@ -16,12 +16,14 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.StringJoiner;
+import java.util.function.Consumer;
 import java.util.stream.Collectors;
 
 import com.carrotsearch.randomizedtesting.RandomizedContext;
 import com.fasterxml.jackson.databind.JsonNode;
 import org.junit.Test;
 
+import org.opensearch.common.CheckedConsumer;
 import org.opensearch.security.dlic.rest.api.Endpoint;
 import org.opensearch.security.ssl.config.CertType;
 import org.opensearch.test.framework.TestSecurityConfig;
@@ -29,6 +31,7 @@ import org.opensearch.test.framework.certificate.TestCertificates;
 import org.opensearch.test.framework.cluster.LocalOpenSearchCluster;
 import org.opensearch.test.framework.cluster.TestRestClient;
 
+import static junit.framework.TestCase.fail;
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -37,10 +40,9 @@ import static org.opensearch.security.dlic.rest.api.RestApiAdminPrivilegesEvalua
 import static org.opensearch.security.support.ConfigConstants.SECURITY_RESTAPI_ADMIN_ENABLED;
 
 public class CertificatesRestApiIntegrationTest extends AbstractApiIntegrationTest {
-
     final static String REST_API_ADMIN_SSL_INFO = "rest-api-admin-ssl-info";
-
     final static String REGULAR_USER = "regular_user";
+    final static String ROOT_CA = "Root CA";
 
     static {
         testSecurityConfig.roles(
@@ -87,13 +89,13 @@ public class CertificatesRestApiIntegrationTest extends AbstractApiIntegrationTe
 
     @Test
     public void availableForTlsAdmin() throws Exception {
-        withUser(ADMIN_USER_NAME, localCluster.getAdminCertificate(), this::verifySSLCertsInfo);
+        withUser(ADMIN_USER_NAME, localCluster.getAdminCertificate(), verifySSLCertsInfo(List.of(CertType.HTTP, CertType.TRANSPORT, CertType.TRANSPORT_CLIENT)));
     }
 
     @Test
     public void availableForRestAdmin() throws Exception {
-        withUser(REST_ADMIN_USER, this::verifySSLCertsInfo);
-        withUser(REST_API_ADMIN_SSL_INFO, this::verifySSLCertsInfo);
+        withUser(REST_ADMIN_USER, verifySSLCertsInfo(List.of(CertType.HTTP, CertType.TRANSPORT, CertType.TRANSPORT_CLIENT)));
+        withUser(REST_API_ADMIN_SSL_INFO, verifySSLCertsInfo(List.of(CertType.HTTP, CertType.TRANSPORT, CertType.TRANSPORT_CLIENT)));
     }
 
     @Test
@@ -105,23 +107,25 @@ public class CertificatesRestApiIntegrationTest extends AbstractApiIntegrationTe
         ok(() -> client.get(sslCertsPath() + "?timeout=0"));
     }
 
-    private void verifySSLCertsInfo(final TestRestClient client) throws Exception {
-        List<CertType> certTypes = new ArrayList<>();
-        for (CertType cert : CertType.CERT_TYPE_REGISTRY) {
-            certTypes.add(cert);
-        }
-        assertSSLCertsInfo(localCluster.nodes(), certTypes, ok(() -> client.get(sslCertsPath())));
-        if (localCluster.nodes().size() > 1) {
-            final var randomNodes = randomNodes();
-            final var nodeIds = randomNodes.stream().map(n -> n.esNode().getNodeEnvironment().nodeId()).collect(Collectors.joining(","));
-            assertSSLCertsInfo(randomNodes, certTypes, ok(() -> client.get(sslCertsPath(nodeIds))));
-        }
-        final var randomCertType = randomFrom(certTypes);
-        assertSSLCertsInfo(
-            localCluster.nodes(),
-            List.of(randomCertType),
-            ok(() -> client.get(String.format("%s?cert_type=%s", sslCertsPath(), randomCertType)))
-        );
+    private CheckedConsumer<TestRestClient, Exception> verifySSLCertsInfo(List<CertType> expectCerts) {
+        return testRestClient -> {
+            try {
+                assertSSLCertsInfo(localCluster.nodes(), expectCerts, ok(() -> testRestClient.get(sslCertsPath())));
+                if (localCluster.nodes().size() > 1) {
+                    final var randomNodes = randomNodes();
+                    final var nodeIds = randomNodes.stream().map(n -> n.esNode().getNodeEnvironment().nodeId()).collect(Collectors.joining(","));
+                    assertSSLCertsInfo(randomNodes, expectCerts, ok(() -> testRestClient.get(sslCertsPath(nodeIds))));
+                }
+                final var randomCertType = randomFrom(expectCerts);
+                assertSSLCertsInfo(
+                        localCluster.nodes(),
+                        List.of(randomCertType),
+                        ok(() -> testRestClient.get(String.format("%s?cert_type=%s", sslCertsPath(), randomCertType)))
+                );
+            } catch (Exception e) {
+                fail("Verify SSLCerts info failed with exception: " + e.getMessage());
+            }
+        };
     }
 
     private void assertSSLCertsInfo(
@@ -131,48 +135,52 @@ public class CertificatesRestApiIntegrationTest extends AbstractApiIntegrationTe
     ) {
         final var body = response.bodyAsJsonNode();
         final var prettyStringBody = body.toPrettyString();
-
         final var _nodes = body.get("_nodes");
         assertThat(prettyStringBody, _nodes.get("total").asInt(), is(expectedNode.size()));
         assertThat(prettyStringBody, _nodes.get("successful").asInt(), is(expectedNode.size()));
         assertThat(prettyStringBody, _nodes.get("failed").asInt(), is(0));
         assertThat(prettyStringBody, body.get("cluster_name").asText(), is(localCluster.getClusterName()));
-
         final var nodes = body.get("nodes");
-
         for (final var n : expectedNode) {
             final var esNode = n.esNode();
             final var node = nodes.get(esNode.getNodeEnvironment().nodeId());
             assertThat(prettyStringBody, node.get("name").asText(), is(n.getNodeName()));
             assertThat(prettyStringBody, node.has("certificates"));
             final var certificates = node.get("certificates");
-            if (expectedCertTypes.contains(CertType.HTTP)) {
-                final var httpCertificates = certificates.get(CertType.HTTP.certID());
-                assertThat(prettyStringBody, httpCertificates.isArray());
-                assertThat(prettyStringBody, httpCertificates.size(), is(1));
-                verifyCertsJson(n.nodeNumber(), httpCertificates.get(0));
-            }
-            if (expectedCertTypes.contains(CertType.TRANSPORT_CLIENT)) {
-                final var transportCertificates = certificates.get(CertType.TRANSPORT.certID());
+            /*
+            Expect each node transport configured with root and issued cert.
+            */
+            for (CertType expectCert : expectedCertTypes) {
+                final JsonNode transportCertificates = certificates.get(expectCert.certID());
                 assertThat(prettyStringBody, transportCertificates.isArray());
-                assertThat(prettyStringBody, transportCertificates.size(), is(1));
+                assertThat(prettyStringBody, transportCertificates.size(), is(2));
                 verifyCertsJson(n.nodeNumber(), transportCertificates.get(0));
+                verifyCertsJson(n.nodeNumber(), transportCertificates.get(1));
             }
         }
     }
 
     private void verifyCertsJson(final int nodeNumber, final JsonNode jsonNode) {
+        if (jsonNode.get("subject_dn").asText().contains(ROOT_CA)) { // handle root cert
+            assertThat(
+                    jsonNode.toPrettyString(),
+                    jsonNode.get("subject_dn").asText(),
+                    is(TestCertificates.CA_SUBJECT)
+            );
+            assertThat(jsonNode.toPrettyString(), jsonNode.get("san").asText().isEmpty());
+        } else { // handle non-root
+            assertThat(
+                    jsonNode.toPrettyString(),
+                    jsonNode.get("subject_dn").asText(),
+                    is(String.format(TestCertificates.NODE_SUBJECT_PATTERN, nodeNumber))
+            );
+            assertThat(
+                    jsonNode.toPrettyString(),
+                    jsonNode.get("san").asText(),
+                    containsString(String.format("node-%s.example.com", nodeNumber))
+            );
+        }
         assertThat(jsonNode.toPrettyString(), jsonNode.get("issuer_dn").asText(), is(TestCertificates.CA_SUBJECT));
-        assertThat(
-            jsonNode.toPrettyString(),
-            jsonNode.get("subject_dn").asText(),
-            is(String.format(TestCertificates.NODE_SUBJECT_PATTERN, nodeNumber))
-        );
-        assertThat(
-            jsonNode.toPrettyString(),
-            jsonNode.get("san").asText(),
-            containsString(String.format("node-%s.example.com", nodeNumber))
-        );
         assertThat(jsonNode.toPrettyString(), jsonNode.has("not_before"));
         assertThat(jsonNode.toPrettyString(), jsonNode.has("not_after"));
     }
@@ -193,5 +201,4 @@ public class CertificatesRestApiIntegrationTest extends AbstractApiIntegrationTe
         Collections.shuffle(tempList, RandomizedContext.current().getRandom());
         return tempList.subList(0, size);
     }
-
 }
