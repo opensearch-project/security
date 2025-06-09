b060bdcd880cefc389e5f75e2f06d98f85e96cca
Craig Perkins <cwperx@amazon.com>
2025-06-09 11:24:53 -0400

    Only log on BASIC_TYPE
    Signed-off-by: Craig Perkins <cwperx@amazon.com>

--- src/main/java/org/opensearch/security/auth/BackendRegistry.java
+++ src/main/java/org/opensearch/security/auth/BackendRegistry.java
@@ -78,6 +78,7 @@ import static org.apache.http.HttpStatus.SC_FORBIDDEN;
 import static org.apache.http.HttpStatus.SC_SERVICE_UNAVAILABLE;
 import static org.apache.http.HttpStatus.SC_UNAUTHORIZED;
 import static org.opensearch.security.auth.http.saml.HTTPSamlAuthenticator.SAML_TYPE;
+import static org.opensearch.security.http.HTTPBasicAuthenticator.BASIC_TYPE;
 
 public class BackendRegistry {
 
@@ -322,6 +323,8 @@ public class BackendRegistry {
                         // saml will always hit this to re-request authentication
                         if (!authDomain.getHttpAuthenticator().getType().equals(SAML_TYPE)) {
                             auditLog.logFailedLogin("<NONE>", false, null, request);
+                        }
+                        if (authDomain.getHttpAuthenticator().getType().equals(BASIC_TYPE)) {
                             log.warn("No 'Authorization' header, send 401 and 'WWW-Authenticate Basic'");
                         }
                         notifyIpAuthFailureListeners(request, authCredentials);
