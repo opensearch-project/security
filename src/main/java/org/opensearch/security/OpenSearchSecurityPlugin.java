9981bc2a4d9045d161351de59ed02b9c2c9c988f
Craig Perkins <cwperx@amazon.com>
2025-06-10 16:31:52 -0400

    [Resource Sharing] Store resource sharing info in indices that map 1-to-1 with resource index (#5358)
    Signed-off-by: Craig Perkins <cwperx@amazon.com>
    Signed-off-by: Darshit Chanpura <dchanp@amazon.com>
    Co-authored-by: Darshit Chanpura <dchanp@amazon.com>

--- src/main/java/org/opensearch/security/OpenSearchSecurityPlugin.java
+++ src/main/java/org/opensearch/security/OpenSearchSecurityPlugin.java
@@ -179,7 +179,6 @@ import org.opensearch.security.resources.ResourceAccessControlClient;
 import org.opensearch.security.resources.ResourceAccessHandler;
 import org.opensearch.security.resources.ResourceIndexListener;
 import org.opensearch.security.resources.ResourcePluginInfo;
-import org.opensearch.security.resources.ResourceSharingConstants;
 import org.opensearch.security.resources.ResourceSharingIndexHandler;
 import org.opensearch.security.rest.DashboardsInfoAction;
 import org.opensearch.security.rest.SecurityConfigUpdateAction;
@@ -192,7 +191,6 @@ import org.opensearch.security.securityconf.impl.CType;
 import org.opensearch.security.setting.OpensearchDynamicSetting;
 import org.opensearch.security.setting.TransportPassiveAuthSetting;
 import org.opensearch.security.spi.resources.FeatureConfigConstants;
-import org.opensearch.security.spi.resources.ResourceProvider;
 import org.opensearch.security.spi.resources.ResourceSharingExtension;
 import org.opensearch.security.spi.resources.client.ResourceSharingClient;
 import org.opensearch.security.ssl.ExternalSecurityKeyStore;
@@ -235,6 +233,7 @@ import org.opensearch.watcher.ResourceWatcherService;
 import static org.opensearch.security.dlic.rest.api.RestApiAdminPrivilegesEvaluator.ENDPOINTS_WITH_PERMISSIONS;
 import static org.opensearch.security.dlic.rest.api.RestApiAdminPrivilegesEvaluator.SECURITY_CONFIG_UPDATE;
 import static org.opensearch.security.privileges.dlsfls.FieldMasking.Config.BLAKE2B_LEGACY_DEFAULT;
+import static org.opensearch.security.resources.ResourceSharingIndexHandler.getSharingIndex;
 import static org.opensearch.security.setting.DeprecatedSettings.checkForDeprecatedSetting;
 import static org.opensearch.security.support.ConfigConstants.OPENDISTRO_SECURITY_AUTHENTICATED_USER;
 import static org.opensearch.security.support.ConfigConstants.SECURITY_ALLOW_DEFAULT_INIT_SECURITYINDEX;
@@ -744,10 +743,7 @@ public final class OpenSearchSecurityPlugin extends OpenSearchSecuritySSLPlugin
                 // Listening on POST and DELETE operations in resource indices
                 ResourceIndexListener resourceIndexListener = new ResourceIndexListener(threadPool, localClient);
                 // CS-SUPPRESS-SINGLE: RegexpSingleline get Resource Sharing Extensions
-                Set<String> resourceIndices = resourcePluginInfo.getResourceSharingExtensions()
-                    .stream()
-                    .flatMap(ext -> ext.getResourceProviders().stream().map(ResourceProvider::resourceIndexName))
-                    .collect(Collectors.toSet());
+                Set<String> resourceIndices = resourcePluginInfo.getResourceIndices();
                 // CS-ENFORCE-SINGLE
                 if (resourceIndices.contains(indexModule.getIndex().getName())) {
                     indexModule.addIndexOperationListener(resourceIndexListener);
@@ -1258,8 +1254,7 @@ public final class OpenSearchSecurityPlugin extends OpenSearchSecuritySSLPlugin
             FeatureConfigConstants.OPENSEARCH_RESOURCE_SHARING_ENABLED,
             FeatureConfigConstants.OPENSEARCH_RESOURCE_SHARING_ENABLED_DEFAULT
         )) {
-            final var resourceSharingIndex = ResourceSharingConstants.OPENSEARCH_RESOURCE_SHARING_INDEX;
-            rsIndexHandler = new ResourceSharingIndexHandler(resourceSharingIndex, localClient, threadPool);
+            rsIndexHandler = new ResourceSharingIndexHandler(localClient, threadPool);
 
             ResourceAccessHandler resourceAccessHandler = new ResourceAccessHandler(threadPool, rsIndexHandler, adminDns);
 
@@ -2183,7 +2178,11 @@ public final class OpenSearchSecurityPlugin extends OpenSearchSecuritySSLPlugin
             // create resource sharing index if absent
             // TODO check if this should be wrapped in an atomic completable future
             log.debug("Attempting to create Resource Sharing index");
-            rsIndexHandler.createResourceSharingIndexIfAbsent();
+            Set<String> resourceIndices = new HashSet<>();
+            if (resourcePluginInfo != null) {
+                resourceIndices = resourcePluginInfo.getResourceIndices();
+            }
+            rsIndexHandler.createResourceSharingIndicesIfAbsent(resourceIndices);
 
         }
 
@@ -2240,11 +2239,13 @@ public final class OpenSearchSecurityPlugin extends OpenSearchSecuritySSLPlugin
                 FeatureConfigConstants.OPENSEARCH_RESOURCE_SHARING_ENABLED,
                 FeatureConfigConstants.OPENSEARCH_RESOURCE_SHARING_ENABLED_DEFAULT
             )) {
-            final SystemIndexDescriptor resourceSharingIndexDescriptor = new SystemIndexDescriptor(
-                ResourceSharingConstants.OPENSEARCH_RESOURCE_SHARING_INDEX,
-                "Resource Sharing index"
-            );
-            systemIndexDescriptors.add(resourceSharingIndexDescriptor);
+            for (String resourceIndex : resourcePluginInfo.getResourceIndices()) {
+                final SystemIndexDescriptor resourceSharingIndexDescriptor = new SystemIndexDescriptor(
+                    getSharingIndex(resourceIndex),
+                    "Resource Sharing index for index: " + resourceIndex
+                );
+                systemIndexDescriptors.add(resourceSharingIndexDescriptor);
+            }
         }
 
         return ImmutableList.copyOf(systemIndexDescriptors);
