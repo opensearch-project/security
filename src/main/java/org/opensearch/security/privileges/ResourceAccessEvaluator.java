f76403f7a4478fd36e83cc6d6b5dc6177891baa8
Darshit Chanpura <dchanp@amazon.com>
2025-06-18 12:58:51 -0400

    Adds debug log statements to the evaluator
    Signed-off-by: Darshit Chanpura <dchanp@amazon.com>

--- src/main/java/org/opensearch/security/privileges/ResourceAccessEvaluator.java
+++ src/main/java/org/opensearch/security/privileges/ResourceAccessEvaluator.java
@@ -6,6 +6,9 @@ import java.util.Set;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.atomic.AtomicBoolean;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import org.opensearch.action.ActionRequest;
 import org.opensearch.action.DocRequest;
 import org.opensearch.common.util.concurrent.ThreadContext;
@@ -20,6 +23,8 @@ import org.opensearch.security.user.User;
 import org.opensearch.threadpool.ThreadPool;
 
 public class ResourceAccessEvaluator {
+    private static final Logger log = LogManager.getLogger(ResourceAccessEvaluator.class);
+
     private final Set<String> resourceIndices;
     private final ThreadContext threadContext;
     private final ResourceSharingIndexHandler resourceSharingIndexHandler;
@@ -48,6 +53,8 @@ public class ResourceAccessEvaluator {
             return presponse;
         }
 
+        log.debug("Evaluating resource access");
+
         final UserSubjectImpl userSubject = (UserSubjectImpl) this.threadContext.getPersistent(
             ConfigConstants.OPENDISTRO_SECURITY_AUTHENTICATED_USER
         );
@@ -55,6 +62,7 @@ public class ResourceAccessEvaluator {
 
         if (user == null) {
             presponse.allowed = false;
+            log.debug("User is not authenticated, returning unauthorized");
             return presponse.markComplete();
         }
 
@@ -65,16 +73,17 @@ public class ResourceAccessEvaluator {
         if (req.id() == null) {
             // check write permissions
             // TODO verify that this can be punted to the regular evaluator since it requires write permissions to the index
+            log.debug("Request id is null, request is of type {}", req.getClass().getName());
             return presponse;
         }
 
         // if requested index is not a resource sharing index, move on to the next evaluator
         if (!resourceIndices.contains(req.index())) {
+            log.debug("Request index {} is not a protected resource index", req.index());
             return presponse;
         }
 
         // Fetch the ResourceSharing document
-
         CountDownLatch latch = new CountDownLatch(1);
 
         Set<String> userRoles = new HashSet<>(user.getSecurityRoles());
@@ -85,6 +94,7 @@ public class ResourceAccessEvaluator {
             if (document == null) {
                 // TODO check whether we should mark response as not allowed. At present, it just returns incomplete response and hence is
                 // delegated to next evaluator
+                log.debug("No resource sharing record found for resource {} and index {}, skipping evaluation.", req.id(), req.index());
                 latch.countDown();
                 return;
             }
@@ -94,6 +104,10 @@ public class ResourceAccessEvaluator {
             if (document.isSharedWithEveryone() || document.isCreatedBy(user.getName())) {
                 presponse.allowed = true;
                 shouldMarkAsComplete.set(true);
+                String message = document.isSharedWithEveryone()
+                    ? "Publicly shared resource"
+                    : "User " + user.getName() + " is the owner of the resource";
+                log.debug("{} {}, granting access.", message, req.id());
                 latch.countDown();
                 return;
             }
@@ -105,8 +119,7 @@ public class ResourceAccessEvaluator {
 
             if (accessLevels.isEmpty()) {
                 presponse.allowed = false;
-                // TODO check why following addition doesn't reflect in the final response message and find an alternative
-                presponse.getMissingPrivileges().add(action);
+                log.debug("Resource {} is not shared with user {}", req.id(), user.getName());
                 shouldMarkAsComplete.set(true);
                 latch.countDown();
                 return;
@@ -119,20 +132,28 @@ public class ResourceAccessEvaluator {
                 WildcardMatcher matcher = WildcardMatcher.from(actions, true);
                 if (matcher.test(action)) {
                     presponse.allowed = true;
+                    log.debug("Resource {} is shared with user {}, granting access.", req.id(), user.getName());
                 } else {
                     // TODO check why following addition doesn't reflect in the final response message and find an alternative
                     presponse.getMissingPrivileges().add(action);
+                    log.debug("User {} has no {} privileges for {}", user.getName(), action, req.id());
                 }
                 latch.countDown();
             } else {
                 // we don't yet support Plugins to access resources
                 presponse.allowed = false;
+                log.debug(
+                    "Plugin access to resources is currently not supported. Plugin {} is not authorized to access resource {}.",
+                    user.getName(),
+                    req.id()
+                );
                 latch.countDown();
             }
             shouldMarkAsComplete.set(true);
 
         }, e -> {
             presponse.allowed = false;
+            log.debug("Something went wrong while evaluating resource {}. Marking request as unauthorized.", req.id());
             shouldMarkAsComplete.set(true);
             latch.countDown();
         }));
