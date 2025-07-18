07b92e06ec207b406a08d576f17019b43f9a1e7b
pranu2502 <52481227+pranu2502@users.noreply.github.com>
2025-07-18 18:12:34 +0530

    [3.1 Backport] Optimizations and simplifications for WildcardMatcher (#5487)
    Signed-off-by: Nils Bandener <nils.bandener@eliatra.com>
    Signed-off-by: Pranav Reddy <pranavrd@amazon.com>
    Co-authored-by: Nils Bandener <33570290+nibix@users.noreply.github.com>

--- src/main/java/org/opensearch/security/securityconf/DynamicConfigFactory.java
+++ src/main/java/org/opensearch/security/securityconf/DynamicConfigFactory.java
@@ -382,7 +382,9 @@ public class DynamicConfigFactory implements Initializable, ConfigurationChangeL
             return this.configuration.getCEntries()
                 .entrySet()
                 .stream()
-                .collect(ImmutableMap.toImmutableMap(Entry::getKey, entry -> WildcardMatcher.from(entry.getValue().getNodesDn(), false)));
+                .collect(
+                    ImmutableMap.toImmutableMap(Entry::getKey, entry -> WildcardMatcher.from(entry.getValue().getNodesDn()).ignoreCase())
+                );
         }
     }
 }
