032d72df48ed9254d762ed417e0ef66ff3f34c58
Mark Boyd <mark.boyd@gsa.gov>
2025-07-21 14:32:11 -0400

    add plugins.security.user_attribute_serialization.enabled setting to control whether user attributes are serialized
    Signed-off-by: Mark Boyd <mark.boyd@gsa.gov>

--- src/main/java/org/opensearch/security/OpenSearchSecurityPlugin.java
+++ src/main/java/org/opensearch/security/OpenSearchSecurityPlugin.java
@@ -2230,6 +2230,15 @@ public final class OpenSearchSecurityPlugin extends OpenSearchSecuritySSLPlugin
                     Property.Final
                 )
             );
+
+            settings.add(
+                Setting.boolSetting(
+                    ConfigConstants.USER_ATTRIBUTE_SERIALIZATION_ENABLED,
+                    ConfigConstants.USER_ATTRIBUTE_SERIALIZATION_ENABLED_DEFAULT,
+                    Property.NodeScope,
+                    Property.Filtered
+                )
+            );
         }
 
         return settings;
