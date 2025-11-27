3134305b85dc390243631b972e92845d20fa0e64
Nils Bandener <nils.bandener@eliatra.com>
2025-11-27 16:41:42 +0100

    Moved DLS/FLS to ResolvedIndices
    Signed-off-by: Nils Bandener <nils.bandener@eliatra.com>

--- src/main/java/org/opensearch/security/privileges/PrivilegesEvaluationContext.java
+++ src/main/java/org/opensearch/security/privileges/PrivilegesEvaluationContext.java
@@ -17,9 +17,12 @@ import java.util.function.Supplier;
 import com.google.common.collect.ImmutableSet;
 
 import org.opensearch.action.ActionRequest;
+import org.opensearch.action.support.ActionRequestMetadata;
 import org.opensearch.cluster.ClusterState;
 import org.opensearch.cluster.metadata.IndexAbstraction;
 import org.opensearch.cluster.metadata.IndexNameExpressionResolver;
+import org.opensearch.cluster.metadata.OptionallyResolvedIndices;
+import org.opensearch.cluster.metadata.ResolvedIndices;
 import org.opensearch.security.resolver.IndexResolverReplacer;
 import org.opensearch.security.support.WildcardMatcher;
 import org.opensearch.security.user.User;
@@ -59,11 +62,14 @@ public class PrivilegesEvaluationContext {
      */
     private final Map<String, WildcardMatcher> renderedPatternTemplateCache = new HashMap<>();
 
+    private final ActionRequestMetadata<?, ?> actionRequestMetadata;
+
     public PrivilegesEvaluationContext(
         User user,
         ImmutableSet<String> mappedRoles,
         String action,
         ActionRequest request,
+        ActionRequestMetadata<?, ?> actionRequestMetadata,
         Task task,
         IndexResolverReplacer indexResolverReplacer,
         IndexNameExpressionResolver indexNameExpressionResolver,
@@ -79,6 +85,7 @@ public class PrivilegesEvaluationContext {
         this.indexNameExpressionResolver = indexNameExpressionResolver;
         this.task = task;
         this.actionPrivileges = actionPrivileges;
+        this.actionRequestMetadata = actionRequestMetadata;
     }
 
     public User getUser() {
@@ -118,6 +125,10 @@ public class PrivilegesEvaluationContext {
         return request;
     }
 
+    /**
+     * @deprecated use getResolvedIndices() instead
+     */
+    @Deprecated
     public IndexResolverReplacer.Resolved getResolvedRequest() {
         IndexResolverReplacer.Resolved result = this.resolvedRequest;
 
@@ -127,6 +138,10 @@ public class PrivilegesEvaluationContext {
         }
 
         return result;
+    }
+
+    public OptionallyResolvedIndices getResolvedIndices() {
+        return this.actionRequestMetadata.resolvedIndices();
     }
 
     public Task getTask() {
@@ -151,6 +166,10 @@ public class PrivilegesEvaluationContext {
 
     public Supplier<ClusterState> getClusterStateSupplier() {
         return clusterStateSupplier;
+    }
+
+    public ClusterState clusterState() {
+        return clusterStateSupplier.get();
     }
 
     public Map<String, IndexAbstraction> getIndicesLookup() {
