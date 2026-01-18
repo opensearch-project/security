6eaf04717f11e86ea396501552f537e73caf9119
Finn Carroll <carrofin@amazon.com>
2026-01-17 17:17:22 -0800

    initial grpc extension of SecurityRequestChannel. Internal representation of security metadata for a request.
    Signed-off-by: Finn Carroll <carrofin@amazon.com>

---
+++ src/main/java/org/opensearch/security/filter/GrpcRequestChannel.java
@@ -0,0 +1,105 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ */
+
+package org.opensearch.security.filter;
+
+import java.net.InetSocketAddress;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+import javax.net.ssl.SSLEngine;
+
+import io.grpc.Metadata;
+import io.grpc.ServerCall;
+import org.opensearch.rest.RestRequest.Method;
+
+/**
+ * gRPC implementation of SecurityRequestChannel.
+ */
+public class GrpcRequestChannel implements SecurityRequestChannel {
+
+    private final ServerCall<?, ?> serverCall;
+    private final Metadata metadata;
+    private final Map<String, List<String>> headers;
+    private final Map<String, String> params;
+    private Optional<SecurityResponse> queuedResponse = Optional.empty();
+
+    public GrpcRequestChannel(ServerCall<?, ?> serverCall, Metadata metadata) {
+        this.serverCall = serverCall;
+        this.metadata = metadata;
+        this.headers = extractHeaders(metadata);
+        this.params = new HashMap<>(); // gRPC doesn't have URL parameters like REST
+    }
+
+    private Map<String, List<String>> extractHeaders(Metadata metadata) {
+        Map<String, List<String>> headerMap = new HashMap<>();
+        for (String key : metadata.keys()) {
+            if (!key.endsWith("-bin")) { // Skip binary headers
+                String value = metadata.get(Metadata.Key.of(key, Metadata.ASCII_STRING_MARSHALLER));
+                if (value != null) {
+                    headerMap.put(key, List.of(value));
+                }
+            }
+        }
+        return headerMap;
+    }
+
+    @Override
+    public Map<String, List<String>> getHeaders() {
+        return headers;
+    }
+
+    @Override
+    public SSLEngine getSSLEngine() {
+        throw new UnsupportedOperationException("Client certificate authentication not supported for gRPC");
+    }
+
+    @Override
+    public String path() {
+        return serverCall.getMethodDescriptor().getFullMethodName();
+    }
+
+    @Override
+    public Method method() {
+        throw new UnsupportedOperationException("HTTP methods not applicable to gRPC");
+    }
+
+    @Override
+    public Optional<InetSocketAddress> getRemoteAddress() {
+        // gRPC ServerCall doesn't directly expose remote address
+        throw new UnsupportedOperationException("Remote address not supported on gRPC");
+    }
+
+    @Override
+    public String uri() {
+        return serverCall.getMethodDescriptor().getFullMethodName();
+    }
+
+    @Override
+    public Map<String, String> params() {
+        return params;
+    }
+
+    @Override
+    public Set<String> getUnconsumedParams() {
+        // Query params should always be contained in the request body/protobuf schema
+        throw new UnsupportedOperationException("Query params not applicable to gRPC");
+    }
+
+    @Override
+    public void queueForSending(SecurityResponse response) {
+        this.queuedResponse = Optional.of(response);
+    }
+
+    @Override
+    public Optional<SecurityResponse> getQueuedResponse() {
+        return queuedResponse;
+    }
+}
