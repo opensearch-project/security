99edcea662e201675a227682fa4960a093135d59
Kishore Kumaar Natarajan <kkumaarn@amazon.com>
2026-06-08 11:15:49 -0700

    fix: Remove unused assertTrue import

--- src/test/java/org/opensearch/security/transport/SecurityInterceptorTests.java
+++ src/test/java/org/opensearch/security/transport/SecurityInterceptorTests.java
@@ -65,7 +65,6 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertThrows;
-import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
